6316a72bb44e1581630430e92fcda5bd98f8c9cb
Z-1337 <un228der@gmail.com>
2025-02-03 01:22:48 +0300

    unit test zagotovo4ka

---
+++ kushekbaev.danil/S0/test-credentials.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE test credentials
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(credentials_test)
+{
+  int i = 1;
+  BOOST_TEST(i);
+  BOOST_TEST(i == 2);
+}
