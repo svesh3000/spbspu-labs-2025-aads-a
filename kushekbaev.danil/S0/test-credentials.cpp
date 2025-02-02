884e7ae75dafbfb501147b2e2e7836fe2ad766e7
Z-1337 <un228der@gmail.com>
2025-02-03 01:53:33 +0300

    deletion of suite

--- kushekbaev.danil/S0/test-credentials.cpp
+++ kushekbaev.danil/S0/test-credentials.cpp
@@ -2,20 +2,9 @@
 #include <sstream>
 #include "credentials.hpp"
 
-BOOST_AUTO_TEST_SUITE(credentials_suite)
-
 BOOST_AUTO_TEST_CASE(credentials_test)
 {
   std::ostringstream out;
   kushekbaev::outCredentials(out);
   BOOST_TEST(out.str() == "kushekbaev.danil");
 }
-
-BOOST_AUTO_TEST_CASE(ya_credentials_test)
-{
-  std::ostringstream out;
-  kushekbaev::outCredentials(out);
-  BOOST_TEST(out.str() == "kushekbaev.danil");
-}
-
-BOOST_AUTO_TEST_SUITE_END()
