eb1b3c5e905ab5270f80f8c0a9dd90fb228ff6e2
Ksenia-rgb <ksenialymova@mail.ru>
2025-02-03 10:59:58 +0300

    add credentials header and implementation

---
+++ alymova.ksenia/S0/credentials.cpp
@@ -0,0 +1,7 @@
+#include "credentials.hpp"
+#include <iostream>
+std::ostream& alymova::out_credentials(std::ostream& out)
+{
+  return out << "alymova.ksenia";
+}
+
--- alymova.ksenia/S0/main.cpp
+++ alymova.ksenia/S0/main.cpp
@@ -1,1 +1,7 @@
+#include <iostream>
+#include "credentials.hpp"
 int main()
+{
+  alymova::out_credentials(std::cout) << "\n";
+}
+
