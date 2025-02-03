6748c60e3599c4f16f6e1b27963ec95a01cb0f70
Nekich06 <p.nikita.73526@gmail.com>
2025-02-03 23:49:58 +0300

    Fix test-main

--- petrov.nikita/S0/test-test.cpp
+++
@@ -1,9 +0,0 @@
-#define BOOST_TEST_MODULE Test credentials
-#include <boost/test/included/unit_test.hpp>
-
-BOOST_AUTO_TEST_CASE(test_test)
-{
-  int i = 1;
-  BOOST_TEST(i);
-  BOOST_TEST(i == 2);
-}
