372303ae73d8dd74a0080c5da7bd27acfd7f3fe9
Sergey Dribas <serdribas@gmail.com>
2025-02-03 22:52:07 +0300

    add suite for test

--- dribas.sergey/S0/test-credentials.cpp
+++ dribas.sergey/S0/test-credentials.cpp
@@ -2,9 +2,13 @@
 #include <sstream>
 #include "credentials.hpp"
 
+BOOST_AUTO_TEST_SUITE(credentials_test)
+
 BOOST_AUTO_TEST_CASE(credentials_test)
 {
   std::ostringstream out;
   dribas::outCredentials(out);
   BOOST_TEST(out.str() == "dribas.sergey");
 }
+
+BOOST_AUTO_TEST_SUITE_END()
