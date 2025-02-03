603ef59265fe392091861b9ad103d56e3eb94946
lee <likachich12@gmail.com>
2025-02-03 19:25:38 +0300

    final

--- hismatova.elizaveta/S0/test-credentials.cpp
+++ hismatova.elizaveta/S0/test-credentials.cpp
@@ -2,13 +2,9 @@
 #include <sstream>
 #include "credentials.hpp"
 
-BOOST_AUTO_TEST_SUITE(credentials_suite)
-
 BOOST_AUTO_TEST_CASE(credentials_test)
 {
   std::ostringstream out;
   hismatova::out_credentials(out);
   BOOST_TEST(out.str() == "hismatova.elizaveta");
 }
-
-BOOST_AUTO_TEST_SUITE_END()
