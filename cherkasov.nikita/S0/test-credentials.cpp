f160bc5ce637b80461fa4c69085c1804ac4c0883
cherkasoov <cherkasov5509@gmail.com>
2025-02-04 19:22:15 +0300

    ...

--- cherkasov.nikita/S0/test-credentials.cpp
+++ cherkasov.nikita/S0/test-credentials.cpp
@@ -1,9 +1,10 @@
-#define BOOST_TEST_MODUL Test credentials
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
+#include <sstream>
+#include "credentials.hpp"
 
 BOOST_AUTO_TEST_CASE(credentials_test)
 {
-  int = 1;
-  BOOST_TEST(i);
-  BOOST_TEST(i == 2);
+  std::ostringstream out;
+  cherkasov::out_credentials(out);
+  BOOST_TEST(out.str() == "cherkasov.nikita");
 }
