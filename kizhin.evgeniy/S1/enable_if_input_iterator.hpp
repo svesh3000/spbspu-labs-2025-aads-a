fc1c5e3fec02c0523a6575148a81e2caffb08751
Might be a Fox <e.kizhin@yandex.ru>
2025-02-11 17:37:54 +0300

    Introduce SFINAE for input iterator validation
    Added enable_if_input_iterator template alias to validate if a
    given type is an input iterator. This will be used in the list
    implementation

---
+++ kizhin.evgeniy/S1/enable_if_input_iterator.hpp
@@ -0,0 +1,33 @@
+#ifndef SPBSPU_LABS_2025_AADS_A_KIZHIN_EVGENIY_S1_ENABLE_IF_INPUT_ITERATOR
+#define SPBSPU_LABS_2025_AADS_A_KIZHIN_EVGENIY_S1_ENABLE_IF_INPUT_ITERATOR
+
+#include <iterator>
+#include <type_traits>
+
+namespace kizhin {
+  namespace detail {
+
+    template < typename... >
+    using void_t = void;
+
+    template < typename T, typename = void >
+    struct is_input_iterator: std::false_type
+    {
+    };
+
+    template < typename T >
+    struct is_input_iterator< T,
+        void_t< typename std::iterator_traits< T >::iterator_category > >:
+      std::is_base_of< std::input_iterator_tag,
+          typename std::iterator_traits< T >::iterator_category >
+    {
+    };
+
+    template < typename It >
+    using enable_if_input_iterator =
+        typename std::enable_if< is_input_iterator< It >::value, int >::type;
+  }
+}
+
+#endif
+
