50acc246d93b0d6491cf9572e720548f90d00ecc
faxryzen <cheburek2006@gmail.com>
2025-02-12 15:21:16 +0300

    Input of lists done, except sums

--- savintsev.ilya/S1/main.cpp
+++ savintsev.ilya/S1/main.cpp
@@ -8,40 +8,61 @@ int main()
   using storage_t = std::list<pair_t>;
   storage_t storage;
   std::string buffer = "";
+  size_t max_size = 0;
   std::cin >> buffer;
   while (!std::cin.eof())
   {
     pair_t temp_pair;
     temp_pair.first = buffer;
     std::cin >> buffer;
+    size_t temp_max_size = 0;
     while (isdigit(buffer[0]) && !std::cin.eof())
     {
       temp_pair.second.push_back(strtoull(buffer.c_str(), nullptr, 10));
+      temp_max_size++;
       std::cin >> buffer;
     }
+    max_size = std::max(max_size, temp_max_size);
     storage.push_back(temp_pair);
   }
-  for (storage_t::iterator i = storage.begin(); i != storage.end(); ++i)
+  for (storage_t::iterator it = storage.begin(); it != storage.end(); ++it)
   {
-    std::cout << i->first;
-    if (std::next(i) != storage.end())
+    std::cout << it->first;
+    if (std::next(it) != storage.end())
     {
       std::cout << " ";
     }
   }
   std::cout << "\n";
-  storage_t::iterator it = storage.begin();
-  for (std::list<size_t>::iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
+  //std::list<size_t> sums;
+  for (size_t i = 0; i < max_size; ++i)
   {
-    for (; it != storage.end(); ++it)
+    size_t printed = 0;
+    //sums.push_back(0);
+    for (storage_t::iterator it = storage.begin(); it != storage.end(); ++it)
     {
-      std::cout << *jt;
-      if (std::next(it) != storage.end())
+      auto temp_it = it->second.begin();
+      if (it->second.size() > i)
       {
-        std::cout << " ";
+        for (size_t next = 0; next < i; ++next)
+        {
+          temp_it++;
+        }
+        if (printed != 0)
+        {
+          std::cout << "_";
+        }
+        std::cout << *(temp_it);
+        printed++;
       }
     }
-    std::cout << '\n';
-    it = storage.begin();
+  std::cout << "\n";
   }
 }
+
+/*
+first 1 1 1
+second 2 2 2 2
+third
+fourth 4 4
+*/
