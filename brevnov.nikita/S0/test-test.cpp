68169d710913761362c9c8bc8276309f5065ec5e
root <root@LAPTOP-FGITSRCQ.localdomain>
2025-02-12 15:36:31 +0300

    message test

--- brevnov.nikita/S0/test-test.cpp
+++ brevnov.nikita/S0/test-test.cpp
@@ -1,7 +1,7 @@
 #define BOOST_TEST_MODULE Test credentials
 #include <boost/test/included/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(credential_test)
+BOOST_AUTO_TEST_CASE(test_test)
 {
   int i = 1;
   BOOST_TEST(i);
