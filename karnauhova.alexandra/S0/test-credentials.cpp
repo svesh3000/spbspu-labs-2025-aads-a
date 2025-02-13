f3274d357f482e8d60b09b71379f559d4b8d7380
“Karnaukhova <masj280906@gmail.com>
2025-02-13 22:42:38 +0300

    without suit

--- karnauhova.alexandra/S0/test-credentials.cpp
+++ karnauhova.alexandra/S0/test-credentials.cpp
@@ -2,13 +2,9 @@
 #include <sstream>
 #include "credentials.hpp"
 
-BOOST_AUTO_TEST_SUITE(credentials_suite)
-
 BOOST_AUTO_TEST_CASE(credentials_test)
 {
   std::ostringstream out;
   karnauhova::out_credentials(out);
   BOOST_TEST(out.str() == "karnauhova.alexandra");
 }
-
-BOOST_AUTO_TEST_SUITE_END()
