7dd57482b9c5a0cfe8a00333b76f7f92a4c150b3
deonik3 <kis.sergey2006@gmail.com>
2025-02-13 20:06:05 +0300

    Upgrate main
    creating a list and filling it with names and numbers

--- kiselev.sergey/S1/main.cpp
+++ kiselev.sergey/S1/main.cpp
@@ -1,3 +1,52 @@
+#include <cstddef>
+#include <iostream>
+#include <list>
+#include <new>
+#include <string>
+
+int* createArr(std::istream& input, size_t size)
+{
+  int* array = new int[size];
+  int number = 0;
+  size_t count = 0;
+  while (input >> number)
+  {
+    if (count >= size)
+    {
+      size_t previousSize = size;
+      size += 5;
+      int* newArray = nullptr;
+      try
+      {
+        newArray = new int[size];
+        for (size_t i = 0; i < previousSize; ++i)
+        {
+          newArray[i] = array[i];
+        }
+      }
+      catch (const std::bad_alloc&)
+      {
+        delete[] array;
+        throw;
+      }
+      delete[] array;
+      array = newArray;
+    }
+    array[size++] = number;
+  }
+  return array;
+}
+
 int main()
 {
+  using pair = std::pair< std::string, int* >;
+  using pairList = std::list< pair >;
+  pairList list;
+  std::string nameNode;
+  size_t capacity = 20;
+  while (std::cin >> nameNode)
+  {
+    int* numbers = createArr(std::cin, capacity);
+    list.push_back(pair(nameNode, numbers));
+  }
 }
