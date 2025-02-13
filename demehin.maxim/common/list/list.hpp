acf5ed750b19144e9f400cabf2b0acb99191aa71
TvoyBatyA12343 <maxdem06@gmail.com>
2025-02-13 20:24:19 +0300

    add signature of basic list methods

--- demehin.maxim/common/list/list.hpp
+++ demehin.maxim/common/list/list.hpp
@@ -1,13 +1,51 @@
 #ifndef LIST_HPP
 #define LIST_HPP
-#include "node.hpp"
+#include "iterator.hpp"
 
 namespace demehin
 {
+  template< typename T >
   struct List
   {
   public:
+    using Iter = ListIterator< T >;
 
+    List();
+
+    ~List();
+
+    List(const List< T >&);
+    List(List< T >&&);
+
+    Iter begin();
+    Iter end();
+
+    size_t size() const;
+
+    void pop_front();
+
+    void push_back(const T&);
+
+    T& front();
+
+  private:
+    using Node = Node< T >;
+
+    Node* fake_;
+    Node* head_;
+    Node* tail_;
+    size_t size_;
+
+  };
+
+  template< typename T >
+  List< T >::List():
+    fake_(static_cast< Node< T >* >(new char[sizeof(Node < T >)])),
+    head_(nullptr),
+    tail_(nullptr),
+    size_(0)
+  {
+    new (fake_) Node< T >(T());
   }
 }
 
