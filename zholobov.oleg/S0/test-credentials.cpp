09ce0ba84f072413b72d1dfee114f45d3849e12a
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-02-14 23:28:24 +0300

    added test suite

--- zholobov.oleg/S0/test-credentials.cpp
+++ zholobov.oleg/S0/test-credentials.cpp
@@ -3,8 +3,12 @@
 
 #include "credentials.hpp"
 
+BOOST_AUTO_TEST_SUITE(credentials_suite)
+
 BOOST_AUTO_TEST_CASE(credentials_test) {
   std::stringstream ss;
   zholobov::out_credentials(ss);
   BOOST_TEST(ss.str() == "zholobov.oleg");
 }
+
+BOOST_AUTO_TEST_SUITE_END()
