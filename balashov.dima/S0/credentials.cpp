db31bac33706e3b65f903aca9cb41f429123a885
“BalashovDima” <zoozoz98765@gmail.com>
2025-02-15 22:38:29 +0300

    out credentials

---
+++ balashov.dima/S0/credentials.cpp
@@ -0,0 +1,7 @@
+#include "credentials.hpp"
+#include <iostream>
+
+std::ostream& balashov::outCredentials(std::ostream& out)
+{
+  return out << "balashov.dima";
+}
---
+++ balashov.dima/S0/credentials.hpp
@@ -0,0 +1,11 @@
+#ifndef CREDENTIALS_HPP
+#define CREDENTIALS_HPP
+
+#include <iosfwd>
+
+namespace balashov
+{
+  std::ostream& outCredentials(std::ostream& out);
+}
+
+#endif
