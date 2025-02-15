a9877b0467044ac2c2848baf41dabcc8af80eb67
Maksim_Shramko <maxramko@gmail.com>
2025-02-15 15:30:42 +0300

    BOOOST to BOOST.

--- shramko.maksim/S0/test-credentials.cpp
+++ shramko.maksim/S0/test-credentials.cpp
@@ -2,7 +2,7 @@
 #include <sstream>
 #include "credentials.hpp"
 
-BOOOST_AUTO_TEST_CASE(test_credentials)
+BOOST_AUTO_TEST_CASE(test_credentials)
 {
   std::ostringstream out;
   shramko::outCredentials(out);
