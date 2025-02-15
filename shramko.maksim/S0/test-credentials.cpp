b48cd7968661b88a831ad1d6209c4888c2af58e7
Maksim_Shramko <maxramko@gmail.com>
2025-02-15 15:22:09 +0300

    Rly...? Shift key is a joker card or smth??? Correcting - to _

--- shramko.maksim/S0/test-credentials.cpp
+++ shramko.maksim/S0/test-credentials.cpp
@@ -2,7 +2,7 @@
 #include <sstream>
 #include "credentials.hpp"
 
-BOOOST_AUTO_TEST_CASE(test-credentials)
+BOOOST_AUTO_TEST_CASE(test_credentials)
 {
   std::ostringstream out;
   shramko::outCredentials(out);
