8347e1ae8b4e932272b729f3b5acc24f501d6bc1
TvoyBatyA12343 <maxdem06@gmail.com>
2025-02-16 16:06:08 +0300

    delete garbage

--- demehin.maxim/S1/main.cpp
+++ demehin.maxim/S1/main.cpp
@@ -8,7 +8,6 @@
 
 namespace
 {
-  //using ListOfPairs = std::list< std::pair< std::string, std::list< int > > >;
   using ListOfPairs = demehin::List< std::pair< std::string, demehin::List< unsigned long long > > >;
 
   size_t defineMaxSize(ListOfPairs pairsList)
@@ -20,46 +19,6 @@ namespace
     }
     return max_size;
   }
-
-  //unsigned long long* copyNums(const unsigned long long* nums, size_t nums_cnt)
-  //{
-    //unsigned long long* new_nums = new unsigned long long[nums_cnt];
-    //for (size_t i = 0; i < nums_cnt; i++)
-    //{
-      //new_nums[i] = nums[i];
-    //}
-    //return new_nums;
-  //}
-
-  //void printSumValues(std::ostream& out, const unsigned long long* vals, size_t size)
- // {
-    //if (size > 0)
-    //{
-      //out << vals[0];
-      //for (size_t i = 1; i < size; i++)
-      //{
-       //out << " " << vals[i];
-      //}
-    //}
-    //else
-    //{
-      //out << 0;
-    //}
-  //}
-
-
-  //demehin::List< unsigned long long > getValues(ListOfPairs pairsList)
-  //{
-    //demehin::List< unsigned long long > numList;
-    //for (auto it = pairsList.begin(); it != pairsList.end(); it++)
-    //{
-      //if (!it->second.empty())
-      //{
-        //numList.push_back(it->second.front());
-      //}
-    //}
-    //return numList;
-  //}
 
   unsigned long long calculateSum(demehin::List< unsigned long long > nums)
   {
@@ -80,7 +39,6 @@ namespace
     }
     return sum;
   }
-
 
   void printValues(std::ostream& out, ListOfPairs pairsList)
   {
@@ -114,138 +72,22 @@ namespace
       out << " " << *it;
     }
   }
-  //void printValues(std::ostream& out, ListOfPairs pairsList)
-  //{
-    //demehin::List< unsigned long long > sumList;
-    //size_t max_size = defineMaxSize(pairsList);
-    //for (size_t i = 0; i < max_size; i++)
-    //{
-      //demehin::List< unsigned long long > numsList = getValues(pairsList);
-      //out << *numsList.begin();
-      //for (auto it = ++numsList.begin(); it != numsList.end(); it++)
-      //{
-        //out << " " << *it;
-      //}
-      //out << "\n";
-      //sumList.push_back(calculateSum(numsList));
-      //pairsList.pop_front();
-    //}
-
-    //out << *sumList.begin();
-    //for (auto it = ++sumList.begin(); it != sumList.end(); it++)
-    //{
-      //out << " " << *it;
-    //}
-  //}
-
-  /*void printValues(std::ostream& out, ListOfPairs pairsList)
-  {
-    size_t capacity = 128;
-    unsigned long long* sum_values = new unsigned long long[capacity]();
-    size_t max_size = defineMaxSize(pairsList);
-    size_t sum_vals_cnt = 0;
-    unsigned long long max_int = std::numeric_limits< unsigned long long >::max();
-    for (size_t i = 0; i < max_size; i++)
-    {
-      unsigned long long sum = 0;
-      bool isFirst = true;
-      for (auto it = pairsList.begin(); it != pairsList.end(); it++)
-      {
-        if (!it->second.empty())
-        {
-          if (!isFirst)
-          {
-            out << " ";
-          }
-          else
-          {
-            isFirst = false;
-          }
-          out << it->second.front();
-          if (sum > max_int - it->second.front())
-          {
-            delete[] sum_values;
-            throw std::overflow_error("error: overflow");
-          }
-          sum += it->second.front();
-          it->second.pop_front();
-        }
-      }
-      if (i >= capacity)
-      {
-        capacity *= 2;
-        unsigned long long* new_values = nullptr;
-        try
-        {
-          new_values = copyNums(sum_values, capacity);
-        }
-        catch (const std::bad_alloc&)
-        {
-          delete[] sum_values;
-          throw;
-        }
-        delete[] sum_values;
-        sum_values = new_values;
-      }
-
-      sum_values[sum_vals_cnt++] = sum;
-      out << "\n";
-    }
-    printSumValues(out, sum_values, sum_vals_cnt);
-    delete[] sum_values;
-  }*/
 }
 
 int main()
 {
-  //std::list< std::pair< std::string, std::list< int > > > pairsList;
   demehin::List< std::pair< std::string, demehin::List< unsigned long long > > > pairsList;
   std::string nodeName;
   while (std::cin >> nodeName)
   {
     demehin::List< unsigned long long > numsList;
     unsigned long long num = 0;
-    //std::string num_str;
-    //int num = 0;
-    //std::getline(std::cin, num_str);
-    //while (std::cin >> num_str && std::cin.peek() != '\n')
     while (std::cin >> num)
     {
-      //if (num_str.empty())
-      //{
-        //b/reak;
-      //}
-      //try
-      //{
-        //unsigned long long num = std::stoll(num_str);
-        //if (len < num_str.length())
-        //{
-          //std::cerr << "erorr: overflow\n";
-          //return 1;
-        //}
-        //numsList.push_back(num);
-      //}
-      //catch (const std::out_of_range&)
-      //{
-        //std::cerr << "error: overflow\n";
-        //return 1;
-      //}
-      //catch (const std::invalid_argument&)
-     // {
-        //pairsList.push_back(std::make_pair(nodeName, numsList));
-      //}
-      //catch (const std::out_of_range&)
-      //{
-        //std::cerr << "error: overflow\n";
-        //return 1;
-      //}
-      //numsList.push_back(num);
       numsList.push_back(num);
     }
     pairsList.push_back(std::make_pair(nodeName, numsList));
     std::cin.clear();
-    //std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-    //std::cin.clear();
   }
 
   if (pairsList.size() == 0)
