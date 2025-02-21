1bbef747ef0dcb96069f5fdff1b7a7792c2dd86c
root <root@kryloff.>
2025-02-21 15:30:02 +0300

    Input and output implmentation with std::list finished

--- krylov.matvey/S1/main.cpp
+++ krylov.matvey/S1/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <utility>
 #include <list>
 #include <string>
 
@@ -8,6 +7,7 @@ int main()
   std::list< std::pair< std::string, std::list< int > > > listOfPairs;
   std::string line = "";
   std::cin >> line;
+  size_t maxSizeOfNumList = 0;
   while (!std::cin.eof())
   {
     std::pair< std::string, std::list< int > > pair;
@@ -15,9 +15,47 @@ int main()
     std::cin >> line;
     while (!std::cin.eof() && isdigit(line[0]))
     {
-      pair.second.push_back(line[0]);
+      pair.second.push_back(strtoull(line.c_str(), nullptr, 0));
       std::cin >> line;
+    }
+    if (maxSizeOfNumList < pair.second.size())
+    {
+      maxSizeOfNumList = pair.second.size();
     }
     listOfPairs.push_back(pair);
   }
+
+  std::list< int > listOfLineSums;
+  for (auto it = listOfPairs.begin(); it != listOfPairs.end(); ++it)
+  {
+    int sumOfLine = 0;
+    for (auto iter = it->second.begin(); iter != it->second.end(); ++iter)
+    {
+      sumOfLine += *iter;
+    }
+    listOfLineSums.push_back(sumOfLine);
+  }
+  for (auto it = listOfPairs.begin(); it != --listOfPairs.end(); ++it)
+  {
+    std::cout << it->first << " ";
+  }
+  std::cout << (--listOfPairs.end())->first << "\n";
+
+
+  for (size_t i = 0; i < maxSizeOfNumList; i++)
+  {
+    for (auto it = listOfPairs.begin(); it != --(listOfPairs.end()); ++it)
+    {
+      auto current = std::next(it->second.begin(), i);
+      std::cout << *current << " ";
+    }
+    std::cout << *(std::next((--(listOfPairs.end()))->second.begin(), i)) << "\n";
+  }
+
+  for (auto it = listOfLineSums.begin(); it != --listOfLineSums.end(); ++it)
+  {
+    std::cout << *it << " ";
+  }
+  std::cout << *(--listOfLineSums.end()) << "\n";
 }
+
