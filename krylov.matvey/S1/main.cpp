478e9d943c4796f23c8fb38dfcf34e4df9bc60cc
root <root@kryloff.>
2025-02-21 07:52:58 +0300

    Input implementation with std::list

--- krylov.matvey/S1/main.cpp
+++ krylov.matvey/S1/main.cpp
@@ -1,2 +1,23 @@
+#include <iostream>
+#include <utility>
+#include <list>
+#include <string>
+
 int main()
-{ }
+{
+  std::list< std::pair< std::string, std::list< int > > > listOfPairs;
+  std::string line = "";
+  std::cin >> line;
+  while (!std::cin.eof())
+  {
+    std::pair< std::string, std::list< int > > pair;
+    pair.first = line;
+    std::cin >> line;
+    while (!std::cin.eof() && isdigit(line[0]))
+    {
+      pair.second.push_back(line[0]);
+      std::cin >> line;
+    }
+    listOfPairs.push_back(pair);
+  }
+}
