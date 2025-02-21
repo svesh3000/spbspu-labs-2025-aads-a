f95b5e1dd9a56c213238aaaad9599d8b3300421c
Danil3352 <danil.tkach2018@yandex.ru>
2025-02-22 01:27:34 +0300

    first input/output

--- tkach.danil/S1/main.cpp
+++ tkach.danil/S1/main.cpp
@@ -1,4 +1,34 @@
+#include <iostream>
+#include <forward_list>
+#include <string>
+#include <sstream>
+
 int main()
 {
+  std::forward_list< std::pair< std::string, std::forward_list < size_t > > > fd_pair_list;
+  std::string list_line = "";
+  auto it_fd = fd_pair_list.before_begin();
+  while(std::getline(std::cin, list_line))
+  {
+    std::istringstream iss(list_line);
+    std::string list_name = "";
+    iss >> list_name;
+    std::forward_list < size_t > temp_list;
+    size_t num = 0;
+    auto it = temp_list.before_begin();
+    while(iss >> num)
+    {
+      it = temp_list.insert_after(it, num);
+    }
+    it_fd = fd_pair_list.insert_after(it_fd, std::make_pair(list_name, temp_list));
+    std::cin.clear();
+  }
+  std::cout << fd_pair_list.begin()->first;
+  for (auto it = ++fd_pair_list.begin(); it != fd_pair_list.end(); ++it)
+  {
+    std::cout << " " << it->first;
+  }
+  std::cout << "\n";
 
+  return 0;
 }
