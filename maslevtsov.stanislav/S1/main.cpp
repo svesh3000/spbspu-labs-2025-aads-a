7e834ae594c73a5077163fad71593ebf38873814
jambii1 <s.maslevtsov@gmail.com>
2025-02-22 20:32:26 +0300

    Using std::list

--- maslevtsov.stanislav/S1/main.cpp
+++ maslevtsov.stanislav/S1/main.cpp
@@ -1,40 +1,84 @@
 #include <iostream>
-#include <forward_list>
+#include <list>
 #include <string>
 #include <utility>
+#include <limits>
 
 namespace {
-  using list_t = std::forward_list< unsigned long long >;
-  using pairs_list_t = std::forward_list< std::pair< std::string, list_t > >;
+  using list_t = std::list< unsigned long long >;
+  using pairs_list_t = std::list< std::pair< std::string, list_t > >;
 
-  // std::size_t get_max_pair_list_size(const pairs_list_t& list)
-  // {
-  //   std::size_t maximum = 0;
-  //   std::size_t size = 0;
-  //   for (auto i = list.begin(); i != list.end(); ++i) {
-  //     for (auto j = i->second.begin(); j != i->second.begin(); ++j) {
-  //       ++size;
-  //     }
-  //     maximum = maximum > size ? maximum : size;
-  //   }
-  //   return maximum;
-  // }
+  std::size_t get_max_pairs_list_size(const pairs_list_t& list) noexcept
+  {
+    std::size_t maximum = 0, size = 0;
+    for (auto i = list.begin(); i != list.end(); ++i) {
+      for (auto j = i->second.begin(); j != i->second.end(); ++j) {
+        ++size;
+      }
+      maximum = maximum > size ? maximum : size;
+    }
+    return maximum;
+  }
+
+  int safe_sum(unsigned long long a, unsigned long long b)
+  {
+    const unsigned long long max_ull = std::numeric_limits< unsigned long long >::max();
+    if (max_ull - a > b) {
+      return a + b;
+    }
+    throw std::overflow_error("overflow");
+  }
+
+  std::size_t get_sum_of_list_elements(const list_t& list)
+  {
+    std::size_t result = 0;
+    for (auto i = list.begin(); i != list.end(); ++i) {
+      result = safe_sum(result, *i);
+    }
+    return result;
+  }
+
+  void print_lists_info(std::ostream& out, const pairs_list_t& pairs_list)
+  {
+    pairs_list_t copy = pairs_list;
+    std::size_t max_pairs_list_size = get_max_pairs_list_size(pairs_list);
+    list_t sums;
+    for (std::size_t i = 0; i != max_pairs_list_size; ++i) {
+      list_t column_elements;
+      for (auto j = copy.begin(); j != copy.end(); ++j) {
+        if (!j->second.empty()) {
+          column_elements.push_back(j->second.front());
+          j->second.pop_front();
+        }
+      }
+      if (!column_elements.empty()) {
+        out << *column_elements.begin();
+        for (auto j = ++column_elements.begin(); j != column_elements.end(); ++j) {
+          out << ' ' << *j;
+        }
+        std::cout << '\n';
+        sums.push_back(get_sum_of_list_elements(column_elements));
+      }
+    }
+    out << *sums.begin();
+    for (auto j = ++sums.begin(); j != sums.end(); ++j) {
+      out << ' ' << *j;
+    }
+  }
 }
 
 int main()
 {
   pairs_list_t pairs_list;
   std::string list_name = "";
-  bool is_empty_input = true;
 
   while (std::cin >> list_name) {
     list_t list;
     unsigned long long num = 0;
     while (std::cin >> num) {
-      list.push_front(num);
-      is_empty_input = false;
+      list.push_back(num);
     }
-    pairs_list.push_front(std::make_pair(list_name, list));
+    pairs_list.push_back(std::make_pair(list_name, list));
     std::cin.clear();
   }
 
@@ -43,8 +87,15 @@ int main()
     std::cout << ' ' << i->first;
   }
   std::cout << '\n';
-  if (is_empty_input) {
+  if (get_max_pairs_list_size(pairs_list) == 0) {
     std::cout << 0 << '\n';
+  } else {
+    try {
+      print_lists_info(std::cout, pairs_list);
+    } catch (const std::overflow_error&) {
+      std::cerr << "Overflow error\n";
+      return 1;
+    }
   }
   std::cout << '\n';
 }
