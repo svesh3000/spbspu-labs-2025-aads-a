bc722a30563257b99aa99a9a4a8822999290fe97
MrMels625 <MrMels625@gmail.com>
2025-02-23 12:22:20 +0300

    List input & output with standard fwd list

--- aleksandrov.igor/S1/main.cpp
+++ aleksandrov.igor/S1/main.cpp
@@ -1,3 +1,35 @@
+#include <iostream>
+#include <string>
+#include <forward_list>
+#include <utility>
+
 int main()
-{}
+{
+  std::forward_list< std::pair< std::string, std::forward_list < int > > > list;
+  std::string listName;
+  auto listIter = list.before_begin();
+  while (std::cin >> listName)
+  {
+    std::forward_list< int > numList;
+    auto numListIter = numList.before_begin();
+    int num = 0;
+    while (std::cin >> num)
+    {
+      numList.insert_after(numListIter, num);
+      ++numListIter;
+    }
+    std::cin.clear();
+    list.insert_after(listIter, std::make_pair(listName, numList));
+    ++listIter;
+  }
+
+  for (auto it = list.begin(); it != list.end(); ++it)
+  {
+    std::cout << it->first << "\n";
+    for (auto it2 = it->second.begin(); it2 != it->second.end(); ++it2)
+    {
+      std::cout << *it2 << "\n";
+    }
+  }
+}
 
