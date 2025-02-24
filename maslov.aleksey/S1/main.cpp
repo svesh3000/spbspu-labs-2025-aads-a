359697c6c6966d6f3f29eef64e86d16de2b28e0a
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-02-24 10:49:00 +0300

    main input

--- maslov.aleksey/S1/main.cpp
+++ maslov.aleksey/S1/main.cpp
@@ -1,2 +1,20 @@
+#include <iostream>
+#include <forward_list>
+#include <string>
+#include "list.hpp"
+
 int main()
-{}
+{
+  maslov::List< std::pair< std::string, std::forward_list< int > > > pair;
+  std::string name;
+  int number = 0;
+  while (std::cin >> name && !std::cin.eof())
+  {
+    std::forward_list<int> numbers;
+    while (std::cin >> number)
+    {
+      numbers.push_front(number);
+    }
+    pair.push_back(std::make_pair(name, numbers));
+  }
+}
