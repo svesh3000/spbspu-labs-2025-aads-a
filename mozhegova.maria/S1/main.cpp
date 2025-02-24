900288965f24ad322a32587aa31fd772a3308586
mipxe <mariamozegova8726@gmail.com>
2025-02-24 21:28:25 +0300

    add input

--- mozhegova.maria/S1/main.cpp
+++ mozhegova.maria/S1/main.cpp
@@ -1,2 +1,20 @@
+#include <iostream>
+#include <string>
+#include <list>
+
 int main()
-{}
+{
+  std::list< std::pair< std::string, std::list< unsigned long long > > > listOfPairs;
+  std::string nameList;
+  while (std::cin >> nameList)
+  {
+    std::list< unsigned long long > numList;
+    unsigned long long num = 0;
+    while (std::cin >> num)
+    {
+      numList.push_back(num);
+    }
+    listOfPairs.push_back(std::make_pair(nameList, numList));
+    std::cin.clear();
+  }
+}
