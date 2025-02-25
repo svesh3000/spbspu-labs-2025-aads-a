b56b421ef224919d587f1f6a7ce01c8771e3f59d
“Karnaukhova <masj280906@gmail.com>
2025-02-25 20:14:13 +0300

    Fix cg

--- karnauhova.alexandra/S1/output.cpp
+++ karnauhova.alexandra/S1/output.cpp
@@ -32,10 +32,10 @@ void karnauhova::output_element_lists(std::forward_list<int> lists, size_t index
   out << *it;
 }
 
-void karnauhova::output_elements_lists(std::forward_list<std::pair<std::string, std::forward_list<int>>> lists, std::ostream& out)
+void karnauhova::output_elements_lists(std::forward_list<std::pair<std::string, std::forward_list<int>>> l, std::ostream& out)
 {
   int max_length = 0;
-  for (const auto& it : lists)
+  for (const auto& it : l)
   {
     if (std::distance((it.second).begin(), (it.second).end()) > max_length)
     {
@@ -45,7 +45,7 @@ void karnauhova::output_elements_lists(std::forward_list<std::pair<std::string, 
   for (int i = 0; i < max_length; ++i)
   {
     bool first = true;
-    for (const auto& it : lists) 
+    for (const auto& it : l)
     {
       output_element_lists(it.second, (i + 1), out, first);
     }
