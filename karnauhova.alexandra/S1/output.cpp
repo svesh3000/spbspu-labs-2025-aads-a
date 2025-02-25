bfac9881712c1074d5565fc26000ab21ba959650
“Karnaukhova <masj280906@gmail.com>
2025-02-25 20:20:18 +0300

    Fix meow

--- karnauhova.alexandra/S1/output.cpp
+++ karnauhova.alexandra/S1/output.cpp
@@ -1,10 +1,10 @@
 #include "output.hpp"
-void karnauhova::output_names_lists(std::forward_list<std::pair<std::string, std::forward_list<int>>> lists, std::ostream& out)
+void karnauhova::output_names_lists(std::forward_list<std::pair<std::string, std::forward_list<int>>> l, std::ostream& out)
 {
-  auto it = lists.begin();
+  auto it = l.begin();
   out << it->first;
   it++;
-  for (; it != lists.end(); it++)
+  for (; it != l.end(); it++)
   {
     out << " " << it->first;
   }
--- karnauhova.alexandra/S1/output.hpp
+++ karnauhova.alexandra/S1/output.hpp
@@ -6,7 +6,7 @@
 
 namespace karnauhova
 {
-  void output_names_lists(std::forward_list<std::pair<std::string, std::forward_list<int>>> lists, std::ostream& out);
+  void output_names_lists(std::forward_list<std::pair<std::string, std::forward_list<int>>> l, std::ostream& out);
   void output_element_lists(std::forward_list<int> lists, size_t index, std::ostream& out, bool& first);
   void output_elements_lists(std::forward_list<std::pair<std::string, std::forward_list<int>>> l, std::ostream& out);
 }
