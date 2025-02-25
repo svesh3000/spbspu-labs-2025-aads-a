c1fdf70e92ad3c175b0c3276a5166ad259d210ea
mipxe <mariamozegova8726@gmail.com>
2025-02-25 18:29:41 +0300

    create const iterator

---
+++ mozhegova.maria/S1/constIterator.hpp
@@ -0,0 +1,97 @@
+#ifndef CONSTITERATOR_HPP
+#define CONSTITERATOR_HPP
+
+#include <cassert>
+#include "node.hpp"
+
+namespace mozhegova
+{
+  template< typename T >
+  struct ConstIterator:
+    public std::iterator< std::bidirectional_iterator_tag, T >
+  {
+  public:
+    Iterator(): node_(nullptr) {};
+    Iterator(const ConstIterator< T > &) = default;
+    Iterator(Node< T > * node): node_(node) {}
+    ~Iterator() = default;
+    ConstIterator< T > & operator=(const ConstIterator< T > &) = default;
+
+    ConstIterator< T > & operator++();
+    ConstIterator< T > operator++(int);
+
+    ConstIterator< T > & operator--();
+    ConstIterator< T > operator--(int);
+
+    T & operator*() const;
+    T * operator->() const;
+
+    bool operator==(const ConstIterator< T > &) const;
+    bool operator!=(const ConstIterator< T > &) const;
+  private:
+    Node< T > * node_;
+  };
+  
+  template< typename T >
+  ConstIterator< T > & ConstIterator< T >::operator++()
+  {
+    assert(node_ != nullptr);
+    node_ = node_->next_;
+    return *this;
+  }
+
+  template< typename T >
+  ConstIterator< T > ConstIterator< T >::operator++(int)
+  {
+    assert(node_ != nullptr);
+    ConstIterator< T > result(*this);
+    ++(*this);
+    return result;
+  }
+
+  template< typename T >
+  ConstIterator< T > & ConstIterator< T >::operator--()
+  {
+    assert(node_ != nullptr);
+    node_ = node_->prev_;
+    return *this;
+  }
+
+  template< typename T >
+  ConstIterator< T > ConstIterator< T >::operator--(int)
+  {
+    assert(node_ != nullptr);
+    ConstIterator< T > result(*this);
+    --(*this);
+    return result;
+  }
+
+  template< typename T >
+  T & ConstIterator< T >::operator*() const
+  {
+    assert(node_ != nullptr);
+    return node_->data_;
+  }
+
+  template< typename T >
+  T * ConstIterator< T >::operator->() const
+  {
+    assert(node_ != nullptr);
+    return std::addressof(node_->data_);
+  }
+
+  template< typename T >
+  bool ConstIterator< T >::operator==(const ConstIterator< T > & rhs) const
+  {
+    return node_ == rhs.node_;
+  }
+
+  template< typename T >
+  bool ConstIterator< T >::operator!=(const ConstIterator< T > & rhs) const
+  {
+    return !(rhs == *this);
+  }
+}
+
+
+#endif
