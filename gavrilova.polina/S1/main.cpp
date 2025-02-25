2940ed35929fbfea4d3e8dcedf64309cc895e07c
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2025-02-25 23:46:19 +0300

    Realizing main withoot implementation functions

--- gavrilova.polina/S1/main.cpp
+++ gavrilova.polina/S1/main.cpp
@@ -1,1 +1,8 @@
-int main() {}
+#include <forward_list>
+#include <string>
+int main() {
+  std::forward_list< std::pair< std::string, std::forward_list< int > > >* listOfPairs = nullptr;
+  size_t numOfPairs = 0;
+  std::string name;
+  
+}
