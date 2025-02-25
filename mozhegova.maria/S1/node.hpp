3eeddb7bc89ffab31b7b5558f0ea3d9d915e5848
mipxe <mariamozegova8726@gmail.com>
2025-02-25 14:30:55 +0300

    create node.hpp

---
+++ mozhegova.maria/S1/node.hpp
@@ -0,0 +1,27 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+namespace mozhegova
+{
+  template< typename T >
+  struct Node
+  {
+    T data_;
+    Node< T > * prev_;
+    Node< T > * next_;
+
+    Node(const T & data):
+      data_(data),
+      prev_(nullptr),
+      next_(nullptr)
+    {}
+
+    Node():
+      data_(T()),
+      prev_(nullptr),
+      next_(nullptr)
+    {}
+  };
+}
+
+#endif
