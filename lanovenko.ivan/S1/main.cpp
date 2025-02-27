1dc8b5e9375573bf8cec205215e99a6256575349
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-02-27 19:21:04 +0300

    input output realized

--- lanovenko.ivan/S1/main.cpp
+++ lanovenko.ivan/S1/main.cpp
@@ -1,2 +1,90 @@
+#include <iostream>
+#include <utility>
+#include <list>
+#include <iomanip>
+
+void printSequencesName(const std::list<std::pair<std::string, std::list<int>>> temporary)
+{
+  for (const auto& it: temporary)
+  {
+    std::cout << it.first << ' ';
+  }
+  std::cout << '\n';
+}
+
+void printSequences(const std::list < std::pair < std::string, std::list<int>>> temporary, int* array, size_t maxSize)
+{
+  for (size_t i = 0; i < maxSize; i++)
+  {
+    size_t res = 0;
+    for (const auto& pair : temporary)
+    {
+      if (pair.second.size() != 0 )
+      {
+        auto it = pair.second.begin();
+        if (i < pair.second.size())
+        {
+          std::advance(it, i);
+          std::cout << *(it) << ' ';
+          res += *(it);
+        }
+      }
+      array[i] = res;
+    }
+    std::cout << '\n';
+  }
+}
+
+void printSequencesSum(const int* sumArray, size_t maxSize)
+{
+  std::cout << sumArray[0];
+  for (size_t i = 1; i < maxSize; i++)
+  {
+    std::cout << ' ' << sumArray[i];
+  }
+  std::cout << '\n';
+}
+
 int main()
-{}
+{
+  std::list < std::pair<std::string, std::list<int>>> myList;
+  std::string sequenceName = "";
+  while (std::cin >> sequenceName)
+  {
+    int element = 0;
+    std::list<int> temporaryList{};
+    while (std::cin >> element)
+    {
+      temporaryList.push_back(element);
+    }
+    std::pair<std::string, std::list<int>> temporaryPair{ sequenceName, temporaryList };
+    myList.push_back(temporaryPair);
+    if (std::cin.eof())
+    {
+      break;
+    }
+    std::cin.clear();
+  }
+  printSequencesName(myList);
+  size_t maxSize = 0;
+  int* sumArray = nullptr;
+  for (const auto& i : myList)
+  {
+    if (maxSize < i.second.size())
+    {
+      maxSize = i.second.size();
+    }
+  }
+  try
+  {
+    sumArray = new int[maxSize] {};
+  }
+  catch (const std::bad_alloc& e)
+  {
+    std::cerr << "Out of memmory\n";
+    return 1;
+  }
+  printSequences(myList, sumArray, maxSize);
+  printSequencesSum(sumArray, maxSize);
+  delete[] sumArray;
+}
