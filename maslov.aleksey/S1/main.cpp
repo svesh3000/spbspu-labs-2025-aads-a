b81f6a06d1ee848c43e7bab4a33bd1062ebd2c4e
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-02-27 09:36:05 +0300

    add reverse

--- maslov.aleksey/S1/main.cpp
+++ maslov.aleksey/S1/main.cpp
@@ -10,21 +10,21 @@ int main()
   std::string name;
   size_t number = 0;
   size_t maxSize = 0;
-  auto it = listOfPairs.before_begin();
   while (std::cin >> name && !std::cin.eof())
   {
     size_t size = 0;
     std::forward_list< size_t > numbers;
-    auto itNum = numbers.before_begin();
     while (std::cin >> number && !std::cin.eof())
     {
-      itNum = numbers.insert_after(itNum, number);
+      numbers.push_front(number);
       size++;
     }
+    numbers.reverse();
     maxSize = std::max(maxSize, size);
-    it = listOfPairs.insert_after(it, std::make_pair(name, numbers));
+    listOfPairs.push_front(std::make_pair(name, numbers));
     std::cin.clear();
   }
+  listOfPairs.reverse();
   if (listOfPairs.empty())
   {
     std::cout << "0\n";
