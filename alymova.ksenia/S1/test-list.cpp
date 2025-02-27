b94c9a3498ca1ccec5a8e0eef9477d64009f6a74
Ksenia-rgb <ksenialymova@mail.ru>
2025-02-28 00:04:59 +0300

    fix test

--- alymova.ksenia/S1/test-list.cpp
+++ alymova.ksenia/S1/test-list.cpp
@@ -9,8 +9,9 @@ BOOST_AUTO_TEST_CASE(test_constructors_operators)
 {
   list_t list1 = {3, 1};
   list_t list2(list1);
-  list_t list3(std::move(list_t{3, 1}));
   BOOST_TEST(list1 == list2);
+
+  list_t list3(std::move(list_t{3, 1}));
   BOOST_TEST(list1 == list3);
 
   list_t list4 = {5, 2};
@@ -19,4 +20,12 @@ BOOST_AUTO_TEST_CASE(test_constructors_operators)
 
   list4 = list_t{3, 1};
   BOOST_TEST(list4 == list1);
+}
+BOOST_AUTO_TEST_CASE(test_list_iterators)
+{
+  list_t list = {3, 1};
+  iter_t iter_b = list.begin();
+  iter_t iter_e = list.end();
+  BOOST_TEST(*iter_b == 1);
+  BOOST_TEST(*iter_e-- == 0);
 }
