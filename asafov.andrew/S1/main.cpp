6ce0fe3d554b1458f3a81037f267e619f5bb52c5
shycoshy <neforscist@gmail.com>
2025-02-27 09:40:28 +0000

    tests no.7

--- asafov.andrew/S1/main.cpp
+++ asafov.andrew/S1/main.cpp
@@ -2,23 +2,7 @@
 
 int main()
 {
-  //sequence_list_t sequences = asafov::getSequences();
-  int* a = new int;
-  *a = 1;
-  sequence_list_t sequences;
-  data_list_t data;
-  data.push_back(1);
-  data.push_back(2);
-  data.push_back(3);
-  sequences.push_back(std::make_pair("asd", data));
-  data_list_t data2;
-  data2.push_back(4);
-  data2.push_back(5);
-  sequences.push_back(std::make_pair("fgh", data2));
-  data_list_t data3;
-  data3.push_back(6);
-  sequences.push_back(std::make_pair("jkl", data3));
+  sequence_list_t sequences = asafov::getSequences();
   asafov::outputSequences(sequences);
-
   return 0;
 }
