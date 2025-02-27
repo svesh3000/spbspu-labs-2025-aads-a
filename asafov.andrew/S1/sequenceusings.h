11fa0e7970b0624eb62a7515e1ca988424baf77b
shycoshy <neforscist@gmail.com>
2025-02-27 09:42:31 +0000

    tests no.8

--- asafov.andrew/S1/sequenceusings.h
+++ asafov.andrew/S1/sequenceusings.h
@@ -1,11 +1,10 @@
 #ifndef LISTUSINGS_H
 #define LISTUSINGS_H
 #include <list>
-#include "fwdlst4.hpp"
 #include <utility>
 #include <string>
 using data_t = int;
-using data_list_t = asafov::Forward_list<data_t>;
+using data_list_t = std::list<data_t>;
 using sequence_t = std::pair<std::string, data_list_t>;
-using sequence_list_t = asafov::Forward_list<sequence_t>;
+using sequence_list_t = std::list<sequence_t>;
 #endif
