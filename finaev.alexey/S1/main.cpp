5b9cea0c2c51560d8b1459d216e5fc787f441608
AlexeyFinaev02 <finaev0212212@mail.ru>
2025-02-27 11:45:56 +0300

    first vers lab without tests and with std::list

--- finaev.alexey/S1/main.cpp
+++ finaev.alexey/S1/main.cpp
@@ -1,2 +1,97 @@
+#include <iostream>
+#include <string>
+#include <list>
+
+void printList(std::list<int> list)
+{
+  std::cout << *list.begin();
+  for (auto i = ++list.begin(); i != list.end(); ++i)
+  {
+    std::cout << " " <<  * i;
+  }
+  std::cout << "\n";
+}
+
+void printList(std::list<std::string> list)
+{
+  std::cout << *list.begin();
+  for (auto i = ++list.begin(); i != list.end(); ++i)
+  {
+    std::cout << " " << *i;
+  }
+  std::cout << "\n";
+}
+
 int main()
-{}
+{
+  std::list< std::pair<std::string, std::list<int>>> List;
+  std::string str = "";
+  while (std::cin >> str)
+  {
+    std::list< int > list;
+    int num = 0;
+    while (std::cin >> num)
+    {
+      list.push_back(num);
+    }
+    if (std::cin.fail() && !std::cin.eof())
+    {
+      std::cin.clear();
+    }
+    std::pair<std::string, std::list<int>> pair(str, list);
+    List.push_back(pair);
+    if (std::cin.eof())
+    {
+      break;
+    }
+  }
+  if (List.size() == 0)
+  {
+    std::cout << 0 << "\n";
+    return 0;
+  }
+  std::list<std::string> listOfHeads;
+  std::list<std::list<int>> listOfLists;
+  size_t max_size = 0;
+  for (auto i = List.begin(); i != List.end(); ++i)
+  {
+    if ((*i).second.size() > max_size)
+    {
+      max_size = (*i).second.size();
+    }
+  }
+  for (size_t i = 0; i < max_size; ++i)
+  {
+    std::list<int> list;
+    listOfLists.push_back(list);
+  }
+  for (auto i = List.begin(); i != List.end(); ++i)
+  {
+    listOfHeads.push_back((*i).first);
+    auto counter = listOfLists.begin();
+    for (auto j = (*i).second.begin(); j != (*i).second.end(); ++j)
+    {
+      (*counter).push_back(*j);
+      if (counter != listOfLists.end())
+      {
+        ++counter;
+      }
+    }
+  }
+  std::list<int> listOfSum;
+  for (auto i = listOfLists.begin(); i != listOfLists.end(); ++i)
+  {
+    int res = 0;
+    for (auto j = (*i).begin(); j != (*i).end(); ++j)
+    {
+      res += *j;
+    }
+    listOfSum.push_back(res);
+  }
+  printList(listOfHeads);
+  for (auto i = listOfLists.begin(); i != listOfLists.end(); ++i)
+  {
+    printList(*i);
+  }
+  printList(listOfSum);
+}
