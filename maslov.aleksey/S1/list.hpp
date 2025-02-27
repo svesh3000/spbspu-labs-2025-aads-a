854001cc5aff4e5be19c167d9a6afd6f19737ee9
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-02-27 13:48:51 +0300

    add FwdList clear, front empty size

--- maslov.aleksey/S1/list.hpp
+++ maslov.aleksey/S1/list.hpp
@@ -1,25 +1,71 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
+#include <cstddef>
+#include "node.hpp"
+#include "iterator.hpp"
+
 namespace maslov
 {
   template< typename T >
-  struct Iterator
+  struct FwdList
   {
-    T & operator*();
-    T * operator->();
-    Iterator & operator++();
-    Iterator operator++(int);
-    Iterator & operator--();
-    Iterator operator--(int);
-  };
-  template< typename T >
-  struct List
-  {
+    FwdList();
+    bool empty() const noexcept;
+    size_t max_size() const noexcept;
     void push_front(const T &);
-    Iterator< T > begin() const;
-    Iterator< T > end() const;
+    T & front() noexcept;
+    const T & front() const noexcept;
+    void swap(FwdList< T > &)
+    FwdListIterator< T > begin() const;
+    FwdListIterator< T > end() const;
+   private:
+    FwdListNode< T > * fake_;
+    size_t size_;
   };
-}
 
+  template< typename T >
+  FwdList< T >::FwdList():
+    fake_(new FwdListNode< T >{T(), nullptr}),
+    size_(0)
+  {
+    fake_->next = fake_;
+  }
+
+  template< typename T >
+  T & FwdList< T >::front() noexcept
+  {
+    return const_cast< T & >(static_cast< const FwdList & >(*this).front());
+  }
+
+  template< typename T >
+  const T & FwdList< T >::front() const noexcept
+  {
+    if (empty())
+    {
+        throw std::runtime_error("ERROR:: empty list");
+    }
+    return fake_->next->data;
+  }
+
+  template< typename T >
+  bool FwdList< T >::empty() const noexcept
+  {
+    return size_ == 0;
+  }
+
+  template< typename T >
+  size_t FwdList< T >::max_size() const noexcept
+  {
+    return size_;
+  }
+
+  template< typename T >
+  void FwdList< T >::swap(FwdList< T > & rhs) noexcept
+  {
+    std::swap(fake_, rhs.fake_);
+    std::swap(size_, rhs.size_);
+  }
+
+}
 #endif
