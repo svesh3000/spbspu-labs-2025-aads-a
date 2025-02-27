17869bd1cac9cde0e8f4428aa4043d0e1af40852
Z-1337 <un228der@gmail.com>
2025-02-27 23:29:29 +0300

    total rebuild of main

--- kushekbaev.danil/S1/main.cpp
+++ kushekbaev.danil/S1/main.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
-#include <list>
-#include <utility>
 #include <limits>
+#include "utilityFunctions.hpp"
 
-using pairedList = std::list< std::pair<std::string, std::list< int > > >;
+using pairedList = std::list< std::pair<std::string, std::list< unsigned long long > > >;
 
 int main()
 {
-  std::list< std::pair< std::string, std::list< int > > > pairsList;
+  pairedList pairsList;
   std::string nodeName = "";
   std::list< std::string > nodeList;
-  std::list< int > valueList;
-  std::list< int > sumList;
+  std::list< unsigned long long > valueList;
+  std::list< unsigned long long > sumList;
 
   while (!std::cin.eof())
   {
     while (std::cin >> nodeName)
     {
-      std::list< int > valueList;
-      int value = 0;
+      unsigned long long value = 0;
       while (std::cin >> value)
       {
         valueList.push_back(value);
@@ -26,65 +24,45 @@ int main()
       pairsList.push_back(std::make_pair(nodeName, valueList));
       std::cin.clear();
     }
+  }
 
-    if (pairsList.size() == 0)
-    {
-      std::cout << "0\n";
-      return 0;
-    }
+  if (pairsList.size() == 0)
+  {
+    std::cout << "0\n";
+    return 0;
+  }
 
-    std::cout << pairsList.begin()->first;
-    for (auto it = ++pairsList.begin(); it != pairsList.end(); ++it)
+  std::cout << pairsList.begin()->first;
+  for (auto it = ++pairsList.begin(); it != pairsList.end(); ++it)
+  {
+    std::cout << "_" << it->first;
+  }
+  std::cout << "\n";
+
+  size_t maxSize = kushekbaev::calcOfSize(pairsList);
+
+  for (size_t i = 0; i < maxSize; ++i)
+  {
+    valueList = kushekbaev::deletingSpaces(pairsList, valueList);
+
+    std::list< unsigned long long > outputList = valueList;
+    while (!outputList.empty())
     {
-      std::cout << " " << it->first;
+      std::cout << outputList.front();
+      if (!(outputList.size() == 1))
+      {
+        std::cout << "_";
+      }
+      outputList.pop_front();
     }
     std::cout << "\n";
 
-    size_t max_size = 0;
+    unsigned long long sum = kushekbaev::calcOfSum(valueList, std::cout);
+  }
 
-    for (auto it = ++pairsList.begin(); it != pairsList.end(); ++it)
-    {
-      max_size = std::max(max_size, it->second.size());
-    }
-
-    for (size_t i = 0; i < max_size; ++i)
-    {
-      std::list< int > valueList;
-      for (auto it = ++pairsList.begin(); it != pairsList.end(); ++it)
-      {
-        if (!it->second.empty())
-        {
-          valueList.push_back(it->second.front());
-          it->second.pop_front();
-        }
-      }
-
-      if (!valueList.empty())
-      {
-        std::cout << valueList.front();
-        for (auto it = ++valueList.begin(); it != valueList.end(); ++it)
-        {
-          std::cout << " " << *it;
-        }
-        std::cout << "\n";
-        int sum = 0;
-        for (auto it = ++valueList.begin(); it != valueList.end(); ++it)
-        {
-          if (sum > std::numeric_limits< int >::max() - *it)
-          {
-            std::cerr << "Overflow!\n";
-            return 1;
-          }
-          sum += *it;
-        }
-        valueList.push_back(sum);
-      }
-    }
-
-    std::cout << sumList.front();
-    for (auto it = ++valueList.begin(); it != valueList.end(); ++it)
-    {
-      std::cout << " " << *it;
-    }
+  std::cout << sumList.front();
+  for (auto it = ++valueList.begin(); it != valueList.end(); ++it)
+  {
+    std::cout << "_" << *it;
   }
 }
