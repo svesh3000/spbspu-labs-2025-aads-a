c226bfab2fdc6ac34d911d5234a4560f1405e63f
Ksenia-rgb <ksenialymova@mail.ru>
2025-02-27 19:54:10 +0300

    fix8

--- alymova.ksenia/S1/main.cpp
+++ alymova.ksenia/S1/main.cpp
@@ -13,19 +13,20 @@ int main()
   try
   {
     alymova::inputProcess(std::cin, list);
-    alymova::outputProcess(std::cout, list);
+    alymova::outputProcess(std::cout, list, overflow);
   }
   catch (const std::bad_alloc& e)
   {
     std::cerr << "Allocate error\n";
   }
-  if (overflow)
-  {
-    std::cerr << "Summation is incorrect\n";
-  }
   catch (const std::logic_error& e)
   {
     std::cerr << e.what() << "\n";
+    return 1;
+  }
+  if (overflow)
+  {
+    std::cerr << "Summation is incorrect\n";
     return 1;
   }
 }
