d92c2a153988548ca8eb3f6e92194c73cc06a467
ZakirovMS <mz.08.08.06@gmail.com>
2025-03-01 03:30:19 +0300

    Almost the correct output

--- zakirov.marat/S1/io_containers.cpp
+++ zakirov.marat/S1/io_containers.cpp
@@ -1,26 +1,29 @@
 #include "io_containers.hpp"
 #include <cstring>
+#include <iostream>
 
 void zakirov::get_list_pair(std::istream & in, list_pair & forward_list)
 {
   std::string sequence_name;
-  list_llint sequence_num = {};
-  list_pair::iterator fillable_iterator = forward_list.begin();
-  while (in >> sequence_name)
+  list_llint sequence_num;
+  list_pair::iterator fillable_iterator = forward_list.before_begin();
+  while (in >> sequence_name && !in.eof())
   {
     zakirov::get_list_llint(in, sequence_num);
     forward_list.insert_after(fillable_iterator, {sequence_name, sequence_num});
     ++fillable_iterator;
+    in.clear();
   }
 }
 
 void zakirov::get_list_llint(std::istream & in, list_llint & forward_list)
 {
   long long int sequence_element = 0;
-  list_llint::iterator fillable_iterator = forward_list.begin();
+  list_llint::iterator fillable_iterator = forward_list.before_begin();
   while (in >> sequence_element)
   {
-    fillable_iterator = forward_list.insert_after(fillable_iterator++, sequence_element);
+    forward_list.insert_after(fillable_iterator, sequence_element);
+    ++fillable_iterator;
   }
 }
 
@@ -54,11 +57,12 @@ void zakirov::output_result(std::ostream & out, list_pair & forward_list)
         continue;
       }
 
-      out << *((* big_iterator).second.begin());
+      out << *((* big_iterator).second.begin()) << " ";
       (* big_iterator).second.pop_front();
       ++big_iterator;
     }
 
+    out << '\n';
     size -= deleted;
   }
 }
