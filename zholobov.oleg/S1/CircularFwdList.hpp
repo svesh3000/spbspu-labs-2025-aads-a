16d9e87a185672f0449e93143e33c6c3b9d43259
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-03-01 22:55:12 +0300

    defined interface for CircularFwdList

--- zholobov.oleg/S1/CircularFwdList.hpp
+++ zholobov.oleg/S1/CircularFwdList.hpp
@@ -14,14 +14,39 @@ namespace zholobov {
   template < typename T >
   class CircularFwdList {
   public:
+    using value_type = T;
+    using reference = value_type&;
+    using const_reference = const reference;
+    using iterator = CircularFwdListIterator< T >;
+    using const_iterator = CircularFwdListConstIterator< T >;
+
     CircularFwdList():
       head_(nullptr), tail_(nullptr), size_(0) {};
     ~CircularFwdList();
     CircularFwdList(const CircularFwdList& other);
     CircularFwdList(CircularFwdList&& other) noexcept;
 
-    friend class CircularFwdListIterator< T >;
-    friend class CircularFwdListConstIterator< T >;
+    iterator begin() noexcept;
+    const_iterator begin() const noexcept;
+    iterator end() noexcept;
+    iterator end() const noexcept;
+
+    reference front();
+    const_reference front() const;
+    reference back();
+    const_reference back() const;
+    bool empty() const noexcept;
+    size_t size() const;
+    void push_front(const value_type& val);
+    void push_front(value_type&& val);
+    void push_back(const value_type& val);
+    void push_back(value_type&& val);
+
+    void pop_front();
+    void pop_back();
+
+    void swap(CircularFwdList& other) noexcept;
+    void clear() noexcept;
 
   private:
     struct Node_t {
@@ -32,6 +57,9 @@ namespace zholobov {
     Node_t* head_;
     Node_t* tail_;
     size_t size_;
+
+    friend class CircularFwdListIterator< T >;
+    friend class CircularFwdListConstIterator< T >;
   };
 }
 
