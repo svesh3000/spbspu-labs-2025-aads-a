d25025b1b473600a2531a43e3ddcf8068de4057c
A1exCRE <alsmmm2065@gmail.com>
2025-03-01 20:52:25 +0300

    fix list

--- smirnov.alexandr/S1/list.hpp
+++ smirnov.alexandr/S1/list.hpp
@@ -6,13 +6,43 @@ template< typename T >
 class List
 {
 public:
-  List();
+  List() = default;
   void pushBack(const T & value);
   bool isEmpty() const;
   size_t getSize() const;
-  using ConstIterator = typename std::list<T>::const_iterator;
+  using ConstIterator = typename std::list< T >::const_iterator;
   ConstIterator begin() const;
   ConstIterator end() const;
 private:
-  std::list<T> data;
+  std::list< T > data;
 };
+
+template< typename T >
+void List< T >::pushBack(const T & value)
+{
+  data.push_back(value);
+}
+
+template< typename T >
+bool List< T >::isEmpty() const
+{
+  return data.empty();
+}
+
+template< typename T >
+size_t List< T >::getSize() const
+{
+  return data.size();
+}
+
+template< typename T >
+typename List< T >::ConstIterator List< T >::begin() const
+{
+  return data.begin();
+}
+
+template< typename T >
+typename List< T >::ConstIterator List< T >::end() const
+{
+  return data.end();
+}
