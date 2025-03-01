7dc1ef4c22381752114dfd32da52fe1d5a20ef2b
A1exCRE <alsmmm2065@gmail.com>
2025-03-01 21:22:43 +0300

    reading the input data

--- smirnov.alexandr/S1/list.hpp
+++ smirnov.alexandr/S1/list.hpp
@@ -1,48 +1,53 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 #include <list>
+#include <cstddef>
 
-template< typename T >
-class List
+namespace smirnov
 {
-public:
-  List() = default;
-  void pushBack(const T & value);
-  bool isEmpty() const;
-  size_t getSize() const;
-  using ConstIterator = typename std::list< T >::const_iterator;
-  ConstIterator begin() const;
-  ConstIterator end() const;
-private:
-  std::list< T > data;
-};
+  template< typename T >
+  class List
+  {
+  public:
+    List() = default;
+    void pushBack(const T & value);
+    bool isEmpty() const;
+    size_t getSize() const;
+    using ConstIterator = typename std::list< T >::const_iterator;
+    ConstIterator begin() const;
+    ConstIterator end() const;
+  private:
+    std::list< T > data;
+  };
+}
 
 template< typename T >
-void List< T >::pushBack(const T & value)
+void smirnov::List< T >::pushBack(const T & value)
 {
   data.push_back(value);
 }
 
 template< typename T >
-bool List< T >::isEmpty() const
+bool smirnov::List< T >::isEmpty() const
 {
   return data.empty();
 }
 
 template< typename T >
-size_t List< T >::getSize() const
+size_t smirnov::List< T >::getSize() const
 {
   return data.size();
 }
 
 template< typename T >
-typename List< T >::ConstIterator List< T >::begin() const
+typename smirnov::List< T >::ConstIterator smirnov::List< T >::begin() const
 {
   return data.begin();
 }
 
 template< typename T >
-typename List< T >::ConstIterator List< T >::end() const
+typename smirnov::List< T >::ConstIterator smirnov::List< T >::end() const
 {
   return data.end();
 }
+#endif
--- smirnov.alexandr/S1/main.cpp
+++ smirnov.alexandr/S1/main.cpp
@@ -1,2 +1,30 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include "list.hpp"
+
 int main()
-{}
+{
+  using namespace smirnov;
+  std::list< std::pair< std::string, List< size_t > > > sequences;
+  std::string input;
+  std::string name;
+  List< size_t > list;
+  while (std::cin >> input)
+  {
+    if (isdigit(input[0]))
+    {
+      int number = std::stoi(input);
+      list.pushBack(number);
+    }
+    else
+    {
+      if (!name.empty())
+      {
+        sequences.emplace_back(name, list);
+        list = List< size_t >();
+      }
+      name = input;
+    }
+  }
+}
