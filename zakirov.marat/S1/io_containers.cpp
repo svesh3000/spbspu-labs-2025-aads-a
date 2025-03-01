f3bb154d9ae9b2cc7a9a9c360d57a42956b860fc
ZakirovMS <mz.08.08.06@gmail.com>
2025-03-02 00:26:15 +0300

    Continuing to write the output logic, adding the following pointers

--- zakirov.marat/S1/io_containers.cpp
+++ zakirov.marat/S1/io_containers.cpp
@@ -5,21 +5,21 @@
 void zakirov::get_list_pair(std::istream & in, list_pair & forward_list)
 {
   std::string sequence_name;
-  list_llint sequence_num;
+  list_ull sequence_num;
   list_pair::iterator fillable_iterator = forward_list.before_begin();
   while (in >> sequence_name && !in.eof())
   {
-    zakirov::get_list_llint(in, sequence_num);
+    zakirov::get_list_ull(in, sequence_num);
     forward_list.insert_after(fillable_iterator, {sequence_name, sequence_num});
     ++fillable_iterator;
     in.clear();
   }
 }
 
-void zakirov::get_list_llint(std::istream & in, list_llint & forward_list)
+void zakirov::get_list_ull(std::istream & in, list_ull & forward_list)
 {
   unsigned long long int sequence_element = 0;
-  list_llint::iterator fillable_iterator = forward_list.before_begin();
+  list_ull::iterator fillable_iterator = forward_list.before_begin();
   while (in >> sequence_element)
   {
     forward_list.insert_after(fillable_iterator, sequence_element);
@@ -29,14 +29,14 @@ void zakirov::get_list_llint(std::istream & in, list_llint & forward_list)
 
 void zakirov::output_result(std::ostream & out, list_pair & forward_list)
 {
-  // size_t size = 0;
-
-  list_pair::iterator big_iter = forward_list.begin();
   list_iter list_iterators;
+  list_pair list_temp(forward_list);
+  list_pair::iterator big_iter = list_temp.begin();
+  list_pair::iterator pre_big_iter;
   list_iter::iterator r_iter_list = list_iterators.before_begin();
   list_iter::iterator pre_iter_list;
 
-  while (big_iter != forward_list.end())
+  while (big_iter != list_temp.end())
   {
     list_iterators.insert_after(r_iter_list, (* big_iter).second.begin());
     ++r_iter_list;
@@ -47,24 +47,40 @@ void zakirov::output_result(std::ostream & out, list_pair & forward_list)
   {
     r_iter_list = list_iterators.begin();
     pre_iter_list = list_iterators.before_begin();
-    big_iter = forward_list.begin();
+    big_iter = list_temp.begin();
+    pre_big_iter = list_temp.before_begin();
 
     while (r_iter_list != list_iterators.end())
     {
       if ((* r_iter_list) == (* big_iter).second.end())
       {
         ++r_iter_list;
+        ++big_iter;
         list_iterators.erase_after(pre_iter_list);
+        list_temp.erase_after(pre_big_iter);
         continue;
       }
 
+      ++r_iter_list;
+      ++big_iter;
+      ++pre_iter_list;
+      ++pre_big_iter;
+    }
+
+    r_iter_list = list_iterators.begin();
+    big_iter = list_temp.begin();
+    if (list_temp.empty())
+    {
+      break;
+    }
+    
+    while (r_iter_list != list_iterators.end())
+    {
       out << ** r_iter_list << ' ';
       ++(* r_iter_list);
       ++r_iter_list;
-      ++pre_iter_list;
       ++big_iter;
     }
-
     out << '\n';
   }
 }
--- zakirov.marat/S1/io_containers.hpp
+++ zakirov.marat/S1/io_containers.hpp
@@ -6,14 +6,14 @@
 #include <iostream>
 
 using pair_strlist = std::pair< std::string, std::forward_list< unsigned long long > >;
-using list_llint = std::forward_list< unsigned long long >;
+using list_ull = std::forward_list< unsigned long long >;
 using list_pair = std::forward_list< std::pair< std::string, std::forward_list< unsigned long long> > >;
-using list_iter = std::forward_list< list_llint::iterator >;
+using list_iter = std::forward_list< list_ull::iterator >;
 
 namespace zakirov
 {
   void get_list_pair(std::istream & in, list_pair & forward_list);
-  void get_list_llint(std::istream & in, list_llint & forward_list);
+  void get_list_ull(std::istream & in, list_ull & forward_list);
   void output_result(std::ostream & out, list_pair & forward_list);
 }
 
