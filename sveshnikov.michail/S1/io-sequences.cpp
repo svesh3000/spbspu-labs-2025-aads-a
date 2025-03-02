8416da929c7c6eb72901c3ef52a2675511887f9c
svesh3000 <russkij2006@mail.ru>
2025-03-02 15:57:33 +0300

    Reverse lists and rewrite output from a recursive way to iterative

--- sveshnikov.michail/S1/io-sequences.cpp
+++ sveshnikov.michail/S1/io-sequences.cpp
@@ -4,22 +4,23 @@
 
 namespace
 {
-  using iterator_t = sveshnikov::list_pair_t::const_iterator;
-  void inputOneList(std::istream &in, std::forward_list< unsigned long long > &data);
-  std::ostream &outputNamesRecursive(std::ostream &out, iterator_t element, iterator_t end);
+  using iter_t = sveshnikov::list_pair_t::const_iterator;
+  void inputOneList(std::istream &in, std::forward_list<unsigned long long> &data);
 
-  void inputOneList(std::istream &in, std::forward_list< unsigned long long > &data)
+  void inputOneList(std::istream &in, std::forward_list<unsigned long long> &data)
   {
+    using iter_list_ull_t = sveshnikov::list_ull_t::const_iterator;
     unsigned long long num = 0;
+    std::forward_list<unsigned long long> data_copied = data;
     while (in >> num)
     {
       try
       {
-        data.push_front(num);
+        data_copied.push_front(num);
       }
       catch (std::bad_alloc &e)
       {
-        data.clear();
+        data_copied.clear();
         throw;
       }
     }
@@ -27,40 +28,39 @@ namespace
     {
       in.clear();
     }
-  }
-
-  std::ostream &outputNamesRecursive(std::ostream &out, iterator_t element, iterator_t end)
-  {
-    iterator_t element_copied = element;
-    if (++element == end)
+    for (iter_list_ull_t it = data_copied.cbegin(); it != data_copied.cend(); it++)
     {
-      out << element_copied->first;
-      return out;
+      data.push_front(*it);
     }
-    element = element_copied;
-    outputNamesRecursive(out, ++element, end);
-    out << " " << element_copied->first;
-    return out;
   }
 }
 
 void sveshnikov::inputLists(std::istream &in, list_pair_t &list)
 {
   std::string name_sequence;
+  list_pair_t list_reverse;
   while (!in.eof())
   {
     std::cin >> name_sequence;
     list_ull_t data_sequence;
     inputOneList(std::cin, data_sequence);
     pair_t sequence = {name_sequence, data_sequence};
-    list.push_front(sequence);
+    list_reverse.push_front(sequence);
+  }
+  for (iter_t it = list_reverse.cbegin(); it != list_reverse.cend(); it++)
+  {
+    list.push_front(*it);
   }
 }
 
 std::ostream &sveshnikov::outputNamesLists(std::ostream &out, const list_pair_t &list)
 {
-  using iterator_t = sveshnikov::list_pair_t::const_iterator;
-  return outputNamesRecursive(out, list.cbegin(), list.cend());
+  out << list.cbegin()->first;
+  for (iter_t it = ++list.cbegin(); it != list.cend(); it++)
+  {
+    out << " " << it->first;
+  }
+  return out;
 }
 
 std::ostream &sveshnikov::outputNewLists(std::ostream &out, const list_pair_t &list)
