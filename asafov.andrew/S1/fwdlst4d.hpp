b1dfea649f5dccb05fc5ed5513abe9c4fd492cd5
shycoshy <neforscist@gmail.com>
2025-03-02 10:53:49 +0000

    tests no.23

--- asafov.andrew/S1/fwdlst4d.hpp
+++ asafov.andrew/S1/fwdlst4d.hpp
@@ -11,7 +11,7 @@ namespace asafov
     {
     public:
       T data_;
-      Node* next_;
+      Node* next_ = nullptr;
       Node() noexcept: data_(NULL), next_(nullptr) {}
       Node(const T& smh, Node* ptr) noexcept: data_(smh), next_(ptr) {}
       Node(const T& smh) noexcept: data_(smh), next_(nullptr) {}
