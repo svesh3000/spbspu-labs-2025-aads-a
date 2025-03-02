286a891d8368f9b56f8f0a0156bc771fb53167da
svesh3000 <russkij2006@mail.ru>
2025-03-02 20:12:01 +0300

    Output lists data function and some fixes in other io functions

--- sveshnikov.michail/S1/io-sequences.cpp
+++ sveshnikov.michail/S1/io-sequences.cpp
@@ -4,14 +4,15 @@
 
 namespace
 {
-  using iter_t = sveshnikov::list_pair_t::const_iterator;
-  void inputOneList(std::istream &in, std::forward_list<unsigned long long> &data);
+  void inputOneList(std::istream &in, list_ull_t &data);
+  std::ostream &outputOneList(std::ostream &out, const list_pair_t &list, size_t size);
+  size_t getSizeList(iter_t i);
+  size_t getNumNewLists(iter_t begin, iter_t end);
 
-  void inputOneList(std::istream &in, std::forward_list<unsigned long long> &data)
+  void inputOneList(std::istream &in, list_ull_t &data)
   {
-    using iter_list_ull_t = sveshnikov::list_ull_t::const_iterator;
     unsigned long long num = 0;
-    std::forward_list<unsigned long long> data_copied = data;
+    list_ull_t data_copied = data;
     while (in >> num)
     {
       try
@@ -33,6 +34,51 @@ namespace
       data.push_front(*it);
     }
   }
+
+  size_t getSizeList(iter_t i)
+  {
+    size_t len = 0;
+    for (iter_list_ull_t j = i->second.cbegin(); j != i->second.cend(); j++)
+    {
+      len++;
+    }
+    return len;
+  }
+
+  size_t getNumNewLists(iter_t begin, iter_t end)
+  {
+    size_t num_lines = 0;
+    for (iter_t i = begin; i != end; i++)
+    {
+      num_lines = std::max(num_lines, getSizeList(i));
+    }
+    return num_lines;
+  }
+
+  std::ostream &outputOneList(std::ostream &out, const list_pair_t &list, size_t size)
+  {
+    bool first = true;
+    for (iter_t it = list.cbegin(); it != list.cend(); it++)
+    {
+      if (size >= getSizeList(it))
+      {
+        continue;
+      }
+      iter_list_ull_t it_data = it->second.cbegin();
+      for (size_t i = 0; i < size; i++)
+      {
+        it_data++;
+      }
+      if (first)
+      {
+        first = false;
+        out << *it_data;
+        continue;
+      }
+      out << " " << *it_data;
+    }
+    return out;
+  }
 }
 
 void sveshnikov::inputLists(std::istream &in, list_pair_t &list)
@@ -41,9 +87,9 @@ void sveshnikov::inputLists(std::istream &in, list_pair_t &list)
   list_pair_t list_reverse;
   while (!in.eof())
   {
-    std::cin >> name_sequence;
+    in >> name_sequence;
     list_ull_t data_sequence;
-    inputOneList(std::cin, data_sequence);
+    inputOneList(in, data_sequence);
     pair_t sequence = {name_sequence, data_sequence};
     list_reverse.push_front(sequence);
   }
@@ -65,6 +111,14 @@ std::ostream &sveshnikov::outputNamesLists(std::ostream &out, const list_pair_t 
 
 std::ostream &sveshnikov::outputNewLists(std::ostream &out, const list_pair_t &list)
 {
+  size_t num_new_lists = getNumNewLists(list.cbegin(), list.cend());
+  size_t size_list = 0;
+  while (size_list + 1 != num_new_lists)
+  {
+    outputOneList(out, list, size_list) << '\n';
+    size_list++;
+  }
+  outputOneList(out, list, size_list);
   return out;
 }
 
--- sveshnikov.michail/S1/io-sequences.hpp
+++ sveshnikov.michail/S1/io-sequences.hpp
@@ -3,11 +3,15 @@
 #include <forward_list>
 #include <string>
 
+using list_ull_t = std::forward_list<unsigned long long>;
+using pair_t = std::pair<std::string, list_ull_t>;
+using list_pair_t = std::forward_list<pair_t>;
+using iter_list_ull_t = list_ull_t::const_iterator;
+using iter_t = list_pair_t::const_iterator;
+
+
 namespace sveshnikov
 {
-  using list_ull_t = std::forward_list< unsigned long long >;
-  using pair_t = std::pair< std::string, list_ull_t >;
-  using list_pair_t = std::forward_list< pair_t >;
   void inputLists(std::istream &in, list_pair_t &list);
   std::ostream &outputNamesLists(std::ostream &out, const list_pair_t &list);
   std::ostream &outputNewLists(std::ostream &out, const list_pair_t &list);
