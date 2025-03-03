f845d8884806d75aaaf74d7bda0568e671fe53d7
ZakirovMS <mz.08.08.06@gmail.com>
2025-03-04 01:00:08 +0300

    renaming files

--- zakirov.marat/S1/my_list.hpp
+++ zakirov.marat/S1/my_list.hpp
@@ -3,6 +3,17 @@
 #include "my_list_node.hpp"
 
 namespace zakirov
-{}
+{
+  template< typename T >
+  class List
+  {
+  public:
+    List();
+    ~List();
+  private:
+    MyListNode< T > fake_node_;
+    MyListNode< T > head_node_;
+  };
+}
 
 #endif
---
+++ zakirov.marat/S1/test-my_list.сpp
@@ -0,0 +1,7 @@
+#include <boost/test/unit_test.hpp>
+#include "my_list.hpp"
+
+BOOST_AUTO_TEST_CASE(my_list)
+{
+  BOOST_TEST(true);
+}
