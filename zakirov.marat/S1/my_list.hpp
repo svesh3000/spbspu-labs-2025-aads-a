36f9d2cb2724f6a1bfe595dd07229afaab12b4db
ZakirovMS <mz.08.08.06@gmail.com>
2025-03-04 00:52:57 +0300

    I removed start of implementation class

--- zakirov.marat/S1/my_list.hpp
+++ zakirov.marat/S1/my_list.hpp
@@ -3,18 +3,6 @@
 #include "my_list_node.hpp"
 
 namespace zakirov
-{
-  template< typename T >
-  class List
-  {
-  public:
-    List();
-    ~List();
-  private:
-    MyListNode< T > fake_node_;
-    MyListNode< T > head_node_;
-  };
-
-}
+{}
 
 #endif
