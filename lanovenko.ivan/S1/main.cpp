7b63f58a451a1df9e261ba4861d53832ee9fcd52
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-03-03 15:52:41 +0300

    used functions with namespace

--- lanovenko.ivan/S1/main.cpp
+++ lanovenko.ivan/S1/main.cpp
@@ -3,80 +3,8 @@
 #include <list>
 #include <limits>
 #include <iomanip>
-
-void printSequencesName(const std::list<std::pair<std::string, std::list<unsigned long long>>> temporary)
-{
-  if (temporary.empty())
-  {
-    return;
-  }
-  auto it = temporary.begin();
-  std::cout << (*it).first;
-  for (++it; it != temporary.end(); it++)
-  {
-    std::cout << " " << (*it).first;
-  }
-  std::cout << '\n';
-}
-
-bool isSumLimit(size_t a, size_t b)
-{
-  return (b > std::numeric_limits<int>::max() - a);
-}
-
-void printSumList(const std::list<int>& sumList, bool sumLimit)
-{
-  if (sumLimit)
-  {
-    return;
-  }
-  if (!sumList.empty())
-  {
-    auto it = sumList.begin();
-    std::cout << (*it);
-    for (++it; it != sumList.end(); it++)
-    {
-      std::cout << " " << (*it);
-    }
-  }
-  else
-  {
-    std::cout << 0;
-  }
-  std::cout << '\n';
-}
-
-void printSequences(const std::list < std::pair < std::string, std::list<unsigned long long>>> temporary, size_t maxSize, bool& sumLimit)
-{
-  std::list<int> sumList{};
-  for (size_t i = 0; i < maxSize; i++)
-  {
-    size_t res = 0;
-    bool isFirstElement = true;
-    for (const auto& pair : temporary)
-    {
-      if (!pair.second.empty() && i < pair.second.size())
-      {
-        auto it = pair.second.begin();
-        std::advance(it, i);
-        if (!isFirstElement)
-        {
-          std::cout << " ";
-        }
-        std::cout << (*it);
-        if (isSumLimit(res, *it))
-        {
-          sumLimit = true;
-        }
-        res += *(it);
-        isFirstElement = false;
-      }
-    }
-    std::cout << '\n';
-    sumList.push_back(res);
-  }
-  printSumList(sumList, sumLimit);
-}
+#include "list_manipulations.hpp"
+#include "forward_list.hpp"
 
 int main()
 {
@@ -98,7 +26,7 @@ int main()
     }
     std::cin.clear();
   }
-  printSequencesName(myList);
+  lanovenko::printSequencesName(myList, std::cout);
   size_t maxSize = 0;
   for (auto it : myList)
   {
@@ -108,7 +36,7 @@ int main()
     }
   }
   bool sumLimit = false;
-  printSequences(myList, maxSize, sumLimit);
+  lanovenko::printSequences(myList, maxSize, sumLimit, std::cout);
   if (sumLimit)
   {
     std::cerr << "Sum limit!\n";
