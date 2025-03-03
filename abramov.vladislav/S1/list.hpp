592e3926227e481cc8443eb0eeab2b376227f32e
15446-rus75 <vladabramov06@yandex.ru>
2025-03-03 17:32:42 +0300

    add find, find_if, remove, remove_if, splice

--- abramov.vladislav/S1/list.hpp
+++ abramov.vladislav/S1/list.hpp
@@ -36,6 +36,14 @@ namespace abramov
     ConstIterator< T > cbegin() const;
     ConstIterator< T > cend() const;
     void fill(Iterator< T > begin, Iterator< T > end, const T &val);
+    Iterator< T > find(Iterator< T > begin, Iterator< T > end, const T &val);
+    template< class C >
+    Iterator< T > find_if(Iterator< T > begin, Iterator< T > end, C condition);
+    Iterator< T > remove(Iterator< T > begin, Iterator< T > end, const T &val);
+    template< class C >
+    Iterator< T > remove_if(Iterator< T > begin, Iterator< T > end, C condition);
+    void splice(ConstIterator< T > pos, List< T > &other);
+    void splice(ConstIterator< T > pos, List< T > &&other);
   private:
     Node< T > *head_;
     Node< T > *tail_;
@@ -223,6 +231,93 @@ namespace abramov
       *begin = val;
       ++begin;
     }
+  }
+
+  template< class T >
+  Iterator< T > List< T >::find(Iterator< T > begin, Iterator< T > end, const T &val)
+  {
+    for (auto iter = begin; iter != end; ++iter)
+    {
+      if (*iter == val)
+      {
+        return iter;
+      }
+    }
+    return end;
+  }
+
+  template< class T >
+  template< class C >
+  Iterator< T > List< T >::find_if(Iterator< T > begin, Iterator< T > end, C condition)
+  {
+    for (auto iter = begin; iter != end; ++iter)
+    {
+      if (condition(*iter))
+      {
+        return iter;
+      }
+    }
+    return end;
+  }
+
+  template< class T >
+  Iterator< T > List< T >::remove(Iterator< T > begin, Iterator< T > end, const T &val)
+  {
+    auto iter = find(begin, end, val);
+    if (iter != end)
+    {
+      for (auto it = iter; ++it != end;)
+      {
+        if (*it != val)
+        {
+          *iter++ = std::move(*it);
+        }
+      }
+    }
+    return iter;
+  }
+
+  template< class T >
+  template< class C >
+  Iterator< T > List< T >::remove_if(Iterator< T > begin, Iterator< T > end, C condition)
+  {
+    auto iter = find_if(begin, end, condition);
+    if (iter != end)
+    {
+      for (auto it = iter; ++it != end;)
+      {
+        if (!condition(*it))
+        {
+          *iter++ = std::move(*it);
+        }
+      }
+    }
+    return iter;
+  }
+
+  template< class T >
+  void List< T >::splice(ConstIterator< T > pos, List< T > &other)
+  {
+    if (pos.node_ == begin().node_)
+    {
+      other.tail_->next_ = head_;
+      head_->prev_ = other.tail_;
+      head_ = other.head_;
+    }
+    pos.node_->prev_->next_ = other.head_;
+    other.head_->prev_ = pos.node_->prev_;
+    pos.node_->prev_ = other.tail_;
+    other.tail_->next_ = pos.node_;
+    other.head_ = nullptr;
+    other.tail_ = nullptr;
+    size_ += other.size_;
+    other.size_ = 0;
+  }
+
+  template< class T >
+  void List< T >::splice(ConstIterator< T > pos, List< T > &&other)
+  {
+    splice(pos, other);
   }
 }
 #endif
