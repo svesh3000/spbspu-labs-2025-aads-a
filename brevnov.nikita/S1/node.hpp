90b218f2b3e92989a2e0617d7f6bef5132a88256
brev1ch <flirktv@gmail.com>
2025-03-03 15:58:28 +0300

    node.hpp

---
+++ brevnov.nikita/S1/node.hpp
@@ -0,0 +1,23 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+namespace brevnov
+{
+  template< typename T>
+  struct Node
+  {
+    T data_;
+    Node< T > * next_;
+    Node< T > * prev_;
+    Node():
+      data_(T()),
+      next_(nullptr),
+      prev_(nullptr)
+    {}
+    Node(const T& data):
+      data_(data),
+      next_(nullptr),
+      prev_(nullptr)
+    {}
+  }
+}
+#endif
