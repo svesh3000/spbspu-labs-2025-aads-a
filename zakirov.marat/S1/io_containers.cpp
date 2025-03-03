7104a1ebf161a22e590eeb5e86bae2097fd3cb42
ZakirovMS <mz.08.08.06@gmail.com>
2025-03-03 18:46:48 +0300

    Fix of input, fix of output as a consequence

--- zakirov.marat/S1/io_containers.cpp
+++ zakirov.marat/S1/io_containers.cpp
@@ -5,10 +5,10 @@
 void zakirov::get_list_pair(std::istream & in, list_pair & forward_list)
 {
   std::string sequence_name;
-  list_ull sequence_num;
   list_pair::iterator fillable_iterator = forward_list.before_begin();
   while (in >> sequence_name && !in.eof())
   {
+    list_ull sequence_num;
     zakirov::get_list_ull(in, sequence_num);
     forward_list.insert_after(fillable_iterator, {sequence_name, sequence_num});
     ++fillable_iterator;
@@ -30,48 +30,47 @@ void zakirov::get_list_ull(std::istream & in, list_ull & forward_list)
 void zakirov::output_result(std::ostream & out, list_pair & forward_list)
 {
   list_iter list_iterators;
-  list_pair list_temp(forward_list);
-  list_pair::iterator big_iter = list_temp.begin();
-  list_pair::iterator pre_big_iter;
-  list_iter::iterator r_iter_list = list_iterators.before_begin();
-  list_iter::iterator pre_iter_list;
-
-  while (big_iter != list_temp.end())
+  list_pair::iterator pair_iter = forward_list.begin();
+  list_iter::iterator iter_list = list_iterators.before_begin();
+  size_t list_size = 0;
+  size_t deleted = 0;
+  while (pair_iter != forward_list.end())
   {
-    list_iterators.insert_after(r_iter_list, (* big_iter).second.begin());
-    ++r_iter_list;
-    ++big_iter;
+    list_iterators.insert_after(iter_list, (* pair_iter).second.begin());
+    ++iter_list;
+    ++pair_iter;
+    ++list_size;
   }
 
-  while (!list_iterators.empty())
+  list_pair::iterator output_iter = forward_list.begin();
+  out << (* output_iter).first;
+  ++output_iter;
+  for (output_iter; output_iter != forward_list.end(); ++output_iter)
   {
-    r_iter_list = list_iterators.begin();
-    pre_iter_list = list_iterators.before_begin();
-    big_iter = list_temp.begin();
-    pre_big_iter = list_temp.before_begin();
+    out << '_' << (* output_iter).first;
+  }
 
-    if (list_temp.empty())
-    {
-      break;
-    }
+  out << '\n';
+  while (deleted != list_size)
+  {
+    deleted = 0;
+    iter_list = list_iterators.begin();
+    pair_iter = forward_list.begin();
 
-    while (r_iter_list != list_iterators.end())
+    while (iter_list != list_iterators.end())
     {
-      if ((* r_iter_list) == (* big_iter).second.end())
+      if ((* iter_list) == (* pair_iter).second.end())
       {
-        ++r_iter_list;
-        ++big_iter;
-        list_iterators.erase_after(pre_iter_list);
-        list_temp.erase_after(pre_big_iter);
+        ++iter_list;
+        ++pair_iter;
+        ++deleted;
         continue;
       }
 
-      out << ** r_iter_list << ' ';
-      ++(* r_iter_list);
-      ++r_iter_list;
-      ++pre_iter_list;
-      ++big_iter;
-      ++pre_big_iter;
+      out << ** iter_list << '_';
+      ++(* iter_list);
+      ++iter_list;
+      ++pair_iter;
     }
 
     out << '\n';
