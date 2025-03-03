36870cc05493ae0765157d674bb7e476dba7ccfd
15446-rus75 <vladabramov06@yandex.ru>
2025-03-03 17:33:19 +0300

    add some tests for find, remove, splice

--- abramov.vladislav/S1/test-list.cpp
+++ abramov.vladislav/S1/test-list.cpp
@@ -106,3 +106,52 @@ BOOST_AUTO_TEST_CASE(fill)
   BOOST_TEST(*(++iter1) == 3);
   BOOST_TEST(list.back() == 3);
 }
+
+BOOST_AUTO_TEST_CASE(find)
+{
+  abramov::List< int > list;
+  list.pushBack(0);
+  list.pushBack(1);
+  list.pushBack(2);
+  list.pushBack(1);
+  auto it = list.find(list.begin(), list.end(), 1);
+  BOOST_TEST(*it == 1);
+}
+
+BOOST_AUTO_TEST_CASE(remove_test)
+{
+  abramov::List< int > list;
+  list.pushBack(0);
+  list.pushBack(1);
+  list.pushBack(1);
+  list.pushBack(2);
+  auto it = list.remove(list.begin(), list.end(), 1);
+  BOOST_TEST(*it == 1);
+}
+
+BOOST_AUTO_TEST_CASE(splice)
+{
+  abramov::List< int > list1;
+  list1.pushBack(3);
+  list1.pushBack(4);
+  list1.pushBack(5);
+  abramov::List< int > list2;
+  list2.pushBack(1);
+  list2.pushBack(2);
+  auto it = list1.cbegin();
+  list1.splice(it, list2);
+  it = list1.cbegin();
+  for (size_t i = 1; i < 6; ++i)
+  {
+    BOOST_TEST(*(it++) == i);
+  }
+  abramov::List< int > list3;
+  list3.pushBack(6);
+  list3.pushBack(7);
+  list1.splice(++list1.cbegin(), list3);
+  it = ++list1.cbegin();
+  for (size_t i = 6; i < 8; ++i)
+  {
+    BOOST_TEST(*(it++) == i);
+  }
+}
