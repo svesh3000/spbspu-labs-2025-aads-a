81875fc734b9f695f551d5093dc94a101f02c0b8
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2025-03-06 03:50:17 +0300

    Test FwdList

---
+++ gavrilova.polina/S1/test-FwdList.cpp
@@ -0,0 +1,31 @@
+#include <boost/test/unit_test.hpp>
+#include "FwdList.hpp"
+
+BOOST_AUTO_TEST_CASE(test_element_access)
+{
+  gavrilova::FwdList< int > list;
+  list.push_front(0);
+  list.push_front(1);
+  BOOST_TEST(list.front() == 1);
+}
+
+BOOST_AUTO_TEST_CASE(test_reverse)
+{
+  gavrilova::FwdList< int > list;
+  list.push_front(0);
+  list.push_front(1);
+  list.push_front(2);
+  list.reverse();
+  BOOST_TEST(list.front() == 2);
+}
+
+BOOST_AUTO_TEST_CASE(test_assignment_operator)
+{
+  gavrilova::FwdList< int > list1;
+  list1.push_front(0);
+  list1.push_front(1);
+  list1.push_front(2);
+  gavrilova::FwdList< int > list2;
+  BOOST_TEST(list1.size() == list2.size());
+  BOOST_TEST(list2.front() == list1.front());
+}
