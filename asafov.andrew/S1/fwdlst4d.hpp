28af70106eadae57ba3431d007aeda1a09cc0f9f
shycoshy <neforscist@gmail.com>
2025-03-09 17:05:25 +0000

    tests no.83

--- asafov.andrew/S1/fwdlst4d.hpp
+++ asafov.andrew/S1/fwdlst4d.hpp
@@ -85,22 +85,6 @@ namespace asafov
       return const_iterator(nullptr, last_);
     }
     void push_back(const T& value)
-    {
-      Node* new_node = new Node(value);
-      if (!head_)
-      {
-        head_ = new_node;
-        last_ = new_node;
-        new_node->next_ = head_;
-      }
-      else
-      {
-        last_->next_ = new_node;
-        new_node->next_ = head_;
-        last_ = new_node;
-      }
-    }
-    void push_back(const T& value)
 {
   Node* new_node = new Node(value);
   if (!head_)
