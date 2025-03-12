21ef4009945d9af8f002d5f039912395a99df1cb
shycoshy <neforscist@gmail.com>
2025-03-12 18:42:42 +0000

    tests no.103

--- asafov.andrew/S1/fl6a.hpp
+++ asafov.andrew/S1/fl6a.hpp
@@ -1,186 +1,138 @@
 #ifndef FL6A_HPP
 #define FL6A_HPP
 #include <memory>
-namespace asafov
+
+template <typename T>
+class forward_list
 {
-  template <typename T>
-  class Forward_list
+  struct Node
   {
-    struct Node
-    {
-      T data_;
-      Node* next_;
-
-      Node(const T& value, Node* ptr) noexcept:
-      data_(value),
-      next_(ptr)
-      {}
-
-      Node(const T& value) noexcept:
-      data_(value),
-      next_(nullptr)
-      {}
-    };
-  public:
-    Forward_list() noexcept:
-    head_(nullptr),
-    tail_(nullptr),
-    size_(0)
+    T data_;
+    Node* next_;
+    explicit Node(const T& value, Node* ptr = nullptr) noexcept:
+    data_(value),
+    next_(ptr)
     {}
+  };
+  Node* head_;
+  Node* tail_;
+  size_t size_;
+public:
+  forward_list() noexcept:
+  head_(nullptr),
+  tail_(nullptr)
+  {}
+  
+  class const_iterator
+  {
+    friend class Forward_list;
+  public:
+    const_iterator():
+    current_(nullptr),
+    last_(nullptr)
+    {}
+    const_iterator(Node* node, Node* last):
+    current_(node),
+    last_(last)
+    {}
+    const_iterator(const const_iterator& data) noexcept:
+    current_(data.current_),
+    last_(data.last_)
+    {}
+    ~const_iterator() = default;
 
-    ~Forward_list() noexcept
+    const T& operator*() const
     {
-      clear();
+      return current_->data_;
+    }
+      
+    T* operator->() const
+    {
+      return std::addressof(current_->data_);
     }
 
-    class const_iterator
+    const_iterator& operator++()
     {
-      friend class Forward_list;
-    public:
-      const_iterator():
-      current_(nullptr),
-      last_(nullptr)
-      {}
-
-      const_iterator(Node* node, Node* last):
-      current_(node),
-      last_(last)
-      {}
-
-      ~const_iterator() = default;
-
-      const T& operator*() const
+      if (current_ && current_ != last_)
       {
-        return current_->data_;
-      }
-
-      T* operator->() const
-      {
-        return std::addressof(current_->data_);
-      }
-
-      const_iterator& operator++()
-      {
-        if (current_ && current_ != last_)
-        {
-          current_ = current_->next_;
-        }
-        else
-        {
-          current_ = nullptr;
-        }
-        return *this;
-      }
-
-
-      bool operator==(const const_iterator& rhs) const
-      {
-        return current_ == rhs.current_;
-      }
-
-      bool operator!=(const const_iterator& rhs) const
-      {
-        return !(*this == rhs);
-      }
-
-    private:
-      Node* current_;
-      Node* last_;
-    };
-
-    const_iterator cbegin() const
-    {
-      return const_iterator(head_, tail_);
-    }
-    const_iterator cend() const
-    {
-      return const_iterator(nullptr, tail_);
-    }
-    const_iterator begin() const
-    {
-      return const_iterator(head_, tail_);
-    }
-    const_iterator end() const
-    {
-      return const_iterator(nullptr, tail_);
-    }
-
-    bool empty()
-    {
-      return !head_;
-    }
-
-    size_t size() const
-    {
-      size_t i = 0;
-      for (const_iterator it = cbegin(); it != cend(); ++it)
-      {
-        ++i;
-      }
-      return i;
-    }
-
-    void pop_front()
-    {
-      if (!head_) return;
-      if (head_ == tail_)
-      {
-        delete head_;
-        head_ = nullptr;
-        tail_ = nullptr;
+        current_ = current_->next_;
       }
       else
       {
-        Node* temp = head_;
-        head_ = head_->next_;
-        tail_->next_ = head_;
-        delete temp;
+        current_ = nullptr;
       }
-      --size_;
+      return *this;
     }
 
-
-    void clear()
+    bool operator==(const const_iterator& rhs) const
     {
-      if (!head_) return;
+      return current_ == rhs.current_;
+    }
 
-      while (head_ != tail_)
-      {
-        Node* temp = head_;
-        head_ = head_->next_;
-        delete temp;
-      }
+    bool operator!=(const const_iterator& rhs) const
+    {
+      return !(*this == rhs);
+    }
+  private:
+    Node* current_;
+    Node* last_;
+  };
 
-      delete tail_;
+  const_iterator cbegin() const
+  {
+    return const_iterator(head_, tail_);
+  }
+  const_iterator cend() const
+  {
+    return const_iterator(nullptr, tail_);
+  }
+  
+  bool empty() const
+  {
+    return !head_;
+  }
+  size_t size() const
+  {
+    return size_;
+  }
+  
+  void pop_front()
+  {
+    if (head_ == nullptr)
+    {
+      return;
+    }
+    Node* temp = head_;
+    if (head_ == tail_)
+    {
       head_ = nullptr;
       tail_ = nullptr;
-      size_ = 0;
     }
-
-
-    void push_back(const T& value)
+    else
     {
-      Node* new_node = new Node(value);
-
-      if (!head_)
-      {
-        head_ = new_node;
-        tail_ = new_node;
-        new_node->next_ = head_;
-      }
-      else
-      {
-        tail_->next_ = new_node;
-        new_node->next_ = head_;
-        tail_ = new_node;
-      }
-      ++size_;
+      head_ = head_->next_;
+      tail_->next_ = head_;
     }
-
-  private:
-    Node* head_;
-    Node* tail_;
-    size_t size_;
-  };
-}
+    delete temp;
+    size_--;
+  }
+  
+  void push_back(const T& value)
+  {
+    Node* new_node = new Node(value);
+    if (!head_)
+    {
+      head_ = new_node;
+      tail_ = new_node;
+      new_node->next_ = head_;
+    }
+    else
+    {
+      tail_->next_ = new_node;
+      new_node->next_ = head_;
+      tail_ = new_node;
+    }
+    size_++;
+  }
+};
 #endif
