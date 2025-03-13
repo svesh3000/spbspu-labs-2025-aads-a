80fe0434f0ccee82c7ddb4584df3c907c0fe188c
A1exCRE <alsmmm2065@gmail.com>
2025-03-13 20:05:52 +0300

    fix diff

--- smirnov.alexandr/S1/main.cpp
+++ smirnov.alexandr/S1/main.cpp
@@ -56,85 +56,85 @@ int main()
       std::cout << " ";
     }
     std::cout << sequence.first;
-    }
-    std::cout << "\n";
+  }
+  std::cout << "\n";
   
-    bool hasNum = false;
+  bool hasNum = false;
+  for (auto it = sequences.begin(); it != sequences.end(); ++it)
+  {
+    const std::pair< std::string, List< unsigned long long > > & seq = *it;
+    if (!seq.second.isEmpty())
+    {
+      hasNum = true;
+      break;
+    }
+  }
+  
+  if (!hasNum)
+  {
+    std::cout << 0 << std::endl;
+    return 0;
+  }
+  
+  std::list< List< unsigned long long > > resultSequences;
+  bool hasNumbers = true;
+  while (hasNumbers)
+  {
+    List< unsigned long long > currentSequence;
+    hasNumbers = false;
     for (auto it = sequences.begin(); it != sequences.end(); ++it)
     {
-      const std::pair< std::string, List< unsigned long long > > & seq = *it;
+      std::pair< std::string, List< unsigned long long > > & seq = *it;
       if (!seq.second.isEmpty())
       {
-        hasNum = true;
-        break;
+        currentSequence.pushBack(*seq.second.begin());
+        seq.second.popFront();
+        hasNumbers = true;
       }
     }
+    if (!currentSequence.isEmpty())
+    {
+      resultSequences.push_back(currentSequence);
+    }
+  }
   
-    if (!hasNum)
+  for (auto it = resultSequences.begin(); it != resultSequences.end(); ++it)
+  {
+    const List< unsigned long long > & sequence = *it;
+    for (auto numIt = sequence.begin(); numIt != sequence.end(); ++numIt)
     {
-      std::cout << 0 << std::endl;
-      return 0;
-    }
-  
-    std::list< List< unsigned long long > > resultSequences;
-    bool hasNumbers = true;
-    while (hasNumbers)
-    {
-      List< unsigned long long > currentSequence;
-      hasNumbers = false;
-      for (auto it = sequences.begin(); it != sequences.end(); ++it)
-      {
-        std::pair< std::string, List< unsigned long long > > & seq = *it;
-        if (!seq.second.isEmpty())
-        {
-          currentSequence.pushBack(*seq.second.begin());
-          seq.second.popFront();
-          hasNumbers = true;
-        }
-      }
-      if (!currentSequence.isEmpty())
-      {
-        resultSequences.push_back(currentSequence);
-      }
-    }
-  
-    for (auto it = resultSequences.begin(); it != resultSequences.end(); ++it)
-    {
-      const List< unsigned long long > & sequence = *it;
-      for (auto numIt = sequence.begin(); numIt != sequence.end(); ++numIt)
-      {
-        if (numIt != sequence.begin())
-        {
-          std::cout << " ";
-        }
-        std::cout << *numIt;
-      }
-      std::cout << "\n";
-    }
-  
-    List< unsigned long long > sums;
-    for (auto it = resultSequences.begin(); it != resultSequences.end(); ++it)
-    {
-      const List< unsigned long long > & sequence = *it;
-      unsigned long long sum = 0;
-      for (auto numIt = sequence.begin(); numIt != sequence.end(); ++numIt)
-      {
-        if (sum > std::numeric_limits< unsigned long long >::max() - *numIt)
-        {
-          std::cerr << "Overflow\n";
-          return 1;
-        }
-        sum += *numIt;
-      }
-      sums.pushBack(sum);
-    }
-    for (auto it = sums.begin(); it != sums.end(); ++it)
-    {
-      if (it != sums.begin())
+      if (numIt != sequence.begin())
       {
         std::cout << " ";
       }
-      std::cout << *it;
+      std::cout << *numIt;
     }
     std::cout << "\n";
   }
+  
+  List< unsigned long long > sums;
+  for (auto it = resultSequences.begin(); it != resultSequences.end(); ++it)
+  {
+    const List< unsigned long long > & sequence = *it;
+    unsigned long long sum = 0;
+    for (auto numIt = sequence.begin(); numIt != sequence.end(); ++numIt)
+    {
+      if (sum > std::numeric_limits< unsigned long long >::max() - *numIt)
+      {
+        std::cerr << "Overflow\n";
+        return 1;
+      }
+      sum += *numIt;
+    }
+    sums.pushBack(sum);
+  }
+  for (auto it = sums.begin(); it != sums.end(); ++it)
+  {
+    if (it != sums.begin())
+    {
+      std::cout << " ";
+    }
+    std::cout << *it;
+  }
+  std::cout << "\n";
+}
