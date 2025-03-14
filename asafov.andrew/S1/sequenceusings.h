a613e0d7b0162ee6f3766b684e88e7ee6cddd747
shycoshy <neforscist@gmail.com>
2025-03-14 14:01:43 +0000

    tests no.113

--- asafov.andrew/S1/sequenceusings.h
+++ asafov.andrew/S1/sequenceusings.h
@@ -4,7 +4,7 @@
 #include <utility>
 #include <string>
 using data_t = size_t;
-using data_list_t = asafov::forward_list<data_t>;
+using data_list_t = forward_list<data_t>;
 using sequence_t = std::pair<std::string, data_list_t>;
-using sequence_list_t = asafov::forward_list<sequence_t>;
+using sequence_list_t = forward_list<sequence_t>;
 #endif
