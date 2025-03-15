956729faa0b06289ff1b4aabe6ea458ab653df3b
LuckyBichar <enbocharov2006@gmail.com>
2025-03-15 15:05:54 +0300

    try aptimization

--- bocharov.egor/S1/actions.hpp
+++ bocharov.egor/S1/actions.hpp
@@ -7,12 +7,14 @@
 
 namespace bocharov
 {
-  using list_t = bocharov::List< unsigned long long >;
-  using pairs_list_t = bocharov::List< std::pair< std::string, list_t > >;
+  using list_t = List< unsigned long long >;
+  using pairs_list_t = List< std::pair< std::string, list_t > >;
 
   std::size_t get_max_pairs_list_size(const pairs_list_t & list) noexcept;
   void get_lists_sums(list_t & sums, const pairs_list_t & pairs_list);
   void print_lists_info(std::ostream & out, const pairs_list_t & pairs_list);
+  pairs_list_t createList(std::istream & input);
+  std::ostream & output(std::ostream & output, const pairs_list_t & list);
 }
 
 #endif
