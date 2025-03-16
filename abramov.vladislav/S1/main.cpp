eafffdd4be25ea5b44c79539a80a482e1438967e
15446-rus75 <vladabramov06@yandex.ru>
2025-03-16 20:25:03 +0300

    add input/output functions

--- abramov.vladislav/S1/main.cpp
+++ abramov.vladislav/S1/main.cpp
@@ -1,105 +1,35 @@
 #include <iostream>
 #include <string>
-#include <limits>
 #include "list.hpp"
+#include "input_outputLists.hpp"
+#include "outputSums.hpp"
 
 int main()
 {
   using namespace abramov;
   List< std::pair< std::string, List< unsigned long long int > > > lists;
-  std::string s;
-  std::cin >> s;
   size_t count = 0;
   size_t lists_count = 0;
-  while (std::cin)
-  {
-    std::pair< std::string, List< unsigned long long int > > bidir;
-    bidir.first = s;
-    std::cin >> s;
-    while(std::cin && std::isdigit(s[0]))
-    {
-      bidir.second.pushBack(std::strtoull(s.c_str(), nullptr, 10));
-      std::cin >> s;
-    }
-    lists.pushBack(bidir);
-    count = std::max(count, bidir.second.size());
-    ++lists_count;
-  }
+  inputLists(std::cin, &lists, lists_count, count);
   if (!lists_count)
   {
     std::cout << "0\n";
     return 0;
   }
-  for (auto iter = lists.begin(); iter != --lists.end(); ++iter)
+  printNames(std::cout, &lists);
+  printValues(std::cout, &lists, count);
+  try
   {
-    std::cout << iter->first << " ";
+    outputSums(std::cout, &lists, count);
   }
-  std::cout << (--lists.end())->first << "\n";
-  for (size_t i = 0; i < count; ++i)
+  catch (const std::bad_alloc &)
   {
-    bool flag = false;
-    for (auto iter = lists.begin(); iter != --lists.end(); ++iter)
-    {
-      if (iter->second.size() > i)
-      {
-        auto iter2 = std::next(iter->second.begin(), i);
-        if (flag)
-        {
-          std::cout << " ";
-        }
-        std::cout << *iter2;
-        flag = true;
-      }
-    }
-    if ((--lists.end())->second.size() <= i)
-    {
-      std::cout << "\n";
-    }
-    else
-    {
-      std::cout << " " << *(std::next((--lists.end())->second.begin(), i)) << "\n";
-    }
+    std::cerr << "Memory fail\n";
+    return 2;
   }
-  unsigned long long int *sums = new unsigned long long int[count];
-  constexpr unsigned long long int max = std::numeric_limits< unsigned long long int >::max();
-  for (size_t i = 0; i < count; ++i)
+  catch (const std::logic_error &e)
   {
-    unsigned long long int sum = 0;
-    for (auto iter = lists.begin(); iter != lists.end(); ++iter)
-    {
-      if (iter->second.size() > i)
-      {
-        auto iter2 = std::next(iter->second.begin(), i);
-        if (*iter2 > max - sum)
-        {
-          std::cerr << "overflow\n";
-          delete[] sums;
-          return 1;
-        }
-        sum += *iter2;
-      }
-    }
-    sums[i] = sum;
+    std::cerr << e.what();
+    return 1;
   }
-  bool empty = true;
-  for (size_t i = 0; i < count; ++i)
-  {
-    if (sums[i] != 0)
-    {
-      empty = false;
-      break;
-    }
-  }
-  if (empty)
-  {
-    std::cout << "0\n";
-    delete[] sums;
-    return 0;
-  }
-  for (size_t i = 0; i < count - 1; ++i)
-  {
-    std::cout << sums[i] << " ";
-  }
-  std::cout << sums[count - 1] << "\n";
-  delete[] sums;
 }
