b2e68705f4bb3d4e4120c50417eab7ed72d9551d
deonik3 <kis.sergey2006@gmail.com>
2025-03-17 18:52:48 +0300

    update main

--- kiselev.sergey/S2/main.cpp
+++ kiselev.sergey/S2/main.cpp
@@ -1,3 +1,35 @@
-int main()
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <istream>
+#include <pthread.h>
+#include "actionsExpr.hpp"
+
+int main(int argc, char* argv[])
 {
+  try
+  {
+    kiselev::queue infixExprs;
+    //if (argc < 1)
+    //{
+    //  kiselev::inputExprs(std::cin, infixExprs);
+    //}
+    if (argc > 1)
+    {
+      std::ifstream file(argv[1]);
+      kiselev::inputExprs(file, infixExprs);
+    }
+    else
+    {
+      kiselev::inputExprs(std::cin, infixExprs);
+    }
+    kiselev::queue postfixExprs = kiselev::convertExpr(infixExprs);
+    kiselev::stackNumber results = kiselev::calculationExpr(postfixExprs);
+    kiselev::outputResults(std::cout, results) << "\n";
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
 }
