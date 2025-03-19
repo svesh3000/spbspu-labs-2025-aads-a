8a851c03a2b0e5383d48d7140bb5fce2cfea52e1
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2025-03-19 13:01:53 +0300

    Input line

--- gavrilova.polina/S2/main.cpp
+++ gavrilova.polina/S2/main.cpp
@@ -1,2 +1,24 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+  std::ifstream inputFile;
+  if (argc > 1) {
+    inputFile.open(argv[1]);
+    if (!inputFile.is_open()) {
+      std::cerr << "Error! Invalid openong file!\n";
+      return 1;
+    }
+  }
+  std::istream& in = (argc > 1) ? inputFile : std::cin;
+  std::string line;
+  while (getline(in, line)) {
+    if (line.empty()) {
+      continue;
+    }
+  }
+  if (!in) {
+  }
+}
