70945db92471e2307561476a2e00738ebe7048c8
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2025-03-19 21:06:58 +0300

    Stack and Queue implementation

--- gavrilova.polina/S2/Queue.hpp
+++ gavrilova.polina/S2/Queue.hpp
@@ -1,15 +1,21 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
 
+#include <cstddef>
+#include <utility>
+#include <stdexcept>
+
 namespace gavrilova {
   template < class T >
   class Queue {
   public:
     Queue();
+    Queue(const Queue< T >& other);
+    Queue(Queue< T >&& other);
     ~Queue();
     
-    Queue& operator=(const Queue& other );
-    Queue& operator=(Queue&& other );
+    Queue& operator=(const Queue& other);
+    Queue& operator=(Queue&& other);
 
     void push(const T& value);
     void push(T&& value);
@@ -19,13 +25,144 @@ namespace gavrilova {
     void pop();
     bool empty() const;
     size_t size() const;
-    void swap(Queue& other ) noexcept;
+    void swap(Queue& other) noexcept;
 
   private:
-    T* stack_;
+    T* queue_;
     size_t size_;
-    size_t capacity;
+    size_t capacity_;
+
+    void resize();
   };
+
+  template < class T >
+  Queue< T >::Queue():
+    size_(0),
+    capacity_(50),
+    queue_(new T[capacity_])
+  {}
+
+  template < class T >
+  Queue< T >::Queue(const Queue< T >& other):
+    size_(other.size_),
+    capacity_(other.capacity_),
+    queue_(new T[capacity_])
+  {
+    for  (size_t i = 0; i < size_; ++i) {
+      queue_[i] = other.queue_[i];
+    }
+  }
+
+  template < class T >
+  Queue< T >::Queue(Queue<T>&& other):
+    size_(other.size_),
+    capacity_(other.capacity_),
+    queue_(other.queue_)
+  {
+    other.size_ = 0;
+    other.capacity_ = 0;
+    other.queue_ = nullptr;
+  }
+
+  template < class T >
+  Queue< T >::~Queue()
+  {
+    delete[] queue_;
+  }
+  
+  template < class T >
+  Queue< T >& Queue< T >::operator=(const Queue& other)
+  {
+    if (this != std::addressof(other)) {
+      Queue cpy(other);
+      swap(cpy);
+    }
+    return *this;
+  }
+
+  template < class T >
+  Queue< T >& Queue< T >::operator=(Queue&& other)
+  {
+    if (this != std::addressof(other)) {
+      swap(other);
+      delete[] other.queue_;
+    }
+    return *this;
+  }
+
+  template < class T >
+  void Queue< T >::push(const T& value)
+  {
+    if (size_ == capacity_) {
+      resize();
+    }
+    queue_[size_++] = value;
+  }
+  
+  template < class T >
+  void Queue< T >::push(T&& value)
+  {
+    if (size_ == capacity_) {
+      resize();
+    }
+    queue_[size_++] = std::move(value);
+  }
+
+  template < class T >
+  T& Queue< T >::front()
+  {
+    return queue_[0];
+  }
+
+  template < class T >
+  T& Queue< T >::back()
+  {
+    return queue_[size_ - 1];
+  }
+  
+  template < class T >
+  void Queue< T >::pop()
+  {
+    if (empty()) {
+      throw std::out_of_range("Stack is empty");
+    }
+    for (size_t i = 1; i < size_; ++i) {
+      queue_[i-1] = std::move(queue_[i]);
+    }
+    --size_;
+  }
+  
+  template < class T >
+  bool Queue< T >::empty() const
+  {
+    return !size_;
+  }
+  
+  template < class T >
+  size_t Queue< T >::size() const
+  {
+    return size_;
+  }
+
+  template < class T >
+  void Queue< T >::swap(Queue& other) noexcept
+  {
+    std::swap(size_, other.size_);
+    std::swap(capacity_, other.capacity_);
+    std::swap(queue_, other.queue_);
+  }
+
+  template < class T >
+  void Queue< T >::resize()
+  {
+    T* new_queue = new T[capacity_+50];
+    for (size_t i = 0; i < size_; ++i) {
+      new_queue[i] = queue_[i];
+    }
+    delete[] queue_;
+    queue_ = new_queue;
+    capacity_ += 50;
+  }
 }
 
 #endif
--- gavrilova.polina/S2/Stack.hpp
+++ gavrilova.polina/S2/Stack.hpp
@@ -1,15 +1,21 @@
 #ifndef STACK_HPP
 #define STACK_HPP
 
+#include <cstddef>
+#include <utility>
+#include <stdexcept>
+
 namespace gavrilova {
   template < class T >
   class Stack {
   public:
     Stack();
+    Stack(const Stack< T >& other);
+    Stack(Stack< T >&& other);
     ~Stack();
     
-    Stack& operator=(const Stack& other );
-    Stack& operator=(Stack&& other );
+    Stack< T >& operator=(const Stack& other);
+    Stack< T >& operator=(Stack&& other);
 
     void push(const T& value);
     void push(T&& value);
@@ -18,11 +24,136 @@ namespace gavrilova {
     void pop();
     bool empty() const;
     size_t size() const;
-    void swap( stack& other ) noexcept;
+    void swap(Stack& other ) noexcept;
 
   private:
-    T* stack_;
     size_t size_;
-    size_t capacity;
+    size_t capacity_;
+    T* stack_;
+
+    void resize();
   };
+
+  template < class T >
+  Stack< T >::Stack():
+    size_(0),
+    capacity_(50),
+    stack_(new T[capacity_])
+  {}
+
+  template < class T >
+  Stack< T >::Stack(const Stack< T >& other):
+    size_(other.size_),
+    capacity_(other.capacity_),
+    stack_(new T[capacity_])
+  {
+    for  (size_t i = 0; i < size_; ++i) {
+      stack_[i] = other.stack_[i];
+    }
+  }
+
+  template < class T >
+  Stack< T >::Stack(Stack<T>&& other):
+    size_(other.size_),
+    capacity_(other.capacity_),
+    stack_(other.stack_)
+  {
+    other.size_ = 0;
+    other.capacity_ = 0;
+    other.stack_ = nullptr;
+  }
+
+  template < class T >
+  Stack< T >::~Stack()
+  {
+    delete[] stack_;
+  }
+  
+  template < class T >
+  Stack< T >& Stack< T >::operator=(const Stack& other)
+  {
+    if (this != std::addressof(other)) {
+      Stack cpy(other);
+      swap(cpy);
+    }
+    return *this;
+  }
+
+  template < class T >
+  Stack< T >& Stack< T >::operator=(Stack&& other)
+  {
+    if (this != std::addressof(other)) {
+      swap(other);
+      delete[] other.stack_;
+    }
+    return *this;
+  }
+
+  template < class T >
+  void Stack< T >::push(const T& value)
+  {
+    if (size_ == capacity_) {
+      resize();
+    }
+    stack_[size_++] = value;
+  }
+  
+  template < class T >
+  void Stack< T >::push(T&& value)
+  {
+    if (size_ == capacity_) {
+      resize();
+    }
+    stack_[size_++] = std::move(value);
+  }
+
+  template < class T >
+  T& Stack< T >::top()
+  {
+    return stack_[size_ - 1];
+  }
+  
+  template < class T >
+  void Stack< T >::pop()
+  {
+    if (empty()) {
+      throw std::out_of_range("Stack is empty");
+    }
+    --size_;
+  }
+  
+  template < class T >
+  bool Stack< T >::empty() const
+  {
+    return !size_;
+  }
+  
+  template < class T >
+  size_t Stack< T >::size() const
+  {
+    return size_;
+  }
+
+  template < class T >
+  void Stack< T >::swap(Stack& other) noexcept
+  {
+    std::swap(size_, other.size_);
+    std::swap(capacity_, other.capacity_);
+    std::swap(stack_, other.stack_);
+  }
+
+  template < class T >
+  void Stack< T >::resize()
+  {
+    T* new_stack = new T[capacity_+50];
+    for (size_t i = 0; i < size_; ++i) {
+      new_stack[i] = stack_[i];
+    }
+    delete[] stack_;
+    stack_ = new_stack;
+    capacity_ += 50;
+  }
+
 }
+
+#endif
