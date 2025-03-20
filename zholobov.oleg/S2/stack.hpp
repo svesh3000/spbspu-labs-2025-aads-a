4159a630f52ca3527bf3b51ff4b380425cf261dd
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-03-20 23:32:06 +0300

    implemented Stack
    added some checks in tests

--- zholobov.oleg/S2/stack.hpp
+++ zholobov.oleg/S2/stack.hpp
@@ -1,26 +1,168 @@
 #ifndef STACK_HPP
 #define STACK_HPP
 
-#include <algorithm>
 #include <stdexcept>
+#include <utility>
 
 namespace zholobov {
 
   template < typename T >
   class Stack {
+    static constexpr size_t initial_capacity = 4;
+
   public:
-    Stack() {}
-    Stack(const Stack& other) {}
-    Stack(Stack&& other) noexcept {}
-    Stack& operator=(const Stack& other) { return *this; }
-    Stack& operator=(Stack&& other) noexcept { return *this; }
-    void push(const T& rhs) {}
-    void push(T&& rhs) {}
-    const T& top() const { return T(); }
-    T& top() { return T(); }
-    void pop() {}
-    bool empty() const { return true; }
+    Stack();
+    Stack(const Stack& other);
+    Stack(Stack&& other) noexcept;
+    Stack& operator=(const Stack& other);
+    Stack& operator=(Stack&& other) noexcept;
+    ~Stack();
+    void push(const T& value);
+    void push(T&& value);
+    void pop();
+    T& top();
+    const T& top() const;
+    bool empty() const noexcept;
+    void clear() noexcept;
+    void swap(Stack& other) noexcept;
+
+  private:
+    T** data_;
+    size_t size_;
+    size_t capacity_;
   };
+}
+
+template < typename T >
+zholobov::Stack< T >::Stack():
+  data_(nullptr), size_(0), capacity_(0)
+{}
+
+template < typename T >
+zholobov::Stack< T >::Stack(const Stack& other):
+  data_(nullptr), size_(0), capacity_(0)
+{
+  if (other.size_ > 0) {
+    try {
+      data_ = new T*[other.capacity_];
+      capacity_ = other.capacity_;
+      for (; size_ < other.size_; ++size_) {
+        data_[size_] = new T(*other.data_[size_]);
+      }
+    } catch (const std::bad_alloc&) {
+      clear();
+      throw;
+    }
+  }
+}
+
+template < typename T >
+zholobov::Stack< T >::Stack(Stack&& other) noexcept:
+  data_(nullptr), size_(0), capacity_(0)
+{
+  swap(other);
+}
+
+template < typename T >
+zholobov::Stack< T >& zholobov::Stack< T >::operator=(const Stack& other)
+{
+  if (this != std::addressof(other)) {
+    Stack temp(other);
+    swap(temp);
+  }
+  return *this;
+}
+
+template < typename T >
+zholobov::Stack< T >& zholobov::Stack< T >::operator=(Stack&& other) noexcept
+{
+  if (this != std::addressof(other)) {
+    clear();
+    swap(other);
+  }
+  return *this;
+}
+
+template < typename T >
+zholobov::Stack< T >::~Stack()
+{
+  clear();
+}
+
+template < typename T >
+void zholobov::Stack< T >::push(const T& value)
+{
+  push(T(value));
+}
+
+template < typename T >
+void zholobov::Stack< T >::push(T&& value)
+{
+  if (size_ == capacity_) {
+    size_t new_capacity = (capacity_ != 0) ? capacity_ * 2 : initial_capacity;
+    T** new_data = new T*[new_capacity];
+    for (size_t i = 0; i < size_; ++i) {
+      new_data[i] = data_[i];
+    }
+    delete[] data_;
+    data_ = new_data;
+    capacity_ = new_capacity;
+  }
+  data_[size_++] = new T(std::move(value));
+}
+
+template < typename T >
+void zholobov::Stack< T >::pop()
+{
+  if (empty()) {
+    throw std::out_of_range("Stack is empty");
+  }
+  delete data_[--size_];
+}
+
+template < typename T >
+T& zholobov::Stack< T >::top()
+{
+  if (empty()) {
+    throw std::out_of_range("Stack is empty");
+  }
+  return *data_[size_ - 1];
+}
+
+template < typename T >
+const T& zholobov::Stack< T >::top() const
+{
+  if (empty()) {
+    throw std::out_of_range("Stack is empty");
+  }
+  return *data_[size_ - 1];
+}
+
+template < typename T >
+bool zholobov::Stack< T >::empty() const noexcept
+{
+  return size_ == 0;
+}
+
+template < typename T >
+void zholobov::Stack< T >::clear() noexcept
+{
+  for (size_t i = 0; i < size_; ++i) {
+    delete data_[i];
+  }
+  size_ = 0;
+  if (data_ != nullptr) {
+    delete[] data_;
+    data_ = nullptr;
+  }
+}
+
+template < typename T >
+void zholobov::Stack< T >::swap(Stack& other) noexcept
+{
+  std::swap(data_, other.data_);
+  std::swap(size_, other.size_);
+  std::swap(capacity_, other.capacity_);
 }
 
 #endif
--- zholobov.oleg/S2/test-stack.cpp
+++ zholobov.oleg/S2/test-stack.cpp
@@ -63,6 +63,14 @@ BOOST_AUTO_TEST_CASE(copy_assign_operator)
   zholobov::Stack< std::string > new_stack = stack;
   BOOST_TEST(to_string(stack) == "content is This");
   BOOST_TEST(to_string(new_stack) == "content is This");
+  stack.push("of");
+  stack.push("a");
+  stack.push("stack");
+  new_stack = stack;
+  BOOST_TEST(to_string(stack) == "stack a of content is This");
+  BOOST_TEST(to_string(new_stack) == "stack a of content is This");
+  new_stack = zholobov::Stack< std::string >();
+  BOOST_TEST(to_string(new_stack) == "");
 }
 
 BOOST_AUTO_TEST_CASE(move_assign_operator)
@@ -98,7 +106,7 @@ BOOST_AUTO_TEST_CASE(push_move)
   BOOST_TEST(val == "");
 }
 
-BOOST_AUTO_TEST_CASE(front__back_pop)
+BOOST_AUTO_TEST_CASE(front_back_pop)
 {
   zholobov::Stack< std::string > stack;
   stack.push("This");
@@ -111,6 +119,24 @@ BOOST_AUTO_TEST_CASE(front__back_pop)
   BOOST_TEST(stack.top() == "is");
   stack.pop();
   BOOST_TEST(stack.top() == "This");
+}
+
+BOOST_AUTO_TEST_CASE(many_push_pop)
+{
+  zholobov::Stack< int > stack;
+  for (int i = 0; i < 500; ++i) {
+    stack.push(i);
+  }
+  for (int i = 0; i < 400; ++i) {
+    stack.pop();
+  }
+  for (int i = 0; i < 1000; ++i) {
+    stack.push(i);
+  }
+  for (int i = 0; i < 1098; ++i) {
+    stack.pop();
+  }
+  BOOST_TEST(to_string(stack) == "1 0");
 }
 
 BOOST_AUTO_TEST_CASE(empty)
