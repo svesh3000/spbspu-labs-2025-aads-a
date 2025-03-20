749bf361ddfc035a921ddbd137ce1d0255569e2b
“Karnaukhova <masj280906@gmail.com>
2025-03-21 02:16:19 +0300

    content

--- karnauhova.alexandra/S2/main.cpp
+++ karnauhova.alexandra/S2/main.cpp
@@ -43,7 +43,7 @@ long long int proc_post(karnauhova::Queue< std::string > post)
   long long int second = 0;
   while (!post.empty())
   {
-    
+
     std::string element = post.front();
     first = std::stoll(element);
     post.pop();
