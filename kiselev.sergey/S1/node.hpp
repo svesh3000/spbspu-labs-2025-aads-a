14700869d02eea42c28fb4c74b19e792c5544b46
deonik3 <kis.sergey2006@gmail.com>
2025-03-20 17:26:07 +0300

    i'm blindly

--- kiselev.sergey/S1/node.hpp
+++ kiselev.sergey/S1/node.hpp
@@ -9,9 +9,6 @@ namespace kiselev
     T data;
     Node< T >* next;
     Node< T >* prev;
-
-    //explicit Node(const T& data): data_(data), next_(nullptr), prev_(nullptr) {}
-    //Node(): data_(T()), next_(nullptr), prev_(nullptr) {}
   };
 }
 #endif
