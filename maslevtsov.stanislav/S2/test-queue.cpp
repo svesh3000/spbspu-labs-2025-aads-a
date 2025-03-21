b496a0097cfe65b53fa0a555fa75d9c9f387907e
jambii1 <s.maslevtsov@gmail.com>
2025-03-21 23:16:20 +0300

    queue tests

--- maslevtsov.stanislav/S2/test-queue.cpp
+++ maslevtsov.stanislav/S2/test-queue.cpp
@@ -1,6 +1,95 @@
 #include <boost/test/unit_test.hpp>
+#include "queue.hpp"
 
-BOOST_AUTO_TEST_CASE(plug)
+BOOST_AUTO_TEST_SUITE(constructors_tests)
+BOOST_AUTO_TEST_CASE(default_constructor_test)
 {
-  BOOST_TEST(true);
+  maslevtsov::Queue< int > q;
+  BOOST_TEST(q.empty());
 }
+
+BOOST_AUTO_TEST_CASE(copy_constructor_test)
+{
+  maslevtsov::Queue< int > q1;
+  q1.push(1);
+  q1.push(2);
+  maslevtsov::Queue< int > q2(q1);
+  BOOST_TEST(q1.front() == q2.front());
+  BOOST_TEST(q1.back() == q2.back());
+  BOOST_TEST(q1.size() == q2.size());
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(operators_tests)
+BOOST_AUTO_TEST_CASE(copy_constructor_test)
+{
+  maslevtsov::Queue< int > q1;
+  q1.push(1);
+  q1.push(2);
+  maslevtsov::Queue< int > q2;
+  q2 = q1;
+  BOOST_TEST(q1.front() == q2.front());
+  BOOST_TEST(q1.back() == q2.back());
+  BOOST_TEST(q1.size() == q2.size());
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(elements_access_tests)
+BOOST_AUTO_TEST_CASE(front_test)
+{
+  maslevtsov::Queue< int > q1;
+  q1.push(1);
+  q1.push(2);
+  BOOST_TEST(q1.front() == 1);
+}
+
+BOOST_AUTO_TEST_CASE(back_test)
+{
+  maslevtsov::Queue< int > q1;
+  q1.push(1);
+  q1.push(2);
+  BOOST_TEST(q1.back() == 2);
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(capacity_tests)
+BOOST_AUTO_TEST_CASE(empty_test)
+{
+  maslevtsov::Queue< int > q1;
+  BOOST_TEST(q1.empty());
+  q1.push(1);
+  BOOST_TEST(!q1.empty());
+}
+
+BOOST_AUTO_TEST_CASE(size_test)
+{
+  maslevtsov::Queue< int > q1;
+  q1.push(1);
+  BOOST_TEST(q1.size() == 1);
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(modifiers_tests)
+BOOST_AUTO_TEST_CASE(push_test)
+{
+  maslevtsov::Queue< int > q1;
+  q1.push(1);
+  BOOST_TEST(q1.front() == 1);
+  BOOST_TEST(q1.size() == 1);
+  q1.push(2);
+  BOOST_TEST(q1.back() == 2);
+  BOOST_TEST(q1.size() == 2);
+}
+
+BOOST_AUTO_TEST_CASE(pop_test)
+{
+  maslevtsov::Queue< int > q1;
+  q1.push(1);
+  q1.push(2);
+  q1.pop();
+  BOOST_TEST(q1.size() == 1);
+  BOOST_TEST(q1.front() == 2);
+  q1.pop();
+  BOOST_TEST(q1.empty());
+}
+BOOST_AUTO_TEST_SUITE_END()
