f8d54a38ca8338998dc4785dbb812ac5f3b827f8
“Karnaukhova <masj280906@gmail.com>
2025-03-21 15:21:05 +0300

    +2 tests for stack

--- karnauhova.alexandra/S2/test-stack.cpp
+++ karnauhova.alexandra/S2/test-stack.cpp
@@ -9,3 +9,30 @@ BOOST_AUTO_TEST_CASE(test_capacity_stack)
   stack.push(2);
   BOOST_TEST(stack.size() == 1);
 }
+
+BOOST_AUTO_TEST_CASE(test_pop_and_push_stack)
+{
+  karnauhova::Stack< int > stack;
+  stack.push(1);
+  BOOST_TEST(stack.top() == 1);
+  stack.push(2);
+  BOOST_TEST(stack.top() == 2);
+  BOOST_TEST(stack.size() == 2);
+  stack.pop();
+  BOOST_TEST(stack.size() == 1);
+  BOOST_TEST(stack.top() == 1);
+  stack.pop();
+  BOOST_TEST(stack.size() == 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_top_stack)
+{
+  karnauhova::Stack< int > stack;
+  stack.push(1);
+  stack.push(2);
+  stack.push(3);
+  int i1 = stack.top();
+  const int i2 = stack.top();
+  BOOST_TEST(i1 == 3);
+  BOOST_TEST(i2 == 3);
+}
