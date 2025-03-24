06f158555843a7aa03847b593fe121374f8a4da8
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-03-24 10:34:11 +0300

    add splitExpression

--- maslov.aleksey/S2/main.cpp
+++ maslov.aleksey/S2/main.cpp
@@ -3,7 +3,30 @@
 #include <string>
 #include "queue.hpp"
 
-void splitExpression(const std::string & str, maslov::Queue< std::string >);
+void splitExpression(const std::string & str, maslov::Queue< std::string > infixQueue)
+{
+  std::string element;
+  char separator = ' ';
+  for (size_t i = 0; i < str.length(); ++i)
+  {
+    if (str[i] != separator)
+    {
+      element += str[i];
+    }
+    else
+    {
+      if (!element.empty())
+      {
+        infixQueue.push(element);
+        element.clear();
+      }
+    }
+  }
+  if (!element.empty())
+  {
+    infixQueue.push(element);
+  }
+}
 
 void inputExpression(std::istream & in, maslov::Queue< maslov::Queue< std::string > > & queue)
 {
