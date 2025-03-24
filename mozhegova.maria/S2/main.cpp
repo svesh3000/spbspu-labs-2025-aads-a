83adf799ce8a9ae80f906a54ad9d9771ef15c808
mipxe <mariamozegova8726@gmail.com>
2025-03-24 15:48:09 +0300

    add convert infix to postfix expression

--- mozhegova.maria/S2/main.cpp
+++ mozhegova.maria/S2/main.cpp
@@ -2,13 +2,14 @@
 #include <fstream>
 #include <string>
 #include "queue.hpp"
+#include "stack.hpp"
 
 namespace
 {
   void inputExprs(std::istream & in, mozhegova::Queue< mozhegova::Queue< std::string > > & queue)
   {
     std::string line;
-    while (std::getline(in, line) && !line.empty())
+    while (std::getline(in, line))
     {
       mozhegova::Queue< std::string > infExpr;
       if (line.empty())
@@ -43,6 +44,105 @@ namespace
       }
     }
   }
+
+  bool isOperand(std::string str)
+  {
+    bool flag = true;
+    for (char c : str)
+    {
+      if (!std::isdigit(c))
+      {
+        flag = false;
+      }
+    }
+    return flag;
+  }
+
+  bool isOperation(std::string op)
+  {
+    return op == "*" || op == "/" || op == "%" || op == "+" || op == "-";
+  }
+
+  int getPriority(std::string op)
+  {
+    if (op == "+" || op == "-")
+    {
+      return 1;
+    }
+    else if (op == "*" || op == "/" || op == "%")
+    {
+      return 2;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+
+  mozhegova::Queue< mozhegova::Queue< std::string > > convertInfToPost(mozhegova::Queue< mozhegova::Queue< std::string > > & queue)
+  {
+    mozhegova::Queue< mozhegova::Queue< std::string > > newQueue;
+    while (!queue.empty())
+    {
+      mozhegova::Queue< std::string > infExpr = queue.front();
+      queue.pop();
+      mozhegova::Queue< std::string > postExpr;
+      mozhegova::Stack< std::string > stack;
+      while (!infExpr.empty())
+      {
+        std::string token = infExpr.front();
+        infExpr.pop();
+        if (isOperand(token))
+        {
+          postExpr.push(token);
+        }
+        else if (token == "(")
+        {
+          stack.push(token);
+        }
+        else if (token == ")")
+        {
+          while (!stack.empty() && stack.top() != "(")
+          {
+            postExpr.push(stack.top());
+            stack.pop();
+          }
+          if (stack.top() == "(")
+          {
+            stack.pop();
+          }
+          else
+          {
+            throw std::logic_error("invalid expression");
+          }
+        }
+        else if (isOperation(token))
+        {
+          while (!stack.empty() && getPriority(token) <= getPriority(stack.top()))
+          {
+            postExpr.push(stack.top());
+            stack.pop();
+          }
+          stack.push(token);
+        }
+        else
+        {
+          throw std::logic_error("invalid expression");
+        }
+      }
+      while (!stack.empty())
+      {
+        if (stack.top() == "(")
+        {
+          throw std::logic_error("invalid expression");
+        }
+        postExpr.push(stack.top());
+        stack.pop();
+      }
+      newQueue.push(postExpr);
+    }
+    return newQueue;
+  }
 }
 
 int main(int argc, char * argv[])
@@ -60,6 +160,7 @@ int main(int argc, char * argv[])
     {
       inputExprs(std::cin, infExprs);
     }
+    Queue< Queue< std::string > > postExprs = convertInfToPost(infExprs);
   }
   catch(const std::exception & e)
   {
