9d9986e4f8f62dd2247229c3c9ad26a47d148ea1
svesh3000 <russkij2006@mail.ru>
2025-03-25 08:18:47 +0300

    Stack

---
+++ sveshnikov.michail/S2/stack.hpp
@@ -0,0 +1,161 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+#include <cstddef>
+#include <algorithm>
+
+namespace sveshnikov
+{
+  template < class T >
+  class Stack
+  {
+  public:
+    explicit Stack();
+    Stack(const Stack &other);
+    Stack(Stack &&other);
+    ~Stack();
+    Stack< T > &operator=(const Stack &other);
+    Stack< T > &operator=(Stack &&other) noexcept;
+
+    T &top() noexcept;
+    const T &top() const noexcept;
+    size_t getSize() noexcept;
+    bool empty() noexcept;
+    void push(const T &data);
+    void push(T &&data);
+    void pop();
+    void swap(Stack &other);
+
+  private:
+    size_t capacity_;
+    T *data_;
+    size_t size_;
+    void resize();
+  };
+
+  template < class T >
+  Stack< T >::Stack():
+    capacity_(100),
+    data_(new T[capacity_]),
+    size_(0)
+  {}
+
+  template < class T >
+  Stack< T >::Stack(const Stack &other):
+    capacity_(other.capacity_),
+    data_(new T[other.capacity_]),
+    size_(other.size_)
+  {
+    for (size_t i = 0; i < other.size_; i++)
+    {
+      data_[i] = other.data_[i];
+    }
+  }
+
+  template < class T >
+  Stack< T >::Stack(Stack &&other):
+    capacity_(other.capacity_),
+    data_(other.data_),
+    size_(other.size_)
+  {
+    other.data_ = nullptr;
+  }
+
+  template < class T >
+  Stack< T >::~Stack()
+  {
+    delete[] data_;
+  }
+
+  template < class T >
+  Stack< T > &Stack< T >::operator=(const Stack &other)
+  {
+    if (*this != std::addressof(other))
+    {
+      Stack rhs(other);
+      swap(rhs);
+    }
+    return *this;
+  }
+
+  template < class T >
+  Stack< T > &Stack< T >::operator=(Stack &&other) noexcept
+  {
+    swap(other);
+    return *this;
+  }
+
+  template < class T >
+  T &Stack< T >::top() noexcept
+  {
+    return const_cast< T & >(static_cast< const Stack< T > & >(*this).top());
+    ;
+  }
+
+  template < class T >
+  const T &Stack< T >::top() const noexcept
+  {
+    assert(!empty());
+    return data[size - 1];
+  }
+
+  template < class T >
+  size_t Stack< T >::getSize() noexcept
+  {
+    return size_;
+  }
+
+  template < class T >
+  bool Stack< T >::empty() noexcept
+  {
+    return size_ == 0;
+  }
+
+  template < class T >
+  void Stack< T >::resize()
+  {
+    if (size_ == capacity_)
+    {
+      capacity_ += 100;
+      T *new_data = new T[capacity_];
+      for (size_t i = 0; i < size_; i++)
+      {
+        new_data[i] = data_[i];
+      }
+      delete[] data_;
+      data_ = new_data;
+    }
+  }
+
+  template < class T >
+  void Stack< T >::push(const T &data)
+  {
+    resize();
+    data_[size_] = data;
+    size_++;
+  }
+
+  template < class T >
+  void Stack< T >::push(T &&data)
+  {
+    resize();
+    data_[size_] = std::move(data);
+    size_++;
+  }
+
+  template < class T >
+  void Stack< T >::pop()
+  {
+    assert(!empty());
+    delete data_[--size_];
+  }
+
+  template < class T >
+  void Stack< T >::swap(Stack &other)
+  {
+    std::swap(capacity_, other.capacity_);
+    std::swap(data_, other.data_);
+    std::swap(size_, other.size_);
+  }
+}
+
+#endif
