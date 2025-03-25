3117f6931275ea53578007954890c392346362b5
Ksenia-rgb <ksenialymova@mail.ru>
2025-03-25 22:45:11 +0300

    fix output

--- alymova.ksenia/S2/main.cpp
+++ alymova.ksenia/S2/main.cpp
@@ -181,9 +181,14 @@ int main(int argc, char** argv)
       return 1;
     }
   }
+  if (!res.empty())
+  {
+    std::cout << res.top();
+    res.pop();
+  }
   while (!res.empty())
   {
-    std::cout << res.top() << " ";
+    std::cout << " " << res.top() << " ";
     res.pop();
   }
   if (argc == 2)
