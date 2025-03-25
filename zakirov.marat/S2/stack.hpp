593615d639f62ea8adef490034cdcbb757f5ef9b
ZakirovMS <mz.08.08.06@gmail.com>
2025-03-25 18:43:02 +0300

    stack implementation, using my FwdList

---
+++ zakirov.marat/S2/stack.hpp
@@ -0,0 +1,97 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+#include <cstddef>
+#include <list>
+#include <stdexcept> 
+#include "../S1/fwd_list.hpp"
+
+namespace zakirov
+{
+  template < typename T >
+  class Stack
+  {
+  public:
+    Stack();
+    Stack(const Stack & other);
+    Stack(Stack && other) noexcept;
+    ~Stack() = default;
+    bool empty() const;
+    size_t size() const;
+    T & top();
+    const T & top() const;
+    void push (const T & value);
+    void push (T && value);
+    void pop();
+  private:
+    FwdList< T > data_;
+  };
+
+  template < typename T >
+  Stack< T >::Stack():
+    data_()
+  {}
+  
+  template < typename T >
+  Stack< T >::Stack(const Stack & other):
+    data_(other.data_)
+  {}
+
+  template < typename T >
+  Stack< T >::Stack(Stack && other) noexcept:
+    data_(std::move(other.data_))
+  {}
+
+  template < typename T >
+  bool Stack< T >::empty() const
+  {
+    return data_.empty();
+  }
+
+  template < typename T >
+  size_t Stack< T >::size() const
+  {
+    return data_.size();
+  }
+
+  template < typename T >
+  T & Stack< T >::top()
+  {
+    if (empty())
+    {
+      throw std::invalid_argument("Empty stack");
+    }
+
+    return *(data_.begin());
+  }
+
+  template < typename T >
+  const T & Stack< T >::top() const
+  {
+    if (empty())
+    {
+      throw std::invalid_argument("Empty stack");
+    }
+
+    return *(data_.cbegin());
+  }
+
+  template < typename T >
+  void Stack< T >::push(const T & value)
+  {
+    data_.push_front(value);
+  }
+
+  template < typename T >
+  void Stack< T >::push(T && value)
+  {
+    data_.push_front(value);
+  }
+
+  template < typename T >
+  void Stack< T >::pop()
+  {
+    data_.pop_front();
+  }
+}
+
+#endif
