649cd8630ad324a1751bf4f8e60668edc5891b5b
Ksenia-rgb <ksenialymova@mail.ru>
2025-03-25 19:41:17 +0300

    fix file input

--- alymova.ksenia/S2/main.cpp
+++ alymova.ksenia/S2/main.cpp
@@ -130,21 +130,32 @@ long long int count_postfix(alymova::Queue< std::string >& queue)
 }
 int main(int argc, char** argv)
 {
-  if (argc != 2)
+  if (argc > 2)
   {
     std::cerr << "Incorrect arguments\n";
     return 1;
   }
+  std::istream* input = &std::cin;
+  std::ifstream file;
+  if (argc == 2)
+  {
+    file.open(argv[1]);
+    if (!file.is_open())
+    {
+      std::cerr << "Incorrect file\n";
+      return 1;
+    }
+    input = &file;
+  }
   alymova::Stack< long long int > res;
-  std::ifstream input(argv[1]);
-  while (input.is_open() && !input.eof())
+  while (!(*input).eof())
   {
     try
     {
       alymova::Queue< std::string > queue;
       alymova::Stack< std::string > stack;
       std::string s;
-      std::getline(input, s);
+      std::getline(*input, s);
       if (s.empty())
       {
         continue;
@@ -158,11 +169,14 @@ int main(int argc, char** argv)
       return 1;
     }
   }
-  input.close();
   while (!res.empty())
   {
     std::cout << res.top() << " ";
     res.pop();
+  }
+  if (argc == 2)
+  {
+    file.close();
   }
 }
 
