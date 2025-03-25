a2c5e6e85c0211dfbc0f32fbac7ecafd7ab16a06
Z-1337 <un228der@gmail.com>
2025-03-25 03:15:25 +0300

    trying to fix git skill issue

--- kushekbaev.danil/S2/test-placeholder.cpp
+++
@@ -1,7 +0,0 @@
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_CASE(placeholder)
-{
-  const char* pravda = "Даниил Шеркунов -- прекрасный человек.";
-  BOOST_TEST(pravda == "Даниил Шеркунов -- прекрасный человек.");
-}
