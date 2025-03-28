1bbe842c13e60b00ef5df5513865194a230e0bf7
LuckyBichar <enbocharov2006@gmail.com>
2025-03-28 03:52:02 +0300

    main

--- bocharov.egor/S2/main.cpp
+++ bocharov.egor/S2/main.cpp
@@ -1,2 +1,29 @@
-int main()
-{}
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include "actions.hpp"
+
+int main(int argc, char * argv[])
+{
+  try
+  {
+    bocharov::queue infixExprs;
+    if (argc > 1)
+    {
+      std::ifstream file(argv[1]);
+      bocharov::inputExprs(file, infixExprs);
+    }
+    else
+    {
+      bocharov::inputExprs(std::cin, infixExprs);
+    }
+    bocharov::queue postfixExprs = bocharov::convertExpr(infixExprs);
+    bocharov::stackNumber results = bocharov::calculationExpr(postfixExprs);
+    bocharov::outputResults(std::cout, results) << "\n";
+  }
+  catch (std::exception & e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
+}
