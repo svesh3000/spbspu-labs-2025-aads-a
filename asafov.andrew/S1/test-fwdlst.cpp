7f342de8a09e900837c21efc042e83601b9dc52a
shycoshy <neforscist@gmail.com>
2025-03-28 12:51:45 +0000

    tests no.197

--- asafov.andrew/S1/test-fwdlst.cpp
+++ asafov.andrew/S1/test-fwdlst.cpp
@@ -26,16 +26,16 @@ BOOST_AUTO_TEST_CASE(cbegin_test)
   BOOST_TEST(*list.cbegin() == 1);
 }
 
-//BOOST_AUTO_TEST_CASE(begin_test)
-//{
-//  asafov::Forward_list<size_t> list;
-//  list.push_back(1);
-//  list.push_back(2);
-//  list.push_back(3);
-//  list.push_back(4);
-//  BOOST_TEST(*list.begin() == 1);
-//}
-
+BOOST_AUTO_TEST_CASE(begin_test)
+{
+  asafov::Forward_list<size_t> list;
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  list.push_back(4);
+  BOOST_TEST(*list.begin() == 1);
+}
+/*
 BOOST_AUTO_TEST_CASE(cend_test)
 {
   asafov::Forward_list<size_t> list;
@@ -160,3 +160,4 @@ BOOST_AUTO_TEST_CASE(swap_test)
   BOOST_TEST(list.front() == 2);
   BOOST_TEST(list2.front() == 1 );
 }
+*/
