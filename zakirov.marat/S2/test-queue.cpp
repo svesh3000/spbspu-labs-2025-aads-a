13b06ba60d63fd0c7fe29589d7da6f9f91980d0e
ZakirovMS <mz.08.08.06@gmail.com>
2025-04-06 01:43:33 +0300

    Addition of stub-test

---
+++ zakirov.marat/S2/test-queue.cpp
@@ -0,0 +1,8 @@
+#include <boost/test/unit_test.hpp>
+#include "queue.hpp"
+
+BOOST_AUTO_TEST_CASE(stub_test)
+{
+  BOOST_TEST(true);
+}
+
---
+++ zakirov.marat/S2/test-stack.cpp
@@ -0,0 +1,7 @@
+#include <boost/test/unit_test.hpp>
+#include "stack.hpp"
+
+BOOST_AUTO_TEST_CASE(stub_test)
+{
+  BOOST_TEST(true);
+}
