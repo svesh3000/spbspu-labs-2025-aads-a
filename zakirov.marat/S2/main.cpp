43fdbcda4861a00ef86c1000cd759bccbf99b322
ZakirovMS <mz.08.08.06@gmail.com>
2025-04-06 13:29:03 +0300

    Error handling in main

--- zakirov.marat/S2/main.cpp
+++ zakirov.marat/S2/main.cpp
@@ -17,17 +17,21 @@ int main(int argc, char ** argv)
   if (argc == 1)
   {
     zakirov::scan_infix(std::cin, stack_qs);
+    if (!std::cin)
+    {
+      std::cerr << "Critical error in getting line" << '\n';
+      return 1;
+    }
   }
   else if (argc == 2)
   {
     std::ifstream file(argv[1]);
+    zakirov::scan_infix(file, stack_qs);
     if (!file)
     {
       std::cerr << "Critical error in file" << '\n';
       return 1;
     }
-
-    zakirov::scan_infix(file, stack_qs);
   }
 
   if (stack_qs.empty())
@@ -54,8 +58,21 @@ int main(int argc, char ** argv)
   stack_qs.pop();
   while (!stack_qs.empty())
   {
-    std::cout << ' ' << zakirov::calculate_postfix_expression(transform_to_postfix(stack_qs.top()));
-    stack_qs.pop();
+    try
+    {
+      std::cout << ' ' << zakirov::calculate_postfix_expression(transform_to_postfix(stack_qs.top()));
+      stack_qs.pop();
+    }
+    catch (const std::invalid_argument & e)
+    {
+      std::cerr << e.what() << '\n';
+      return 1;
+    }
+    catch (const std::overflow_error & e)
+    {
+      std::cerr << e.what() << '\n';
+      return 1;
+    }
   }
 
   std::cout << '\n';
