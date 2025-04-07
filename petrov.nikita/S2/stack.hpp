34b274edbafa2d4223626953c488031935e78164
Nekich06 <p.nikita.73526@gmail.com>
2025-04-07 21:26:40 +0300

    Implemented Stack

--- petrov.nikita/S2/stack.hpp
+++ petrov.nikita/S2/stack.hpp
@@ -2,6 +2,7 @@
 #define STACK_HPP
 
 #include <cstddef>
+#include <memory>
 
 namespace petrov
 {
@@ -11,8 +12,9 @@ namespace petrov
   public:
     using this_t = Stack< T >;
     Stack(): 
-      massive_(nullptr), 
-      size_of_stack(0)
+      massive_(nullptr),
+      capacity_(0), 
+      size_of_stack_(0)
     {}
     Stack(const this_t & rhs);
     Stack(this_t && rhs);
@@ -28,8 +30,130 @@ namespace petrov
     void swap(this_t & rhs) noexcept;
   private:
     T * massive_;
-    size_t size_of_stack;
+    size_t capacity_;
+    size_t size_of_stack_;
   };
-}
 
+  template< typename T >
+  Stack< T >::Stack(const this_t & rhs):
+    massive_(nullptr),
+    capacity_(rhs.capacity_),
+    size_of_stack_(rhs.size_of_stack_)
+  {
+    massive_ = new T[rhs.size_of_stack_];
+    for (size_t i = 0; i < rhs.size_of_stack_; i++)
+    {
+      massive_[i] = rhs.massive_[i];
+    }
+  }
+
+  template< typename T >
+  Stack< T >::Stack(this_t && rhs):
+    massive_(nullptr),
+    capacity_(rhs.capacity_),
+    size_of_stack_(rhs.size_of_stack_)
+  {
+    massive_ = new T[rhs.size_of_stack_];
+    for (size_t i = 0; i < rhs.size_of_stack_; i++)
+    {
+      massive_[i] = rhs.massive_[i];
+    }
+  }
+
+  template< typename T >
+  Stack< T >::~Stack()
+  {
+    delete[] massive_;
+  }
+
+  template< typename T >
+  typename Stack< T >::this_t & Stack< T >::operator=(const this_t & rhs)
+  {
+    Stack< T > cpy(rhs);
+    this->swap(cpy);
+    return *this;
+  }
+
+  template< typename T >
+  typename Stack< T >::this_t & Stack< T >::operator=(this_t && rhs)
+  {
+    this->swap(rhs);
+    return *this;
+  }
+
+  template< typename T >
+  void Stack< T >::push(const T & element)
+  {
+    if (this->empty())
+    {
+      capacity_ = 5;
+      massive_ = new T[capacity_];
+      massive_[size_of_stack_++] = element;
+    }
+    else if (size_of_stack_ == capacity_)
+    {
+      T * temp = massive_;
+      massive_ = new T[capacity_ *= 2];
+      size_t i = 0;
+      while (i < size_of_stack_)
+      {
+        massive_[i] = temp[i];
+        ++i;
+      }
+      delete[] temp;
+      massive_[i] = element;
+      size_of_stack_++;
+    }
+    else
+    {
+      massive_[size_of_stack_++] = element;
+    }
+  }
+
+  template< typename T >
+  void Stack< T >::pop()
+  {
+    if (this->empty())
+    {
+      return;
+    }
+    size_of_stack_--;
+  }
+
+  template< typename T >
+  T & Stack< T >::top()
+  {
+    return massive_[size_of_stack_ - 1];
+  }
+
+  template< typename T >
+  const T & Stack< T >::top() const
+  {
+    if (this->empty())
+    {
+      throw std::logic_error("ERROR: Stack is empty");
+    }
+    return massive_[size_of_stack_ - 1];
+  }
+
+  template< typename T >
+  bool Stack< T >::empty() const noexcept
+  {
+    return !size_of_stack_;
+  }
+
+  template< typename T >
+  size_t Stack< T >::size() const noexcept
+  {
+    return size_of_stack_;
+  }
+
+  template< typename T >
+  void Stack< T >::swap(this_t & rhs) noexcept
+  {
+    std::swap(massive_, rhs.massive_);
+    std::swap(capacity_, rhs.capacity_);
+    std::swap(size_of_stack_, rhs.size_of_stack_);
+  }
+}
 #endif
