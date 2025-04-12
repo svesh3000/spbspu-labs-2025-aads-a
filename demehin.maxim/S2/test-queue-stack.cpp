e7de708945aab1b3daae7c3ecf92825a144be89f
TvoyBatyA12343 <maxdem06@gmail.com>
2025-04-12 17:43:52 +0300

    tests for operators=

--- demehin.maxim/S2/test-queue-stack.cpp
+++ demehin.maxim/S2/test-queue-stack.cpp
@@ -20,6 +20,16 @@ BOOST_AUTO_TEST_CASE(stack_test)
   demehin::Stack< int > mv_stack(std::move(cpy_stack));
   BOOST_TEST(mv_stack.size() == 1);
   BOOST_TEST(mv_stack.top() == 1);
+
+  orig_stack.pop();
+  mv_stack = orig_stack;
+  BOOST_TEST(mv_stack.size() == 1);
+  BOOST_TEST(mv_stack.top() == 1);
+
+  orig_stack.push(3);
+  mv_stack = std::move(orig_stack);
+  BOOST_TEST(mv_stack.size() == 2);
+  BOOST_TEST(mv_stack.top() == 3);
 }
 
 BOOST_AUTO_TEST_CASE(queue_test)
@@ -38,7 +48,19 @@ BOOST_AUTO_TEST_CASE(queue_test)
   BOOST_TEST(cpy_queue.back() == 3);
 
   cpy_queue.pop();
-  demehin::Queue< int > mv_queue(cpy_queue);
+  demehin::Queue< int > mv_queue(std::move(cpy_queue));
   BOOST_TEST(mv_queue.size() == 2);
   BOOST_TEST(mv_queue.front() == 2);
+
+  orig_queue.push(4);
+  mv_queue = orig_queue;
+  BOOST_TEST(mv_queue.size() == 4);
+  BOOST_TEST(mv_queue.front() == 1);
+  BOOST_TEST(mv_queue.back() == 4);
+
+  orig_queue.pop();
+  mv_queue = std::move(orig_queue);
+  BOOST_TEST(mv_queue.size() == 3);
+  BOOST_TEST(mv_queue.front() == 2);
+  BOOST_TEST(mv_queue.back() == 4);
 }
