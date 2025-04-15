90f31024d9e4f9dec6333ed3ec976d6ee5278dce
LuckyBichar <enbocharov2006@gmail.com>
2025-04-15 21:34:37 +0300

    using namespace in main.cpp

--- bocharov.egor/S1/main.cpp
+++ bocharov.egor/S1/main.cpp
@@ -2,18 +2,20 @@
 #include "actions.hpp"
 #include <exception>
 
+using namespace bocharov
+
 int main()
 {
   try
   {
-    bocharov::pairs_list_t pairs_list = bocharov::createList(std::cin);
+    pairs_list_t pairs_list = createList(std::cin);
     if (pairs_list.empty())
     {
       std::cout << 0 << '\n';
       return 0;
     }
-    bocharov::outputList(std::cout, pairs_list);
-    bocharov::print_lists_info(std::cout, pairs_list);
+    outputList(std::cout, pairs_list);
+    print_lists_info(std::cout, pairs_list);
   }
   catch (const std::exception & e)
   {
