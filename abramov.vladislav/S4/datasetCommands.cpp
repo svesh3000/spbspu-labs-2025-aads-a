5027a8725cf2b588398cfa9e630ec2d7d767459f
abramov_vladislav_ <vladabramov06@yandex.ru>
2025-04-20 19:07:52 +0300

    fisrt implementation of dataset commands

---
+++ abramov.vladislav/S4/datasetCommands.cpp
@@ -0,0 +1,82 @@
+#include "datasetCommands.hpp"
+#include <iostream>
+
+std::map< std::string, std::map< int, std::string > > abramov::getDataSets(std::ifstream &in)
+{
+  std::map< std::string, std::map< int, std::string > > collection;
+  while (in)
+  {
+    std::string name;
+    in >> name;
+    std::map< int, std::string > dict;
+    int k;
+    std::string s;
+    while(in && in.peek() != '\n')
+    {
+      in >> k >> s;
+      dict[k] = s;
+    }
+    collection[name] = dict;
+  }
+  return collection;
+}
+
+void abramov::printDataSet(const std::string &name, const std::map< std::string, std::map< int, std::string > > &dicts)
+{
+  std::cout << name << " ";
+  std::map< int, std::string > dict = dicts.find(name)->second;
+  for (auto it = dict.begin(); it != --dict.end(); ++it)
+  {
+    std::cout << it->first << " " << it->second << " ";
+  }
+  std::cout << (--dict.end())->first << (--dict.end())->second << "\n";
+}
+
+std::map< int, std::string > abramov::complementDataSets(const std::string &s1, const std::string &s2, const std::map< std::string, std::map< int, std::string > > &dicts)
+{
+  std::map< int, std::string > dict1 = dicts.find(s1)->second;
+  std::map< int, std::string > dict2 = dicts.find(s2)->second;
+  std::map< int, std::string > res = std::map< int, std::string >();
+  for (auto it = dict1.begin(); it != dict1.end(); ++it)
+  {
+    if (dict2.find(it->first) == dict2.end())
+    {
+      res[it->first] = it->second;
+    }
+  }
+  return res;
+}
+
+std::map< int, std::string > abramov::intersectDataSets(const std::string &s1, const std::string &s2, const std::map< std::string, std::map< int, std::string > > &dicts)
+{
+  std::map< int, std::string > dict1 = dicts.find(s1)->second;
+  std::map< int, std::string > dict2 = dicts.find(s2)->second;
+  std::map< int, std::string > res = std::map< int, std::string >();
+  for (auto it = dict1.begin(); it != dict1.end(); ++it)
+  {
+    if (dict2.find(it->first) != dict2.end())
+    {
+      res[it->first] = it->second;
+    }
+  }
+  return res;
+}
+
+std::map< int, std::string > abramov::unionDataSets(const std::string &s1, const std::string &s2, const std::map< std::string, std::map< int, std::string > > &dicts)
+{
+  std::map< int, std::string > dict1 = dicts.find(s1)->second;
+  std::map< int, std::string > dict2 = dicts.find(s2)->second;
+  std::map< int, std::string > res = std::map< int, std::string >();
+  for (auto it = dict1.begin(); it != dict1.end(); ++it)
+  {
+    res[it->first] = it->second;
+  }
+  for (auto it = dict2.begin(); it != dict2.end(); ++it)
+  {
+    if (res.find(it->first) != res.end())
+    {
+      res[it->first] = it->second;
+    }
+  }
+  return res;
+}
---
+++ abramov.vladislav/S4/datasetCommands.hpp
@@ -0,0 +1,15 @@
+#ifndef DATASETCOMMANDS_HPP
+#define DATASETCOMMANDS_HPP
+#include <map>
+#include <string>
+#include <fstream>
+
+namespace abramov
+{
+  std::map< std::string, std::map< int, std::string > > getDataSets(std::ifstream &in);
+  void printDataSet(const std::string &s, const std::map< std::string, std::map< int, std::string > > &dicts);
+  std::map< int, std::string > complementDataSets(const std::string &s1, const std::string &s2, const std::map< std::string, std::map< int, std::string > > &dicts);
+  std::map< int, std::string > intersectDataSets(const std::string &s1, const std::string &s2, const std::map< std::string, std::map< int, std::string > > &dicts);
+  std::map< int, std::string > unionDataSets(const std::string &s1, const std::string &s2, const std::map< std::string, std::map< int, std::string > > &dicts);
+}
+#endif
