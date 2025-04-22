39f3dbddf362068645962147cb3e75227e3cd664
Maksim_Shramko <maxramko@gmail.com>
2025-04-22 12:51:08 +0300

    list.hpp remake

--- shramko.maksim/S1/list.hpp
+++ shramko.maksim/S1/list.hpp
@@ -4,6 +4,8 @@
 #include "node.hpp"
 #include "iterator.hpp"
 #include <cstddef>
+#include <utility>
+#include <stdexcept>
 
 namespace shramko
 {
@@ -11,32 +13,41 @@ namespace shramko
   class FwdList
   {
   public:
+    using iterator = FwdListIterator< T, false >;
+    using const_iterator = FwdListIterator< T, true >;
+
     FwdList();
-    FwdList(const FwdList& rhs);
-    FwdList(FwdList&& rhs);
     ~FwdList();
+    FwdList(const FwdList& rhs);
+    FwdList(FwdList&& rhs) noexcept;
+    FwdList& operator=(FwdList rhs);
+    FwdList& operator=(FwdList&& rhs);
 
-    FwdList< T > begin() const noexcept;
-    FwdList< T > end() const noexcept;
-
-    FwdList& operator=(const FwdList& rhs) const noexcept;
-    FwdList& operator=(FwdList&& rhs) const noexcept;
+    iterator begin() const noexcept;
+    iterator end() const noexcept;
+    const_iterator cbegin() const noexcept;
+    const_iterator cend() const noexcept;
 
     T& front() const;
     T& back() const;
-
-    std::size_t size() const noexcept;
+    size_t size() const noexcept;
     bool empty() const noexcept;
 
+    void push_front(const T& value);
     void push_front(T&& value);
+    void push_back(const T& value);
+    void push_back(T&& value);
     void pop_front() noexcept;
     void swap(FwdList& other) noexcept;
     void clear() noexcept;
-  
+
   private:
     FwdListNode< T >* head_;
     FwdListNode< T >* tail_;
-    std::size_t size_;
+    size_t size_;
+
+    void pushFrontNode(FwdListNode< T >* node);
+    void pushBackNode(FwdListNode< T >* node);
   };
 }
 
@@ -49,26 +60,51 @@ shramko::FwdList< T >::FwdList():
   tail_->next_ = tail_;
 }
 
-shramko::FwdList< T >::FwdList():
-  FwdList();
+template< typename T >
+shramko::FwdList< T >::FwdList(const FwdList& rhs):
+  FwdList()
 {
-  FwdList< T > revers;
-  for (auto i = rhs.begin(); i != rhs.end(); ++i)
+  FwdList< T > temp;
+  for (auto it = rhs.begin(); it != rhs.end(); ++it)
   {
-    revers.push_front(*i);
+    temp.push_front(*it);
   }
-  for (auto i = revers.begin(); i != revers.end(); ++i)
+  for (auto it = temp.begin(); it != temp.end(); ++it)
   {
-    push_front(*i);
+    push_front(*it);
   }
 }
 
-template<typename T>
-shramko::FwdList<T>::FwdList(FwdList&& rhs):
-  head_{std::exchange(rhs.head_, nullptr)}, 
-  tail_{std::exchange(rhs.tail_, nullptr)},
-  size_{std::exchange(rhs.size_, 0)}
-{}
+template< typename T >
+shramko::FwdList< T >::FwdList(FwdList&& rhs) noexcept:
+  head_(std::exchange(rhs.head_, nullptr)),
+  tail_(std::exchange(rhs.tail_, new FwdListNode< T >(T()))),
+  size_(std::exchange(rhs.size_, 0))
+{
+  tail_->next_ = tail_;
+}
+
+template< typename T >
+shramko::FwdList< T >& shramko::FwdList< T >::operator=(FwdList rhs)
+{
+  swap(rhs);
+  return *this;
+}
+
+template< typename T >
+shramko::FwdList< T >& shramko::FwdList< T >::operator=(FwdList&& rhs)
+{
+  if (this != &rhs)
+  {
+    clear();
+    delete tail_;
+    head_ = std::exchange(rhs.head_, nullptr);
+    tail_ = std::exchange(rhs.tail_, new FwdListNode< T >(T()));
+    size_ = std::exchange(rhs.size_, 0);
+    tail_->next_ = tail_;
+  }
+  return *this;
+}
 
 template< typename T >
 shramko::FwdList< T >::~FwdList()
@@ -78,65 +114,132 @@ shramko::FwdList< T >::~FwdList()
 }
 
 template< typename T >
-shramko::FwdIterator< T > shramko::FwdList< T >::begin() const noexcept
+typename shramko::FwdList< T >::iterator shramko::FwdList< T >::begin() const noexcept
 {
-  return FwdIterator< T >(head_);
+  return iterator(head_);
 }
 
 template< typename T >
-shramko::FwdIterator< T > shramko::FwdList< T >::end()
+typename shramko::FwdList< T >::iterator shramko::FwdList< T >::end() const noexcept
 {
-  return FwdIterator< T >(tail_);
+  return iterator(tail_);
+}
+
+template< typename T >
+typename shramko::FwdList< T >::const_iterator shramko::FwdList< T >::cbegin() const noexcept
+{
+  return const_iterator(head_);
+}
+
+template< typename T >
+typename shramko::FwdList< T >::const_iterator shramko::FwdList< T >::cend() const noexcept
+{
+  return const_iterator(tail_);
 }
 
 template< typename T >
 T& shramko::FwdList< T >::front() const
 {
-  return assert(head_);
+  if (empty())
+  {
+    throw std::logic_error("List is empty");
+  }
+  return head_->data_;
 }
 
 template< typename T >
 T& shramko::FwdList< T >::back() const
 {
-  FwdIterator< T > it = begin();
-  for (; it != end(); ++it)
-  return *it
+  if (empty())
+  {
+    throw std::logic_error("List is empty");
+  }
+  FwdListNode< T >* current = head_;
+  while (current->next_ != tail_)
+  {
+    current = current->next_;
+  }
+  return current->data_;
 }
 
 template< typename T >
-std::size_t shramko::FwdList< T >::size() const
+size_t shramko::FwdList< T >::size() const noexcept
 {
   return size_;
 }
 
 template< typename T >
-bool shramko::FwdList< T >::empty() const
+bool shramko::FwdList< T >::empty() const noexcept
 {
   return size_ == 0;
 }
 
 template< typename T >
-void shramko::FwdList< T >::push_front(T&& value)
+void shramko::FwdList< T >::pushFrontNode(FwdListNode< T >* node)
 {
-  FwdListNode< T >* new_node = new FwdListNode< T >(std::move(value));
-  new_node->next_ = empty() ? tail_ : head_;
-  tail_->next_ = new_node;
-  head_ = new_node;
+  node->next_ = empty() ? tail_ : head_;
+  tail_->next_ = node;
+  head_ = node;
   ++size_;
 }
 
 template< typename T >
-void shramko::FwdList< T >::pop_front()
+void shramko::FwdList< T >::push_front(const T& value)
 {
-  FwdListNode< T >* new_head = head_->next_;
-  delete head_;
-  tail_->next_ = new_head;
-  head_ = new_head;
-  --size_;
+  pushFrontNode(new FwdListNode< T >(value));
 }
 
 template< typename T >
-void shramko::FwdList< T >::swap(FwdList& other)
+void shramko::FwdList< T >::push_front(T&& value)
+{
+  pushFrontNode(new FwdListNode< T >(std::move(value)));
+}
+
+template< typename T >
+void shramko::FwdList< T >::pushBackNode(FwdListNode< T >* node)
+{
+  if (empty())
+  {
+    pushFrontNode(node);
+    return;
+  }
+  FwdListNode< T >* current = head_;
+  while (current->next_ != tail_)
+  {
+    current = current->next_;
+  }
+  current->next_ = node;
+  node->next_ = tail_;
+  ++size_;
+}
+
+template< typename T >
+void shramko::FwdList< T >::push_back(const T& value)
+{
+  pushBackNode(new FwdListNode< T >(value));
+}
+
+template< typename T >
+void shramko::FwdList< T >::push_back(T&& value)
+{
+  pushBackNode(new FwdListNode< T >(std::move(value)));
+}
+
+template< typename T >
+void shramko::FwdList< T >::pop_front() noexcept
+{
+  if (!empty())
+  {
+    FwdListNode< T >* temp = head_;
+    head_ = head_->next_;
+    tail_->next_ = head_;
+    delete temp;
+    --size_;
+  }
+}
+
+template< typename T >
+void shramko::FwdList< T >::swap(FwdList& other) noexcept
 {
   std::swap(head_, other.head_);
   std::swap(tail_, other.tail_);
@@ -144,9 +247,10 @@ void shramko::FwdList< T >::swap(FwdList& other)
 }
 
 template< typename T >
-void shramko::FwdList< T >::clear()
+void shramko::FwdList< T >::clear() noexcept
 {
-  while (!empty()) {
+  while (!empty())
+  {
     pop_front();
   }
 }
