ec71a3f828226cf8c49aa300d52ebbf089f89b2a
Ksenia-rgb <ksenialymova@mail.ru>
2025-04-22 03:12:03 +0300

    CG

--- alymova.ksenia/S4/utils.hpp
+++ alymova.ksenia/S4/utils.hpp
@@ -7,7 +7,7 @@ namespace alymova
 {
   using dict_t = std::map< size_t, std::string, std::less< size_t > >;
   using all_dict_t = std::map< std::string, dict_t, std::less< std::string > >;
-  
+
   void print(std::ostream& out, const all_dict_t& dicts, const std::string& dataset);
   dict_t complement(all_dict_t& dicts, const std::string& newdataset,
     const std::string& dataset1, const std::string& dataset2);
