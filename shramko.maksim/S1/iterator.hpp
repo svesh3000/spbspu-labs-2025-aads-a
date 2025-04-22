855a947b9089b13ac63479fccdbaf57f53f4851f
Maksim_Shramko <maxramko@gmail.com>
2025-04-22 13:13:16 +0300

    Remake for iterator.hpp

--- shramko.maksim/S1/iterator.hpp
+++ shramko.maksim/S1/iterator.hpp
@@ -3,20 +3,39 @@
 
 #include <iterator>
 #include <type_traits>
+#include <cassert>
 #include "node.hpp"
 
 namespace shramko
 {
   template< typename T >
-  class List;
+  class FwdList;
 
   template< typename T, bool is_const >
-  class FwdListIterator final: public std::iterator< std::forward_iterator_tag, int >
+  class FwdListIterator : public std::iterator<std::forward_iterator_tag, T>
   {
   public:
+    using this_t = FwdListIterator<T, is_const>;
+    using node_type = typename std::conditional<is_const, const FwdListNode<T>, FwdListNode<T>>::type;
+    using value_type = typename std::conditional<is_const, const T, T>::type;
+    
     FwdListIterator() noexcept = default;
+    FwdListIterator(node_type* node) noexcept : node_(node) {}
     FwdListIterator(const this_t&) noexcept = default;
-    ~FwdListIterator() = default;
+    
+    this_t& operator=(const this_t&) noexcept = default;
+    
+    value_type& operator*() const noexcept
+    {
+      assert(node_ != nullptr);
+      return node_->data_;
+    }
+
+    value_type* operator->() const noexcept
+    {
+      assert(node_ != nullptr);
+      return std::addressof(node_->data_);
+    }
 
     this_t& operator++() noexcept
     {
@@ -27,38 +46,24 @@ namespace shramko
 
     this_t operator++(int) noexcept
     {
-      assert(node_ != nullptr);
-      FwdListIterator< T, is_const > res = *this;
+      this_t tmp = *this;
       ++(*this);
-      return res;
+      return tmp;
     }
 
-    typename std::conditional< is_const, const T&, T& >::type operator*()const noexcept
+    bool operator==(const this_t& rhs) const noexcept
     {
-      return node_->data_
+      return node_ == rhs.node_;
     }
 
-    typename std::conditional< is_const, const T *, T * >::type operator->() const noexcept
-    {
-      return std::addressof(node_->data_);
-    }
-
-    typename std::conditional< is_const, const T *, T * >::type operator->() const noexcept
-    {
-      return std::addressof(node_->data_);
-    }
-
-    bool operator!=(const Iterator & rhs) const noexcept
+    bool operator!=(const this_t& rhs) const noexcept
     {
       return !(*this == rhs);
     }
 
   private:
-    friend class List< T >;
-    FwdListNode< T >* node_;
-    FwdListIterator(FwdListNode< T >* node) noexcept:
-      node_(node)
-    {}
+    friend class FwdList<T>;
+    node_type* node_ = nullptr;
   };
 }
 
