f66d323d7bb80472feaea26e772ccb324e73afbd
faxryzen <cheburek2006@gmail.com>
2025-04-25 01:32:06 +0300

    Fixed O(n) pop

--- savintsev.ilya/S2/dynamic-array.hpp
+++ savintsev.ilya/S2/dynamic-array.hpp
@@ -17,10 +17,10 @@ namespace savintsev
     bool empty() const noexcept;
     size_t size() const noexcept;
 
-    const T & front() const noexcept;
-    T & front() noexcept;
-    const T & back() const noexcept;
-    T & back() noexcept;
+    const T & front() const;
+    T & front();
+    const T & back() const;
+    T & back();
 
     template< typename U >
     void push_back(U && rhs);
@@ -32,6 +32,7 @@ namespace savintsev
   private:
     T * data_ = nullptr;
     size_t size_ = 0;
+    size_t start_ = 0;
     size_t capacity_ = 0;
   };
 
@@ -40,6 +41,7 @@ namespace savintsev
   {
     std::swap(x.data_, y.data_);
     std::swap(x.size_, y.size_);
+    std::swap(x.start, y.start_);
     std::swap(x.capacity_, y.capacity_);
   }
 
@@ -47,6 +49,7 @@ namespace savintsev
   Array< T >::Array(size_t n):
     data_(new T[n]),
     size_(0),
+    start_(0),
     capacity_(n)
   {}
 
@@ -54,6 +57,7 @@ namespace savintsev
   Array< T >::Array(const Array & rhs):
     data_(new T[rhs.capacity_]),
     size_(rhs.size_),
+    start_(rhs.start_),
     capacity_(rhs.capacity_)
   {
     try
@@ -100,25 +104,25 @@ namespace savintsev
   }
 
   template< typename T >
-  const T & Array< T >::front() const noexcept
+  const T & Array< T >::front() const
   {
-    return data_[0];
+    return data_[start_];
   }
 
   template< typename T >
-  T & Array< T >::front() noexcept
+  T & Array< T >::front()
   {
-    return data_[0];
+    return data_[start_];
   }
 
   template< typename T >
-  const T & Array< T >::back() const noexcept
+  const T & Array< T >::back() const
   {
     return data_[size_ - 1];
   }
 
   template< typename T >
-  T & Array< T >::back() noexcept
+  T & Array< T >::back()
   {
     return data_[size_ - 1];
   }
@@ -127,13 +131,13 @@ namespace savintsev
   template< typename U >
   void Array< T >::push_back(U && rhs)
   {
-    if (size_ < capacity_)
+    if (size_ + start_ < capacity_)
     {
-      data_[size_] = std::forward< U >(rhs);
+      data_[size_ + start_] = std::forward< U >(rhs);
       size_++;
       return;
     }
-    T * arr = createExpandCopy(data_, capacity_, capacity_ + capacity_);
+    T * arr = createExpandCopy(data_ + start_, capacity_, capacity_ + capacity_);
     try
     {
       arr[size_] = std::forward< U >(rhs);
@@ -146,6 +150,7 @@ namespace savintsev
     delete[] data_;
     data_ = arr;
     capacity_ += capacity_;
+    start_ = 0;
     size_++;
   }
 
@@ -158,11 +163,8 @@ namespace savintsev
   template< typename T >
   void Array< T >::pop_front()
   {
-    for (size_t i = 0; i < size_ - 1; ++i)
-    {
-      data_[i] = data_[i + 1];
-    }
-    size_--;
+    ++start_;
+    --size_;
   }
 }
 
