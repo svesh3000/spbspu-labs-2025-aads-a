2baa2efea227b326da8160ef4ce8e69915ec151a
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-04-25 22:55:25 +0300

    bitree default constructor + clean

--- maslov.aleksey/S4/main.cpp
+++ maslov.aleksey/S4/main.cpp
@@ -174,7 +174,7 @@ int main(int argc, char* argv[])
   }
   catch (const std::exception & e)
   {
-    std::cerr << e.what() << "\n";
+    std::cerr << e.what() << '\n';
     return 1;
   }
 }
--- maslov.aleksey/S4/tree.hpp
+++ maslov.aleksey/S4/tree.hpp
@@ -1,4 +1,4 @@
-/*#ifndef TREE_HPP
+#ifndef TREE_HPP
 #define TREE_HPP
 
 #include "node.hpp"
@@ -30,9 +30,53 @@ namespace maslov
     void clear();
     void swap(BiTree< Key, T, Cmp > & rhs) noexcept;
    private:
-    BiTreeNode< Key, T > * node_;
     BiTreeNode< Key, T > * fakeRoot_;
     BiTreeNode< Key, T > * fakeLeaf_;
+    size_t size_;
+    void clearTree(BiTreeNode< Key, T > * root);
   };
+
+  template< typename Key, typename T, typename Cmp >
+  BiTree< Key, T, Cmp >::BiTree():
+    fakeRoot_(new BiTreeNode< Key, T >{std::pair< const Key, Value >(), nullptr, nullptr, nullptr}),
+    fakeLeaf_(new BiTreeNode< Key, T >{std::pair< const Key, Value >(), nullptr, nullptr, nullptr})
+    size_(0)
+  {
+    fakeLeaf_->left = fakeLeaf_;
+    fakeLeaf_->right = fakeLeaf_;
+    fakeLeaf_->parent = fakeRoot_;
+    fakeRoot_->left = fakeLeaf_;
+    fakeRoot_->parent = fakeLeaf_;
+  }
+
+  template< typename Key, typename T, typename Cmp >
+  BiTree< Key, T, Cmp >::~BiTree()
+  {
+    clear();
+    delete fakeRoot_;
+    delete fakeLeaf_;
+  }
+
+  template< typename Key, typename T, typename Cmp >
+  void BiTree< Key, T, Cmp >::clear()
+  {
+    clearTree(fakeRoot_->left);
+    clearTree(fakeRoot_->right);
+    fakeRoot_->left = fakeLeaf_;
+    fakeRoot_->right = fakeLeaf_;
+    size_ = 0;
+  }
+
+  template< typename Key, typename T, typename Cmp >
+  void BiTree< Key, T, Cmp >::clearTree(BiTreeNode< Key, T > * root)
+  {
+    if (root == fakeLeaf_)
+    {
+      return;
+    }
+    clearTree(node->left);
+    clearTree(node->right);
+    delete root;
+  }
 }
-#endif*/
+#endif
