3741ae8dd888dbae240b3c8231737556fc580f3d
ZakirovMS <mz.08.08.06@gmail.com>
2025-04-28 23:41:38 +0300

    intersect function implementation

--- zakirov.marat/S4/LineProcessing.cpp
+++ zakirov.marat/S4/LineProcessing.cpp
@@ -18,19 +18,44 @@ void zakirov::print_d(std::ostream & out, std::string name, const std::map< K, T
 }
 
 template < class K, class T >
-void zakirov::complement_d(std::map< K, T > & result_d, std::map< K, T > & first_d, std::map< K, T > & second_d)
+void zakirov::complement_d(std::map< K, T > & result_d, const std::map< K, T > & first_d, const std::map< K, T > & second_d)
 {
 
 }
 
 template < class K, class T >
-void zakirov::intersect_d(std::map< K, T > & result_d, std::map< K, T > & first_d, std::map< K, T > & second_d)
+void zakirov::intersect_d(std::map< K, T > & result_d, const std::map< K, T > & first_d, const std::map< K, T > & second_d)
 {
+  result_d.clear();
+  if (first_d.empty() || second_d.empty())
+  {
+    return;
+  }
 
+  auto first_iter = first_d.cbegin();
+  auto second_iter = second_d.cbegin();
+
+  while (first_iter != first_d.cend() && second_iter != second_d.cend())
+  {
+    if (first_iter->first < second_iter->first)
+    {
+      ++first_iter;
+    }
+    else if (first_iter->first > second_iter->first)
+    {
+      ++second_iter;
+    }
+    else
+    {
+      result_d.emplace(first_iter->first, first_iter->second);
+      ++second_iter;
+      ++first_iter;
+    }
+  }
 }
 
 template < class K, class T >
-void zakirov::union_d(std::map< K, T > & result_d, std::map< K, T > & first_d, std::map< K, T > & second_d)
+void zakirov::union_d(std::map< K, T > & result_d, const std::map< K, T > & first_d, const std::map< K, T > & second_d)
 {
 
 }
--- zakirov.marat/S4/LineProcessing.hpp
+++ zakirov.marat/S4/LineProcessing.hpp
@@ -9,11 +9,11 @@ namespace zakirov
   template < class K, class T >
   void print_d(std::ostream & out, std::string name, const std::map< K, T > & dictionary);
   template < class K, class T >
-  void complement_d(std::map< K, T > & result_d, std::map< K, T > & first_d, std::map< K, T > & second_d);
+  void complement_d(std::map< K, T > & result_d, const std::map< K, T > & first_d, const std::map< K, T > & second_d);
   template < class K, class T >
-  void intersect_d(std::map< K, T > & result_d, std::map< K, T > & first_d, std::map< K, T > & second_d);
+  void intersect_d(std::map< K, T > & result_d, const std::map< K, T > & first_d, const std::map< K, T > & second_d);
   template < class K, class T >
-  void union_d(std::map< K, T > & result_d, std::map< K, T > & first_d, std::map< K, T > & second_d);
+  void union_d(std::map< K, T > & result_d, const std::map< K, T > & first_d, const std::map< K, T > & second_d);
 }
 
 #endif
