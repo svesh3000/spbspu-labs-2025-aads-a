77d158c83682a1425d8664e21b5a1d368d73657b
ZakirovMS <mz.08.08.06@gmail.com>
2025-04-28 23:45:14 +0300

    complement function implementation

--- zakirov.marat/S4/LineProcessing.cpp
+++ zakirov.marat/S4/LineProcessing.cpp
@@ -20,7 +20,38 @@ void zakirov::print_d(std::ostream & out, std::string name, const std::map< K, T
 template < class K, class T >
 void zakirov::complement_d(std::map< K, T > & result_d, const std::map< K, T > & first_d, const std::map< K, T > & second_d)
 {
+  if (first_d.empty())
+  {
+    result_d = second_d;
+    return
+  }
+  else
+  {
+    result_d = first_d;
+    return
+  }
 
+  auto first_iter = first_d.cbegin();
+  auto second_iter = second_d.cbegin();
+
+  while (first_iter != first_d.cend() && second_iter != second_d.cend())
+  {
+    if (first_iter->first < second_iter->first)
+    {
+      result_d.emplace(first_iter->first, first_iter->second);
+      ++first_iter;
+    }
+    else if (first_iter->first > second_iter->first)
+    {
+      result_d.emplace(second_iter->first, second_iter->second);
+      ++second_iter;
+    }
+    else
+    {
+      ++second_iter;
+      ++first_iter;
+    }
+  }
 }
 
 template < class K, class T >
