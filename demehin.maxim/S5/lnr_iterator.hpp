062fe62f9cfa7d548137c2583763cd129265d1bb
TvoyBatyA12343 <maxdem06@gmail.com>
2025-04-29 18:28:56 +0300

    lnr iter impl (mb)

--- demehin.maxim/S5/lnr_iterator.hpp
+++ demehin.maxim/S5/lnr_iterator.hpp
@@ -1,5 +1,8 @@
 #ifndef LNR_ITERATOR_HPP
 #define LNR_ITERATOR_HPP
+#include <cassert>
+#include <tree/node.hpp>
+#include <stack.hpp>
 
 namespace demehin
 {
@@ -34,8 +37,89 @@ namespace demehin
     node_t* node_;
     stack_t stack_;
 
-    explicit LnrIterator(node_t* node);
+    explicit LnrIterator(node_t*) noexcept;
+  }
+
+  template< typename Key, typename T, typename Cmp, bool isConst >
+  LnrIterator< Key, T, Cmp, isConst >::LnrIterator() noexcept:
+    node_(nullptr),
+    stack_()
+  {}
+
+  template< typename Key, typename T, typename Cmp, bool isConst >
+  LnrIterator< Key, T, Cmp, isConst >::LnrIterator(node_t* node) noexcept:
+    node_(node),
+    stack_()
+  {}
+
+  template< typename Key, typename T, typename Cmp, bool isConst >
+  typename LnrIterator< Key, T, Cmp, isConst >::this_t& LnrIterator< Key, T, Cmp, isConst >::operator++() noexcept
+  {
+    if (node_->right != nullptr)
+    {
+      stack_.push(node_);
+      node_ = node_->right;
+      while (node_->left != nullptr)
+      {
+        node_ = node_->left;
+      }
+    }
+    else
+    {
+      while (stack_.top()->right == node_)
+      {
+        node_ = stack_.top();
+        stack.pop();
+        if (stack_.empty())
+        {
+          node_ = nullptr;
+          return *this;
+        }
+      }
+      while (!stack_.empty() && stack_.top()->left == node_)
+      {
+        node_ = stack_.top();
+        stack_.pop()
+      }
+    }
+
+    return *this;
+  }
+
+  template< typename Key, typename T, typename Cmp, bool isConst >
+  typename LnrIterator< Key, T, Cmp, isConst >::this_t LnrIterator< Key, T, Cmp, isConst >::operator++(int) noexcept
+  {
+    this_t res(*this);
+    ++(*this);
+    return res;
+  }
+
+  template< typename Key, typename T, typename Cmp, bool isConst >
+  typename LnrIterator< Key, T, Cmp, isConst >::data_t& LnrIterator< Key, T, Cmp, isConst >::operator*() const noexcept
+  {
+    assert(node_ != nullptr);
+    return node_->data;
+  }
+
+  template< typename Key, typename T, typename Cmp, bool isConst >
+  typename LnrIterator< Key, T, Cmp, isConst >::data_t* LnrIterator< Key, T, Cmp, isConst >::operator->() const noexcept
+  {
+    assert(node_ != nullptr);
+    return std::addressof(node_->data);
+  }
+
+  template< typename Key, typename T, typename Cmp, bool isConst >
+  bool LnrIterator< Key, T, Cmp, isConst >::operator==(const this_t& rhs) const noexcept
+  {
+    return node_ == rhs.node_;
+  }
+
+  template< typename Key, typename T, typename Cmp, bool isConst >
+  bool LnrIterator< Key, T, Cmp, isConst >::operator!=(const this_t& rhs) const noexcept
+  {
+    return !(*this == rhs);
   }
 }
 
 #endif
+
