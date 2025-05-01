42267a4ea5d09ee8bdfc63d8f7974a8fa8b8ca68
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2025-05-01 04:45:19 +0300

    ArrayBuffer, now without dublication in Stack and Queue

--- gavrilova.polina/S2/Queue.hpp
+++ gavrilova.polina/S2/Queue.hpp
@@ -4,120 +4,54 @@
 #include <cstddef>
 #include <utility>
 #include <stdexcept>
+#include "ArrayBuffer.hpp"
 
 namespace gavrilova {
   template < class T >
   class Queue {
   public:
-    Queue();
-    Queue(const Queue< T >& other);
-    Queue(Queue< T >&& other);
-    ~Queue();
-
-    Queue& operator=(const Queue& other);
-    Queue& operator=(Queue&& other);
-
+    Queue() = default;
+    
     void push(const T& value);
     void push(T&& value);
+    void pop();
 
     T& front();
-    T& back();
-    void pop();
+    const T& front() const;
+    
     bool empty() const;
     size_t size() const;
     void swap(Queue& other) noexcept;
+    void clear();
 
   private:
-    size_t size_;
-    size_t capacity_;
-    T* queue_;
-
-    void resize();
+    ArrayBuffer< T > buffer_;
+    size_t head_ = 0;
   };
 
-  template < class T >
-  Queue< T >::Queue():
-    size_(0),
-    capacity_(50),
-    queue_(new T[capacity_])
-  {}
-
-  template < class T >
-  Queue< T >::Queue(const Queue< T >& other):
-    size_(other.size_),
-    capacity_(other.capacity_),
-    queue_(new T[capacity_])
-  {
-    for  (size_t i = 0; i < size_; ++i) {
-      queue_[i] = other.queue_[i];
-    }
-  }
-
-  template < class T >
-  Queue< T >::Queue(Queue<T>&& other):
-    size_(other.size_),
-    capacity_(other.capacity_),
-    queue_(other.queue_)
-  {
-    other.size_ = 0;
-    other.capacity_ = 0;
-    other.queue_ = nullptr;
-  }
-
-  template < class T >
-  Queue< T >::~Queue()
-  {
-    delete[] queue_;
-  }
-
-  template < class T >
-  Queue< T >& Queue< T >::operator=(const Queue& other)
-  {
-    if (this != std::addressof(other)) {
-      Queue cpy(other);
-      swap(cpy);
-    }
-    return *this;
-  }
-
-  template < class T >
-  Queue< T >& Queue< T >::operator=(Queue&& other)
-  {
-    if (this != std::addressof(other)) {
-      swap(other);
-      delete[] other.queue_;
-    }
-    return *this;
-  }
 
   template < class T >
   void Queue< T >::push(const T& value)
   {
-    if (size_ == capacity_) {
-      resize();
-    }
-    queue_[size_++] = value;
+    buffer_.push_back(value);
   }
 
   template < class T >
   void Queue< T >::push(T&& value)
   {
-    if (size_ == capacity_) {
-      resize();
-    }
-    queue_[size_++] = std::move(value);
+    buffer_.push_back(std::move(value));
   }
 
   template < class T >
   T& Queue< T >::front()
   {
-    return queue_[0];
+    return buffer_[head_];
   }
 
   template < class T >
-  T& Queue< T >::back()
+  const T& Queue< T >::front() const
   {
-    return queue_[size_ - 1];
+    return buffer_[head_];
   }
 
   template < class T >
@@ -126,42 +60,36 @@ namespace gavrilova {
     if (empty()) {
       throw std::out_of_range("Stack is empty");
     }
-    for (size_t i = 1; i < size_; ++i) {
-      queue_[i-1] = std::move(queue_[i]);
+    if (++head_ >= buffer_.size())
+    {
+      clear();
     }
-    --size_;
   }
 
   template < class T >
   bool Queue< T >::empty() const
   {
-    return !size_;
+    return head_ >= buffer_.size();
   }
 
   template < class T >
   size_t Queue< T >::size() const
   {
-    return size_;
+    return buffer_.size() - head_;
   }
 
   template < class T >
   void Queue< T >::swap(Queue& other) noexcept
   {
-    std::swap(size_, other.size_);
-    std::swap(capacity_, other.capacity_);
-    std::swap(queue_, other.queue_);
+    buffer_.swap(other.buffer_);
+    std::swap(head_, other.head_);
   }
 
-  template < class T >
-  void Queue< T >::resize()
+  template <class T>
+  void Queue< T >::clear()
   {
-    T* new_queue = new T[capacity_+50];
-    for (size_t i = 0; i < size_; ++i) {
-      new_queue[i] = queue_[i];
-    }
-    delete[] queue_;
-    queue_ = new_queue;
-    capacity_ += 50;
+    buffer_.clear();
+    head_ = 0;
   }
 }
 
--- gavrilova.polina/S2/test-queue.cpp
+++ gavrilova.polina/S2/test-queue.cpp
@@ -1,6 +1,6 @@
 #include <boost/test/unit_test.hpp>
 #include "Queue.hpp"
-
+#include <iostream>
 BOOST_AUTO_TEST_CASE(test_element_access_queue)
 {
   gavrilova::Queue< int > queue{};
@@ -8,7 +8,6 @@ BOOST_AUTO_TEST_CASE(test_element_access_queue)
   queue.push(2);
   queue.push(1 + 2);
   BOOST_TEST(queue.front() == 1);
-  BOOST_TEST(queue.back() == 3);
 }
 
 BOOST_AUTO_TEST_CASE(test_constructors_queue)
@@ -18,8 +17,7 @@ BOOST_AUTO_TEST_CASE(test_constructors_queue)
   queue1.push(2);
   queue1.push(1 + 2);
   gavrilova::Queue< int > queue2 = queue1;
-  BOOST_TEST(queue1.front() == queue2.front());
-  BOOST_TEST(queue1.back() == queue2.back());
+  BOOST_TEST(queue1.front() == 1);
   BOOST_TEST(queue1.size() == queue2.size());
 }
 
@@ -31,8 +29,8 @@ BOOST_AUTO_TEST_CASE(test_assignment_operators_queue)
   queue1.push(1 + 2);
   gavrilova::Queue< int > queue2;
   queue2 = queue1;
-  BOOST_TEST(queue1.front() == queue2.front());
-  BOOST_TEST(queue1.back() == queue2.back());
+  BOOST_TEST(queue1.front() == 1);
+  BOOST_TEST(queue2.front() == 1);
   BOOST_TEST(queue1.size() == queue2.size());
 }
 
