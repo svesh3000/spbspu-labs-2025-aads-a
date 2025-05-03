3fdc7b66a266ca6a9f01e7a2c709f1073bba2057
brev1ch <flirktv@gmail.com>
2025-05-03 21:25:45 +0300

    main.cpp

--- brevnov.nikita/S1/main.cpp
+++ brevnov.nikita/S1/main.cpp
@@ -1,2 +1,29 @@
+#include <exception>
+#include "listact.hpp"
 int main()
-{}
+{
+  try
+  {
+    brevnov::list listpair = brevnov::createList(std::cin);
+    if (listpair.empty())
+    {
+      std::cout << "0\n";
+      return 0;
+    }
+    outputName(std::cout, listpair) << "\n";
+    if (listpair.front().second.empty())
+    {
+      std::cout << "0\n";
+      return 0;
+    }
+    outputNumbers(std::cout, listpair);
+    brevnov::numberList sum = calculateSum(listpair);
+    outputNumber(std::cout, sum) << "\n";
+    return 0;
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
+}
