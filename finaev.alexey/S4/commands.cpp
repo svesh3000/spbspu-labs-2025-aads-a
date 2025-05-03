e47096d96127d998aa80e65adfa42bcaea268226
AlexeyFinaev02 <finaev0212212@mail.ru>
2025-05-03 11:45:30 +0300

    fix

--- finaev.alexey/S4/commands.cpp
+++ finaev.alexey/S4/commands.cpp
@@ -10,6 +10,7 @@ void finaev::printCMD(std::ostream& out, const mainDict& dict)
   if (current.empty())
   {
     std::cout << "<EMPTY>\n";
+    return;
   }
   out << name;
   for (auto i = current.cbegin(); i != current.cend(); ++i)
