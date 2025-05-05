d6373189e37e6748992ff38c172773494f6f636f
jambii1 <s.maslevtsov@gmail.com>
2025-05-05 17:43:02 +0300

    input

--- maslevtsov.stanislav/S5/main.cpp
+++ maslevtsov.stanislav/S5/main.cpp
@@ -1,2 +1,28 @@
-int main()
-{}
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <tree/definition.hpp>
+
+int main(int argc, char** argv)
+{
+  if (argc != 3) {
+    std::cerr << "<INVALID PARAMETERS NUMBER>\n";
+    return 1;
+  }
+
+  std::ifstream fin(argv[2]);
+  if (!fin) {
+    std::cerr << "<INVALID DATA FILE>\n";
+    return 1;
+  }
+  std::map< int, std::string > data;
+  int key = 0;
+  std::string value = "";
+  while (fin >> key >> value) {
+    data[key] = value;
+  }
+  if (!fin.eof()) {
+    std::cerr << "<INPUT ERROR>\n";
+    return 1;
+  }
+}
