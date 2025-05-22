db34e983c9a2e00f9ba7be01846cf817343f9c14
TvoyBatyA12343 <maxdem06@gmail.com>
2025-05-22 13:36:09 +0300

    little fix

--- demehin.maxim/S7/commands.cpp
+++ demehin.maxim/S7/commands.cpp
@@ -185,7 +185,7 @@ void demehin::extract(std::istream& in, MapOfGraphs& graphs)
     throw std::logic_error("incorrect parameters");
   }
 
-  std::list< std::string > vrts;
+  List< std::string > vrts;
   auto gr = graphs.at(gr_name);
   for (size_t i = 0; i < vrt_cnt; i++)
   {
