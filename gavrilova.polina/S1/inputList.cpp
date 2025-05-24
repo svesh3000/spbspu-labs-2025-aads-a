807aa7ee14ab9c0e02d11feb79fdad54ca83cee3
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2025-05-24 13:06:13 +0300

    To merge S2

--- gavrilova.polina/S1/inputList.cpp
+++ gavrilova.polina/S1/inputList.cpp
@@ -5,9 +5,7 @@ gavrilova::FwdList< unsigned long long > gavrilova::inputNumbers(std::istream& i
   FwdList< unsigned long long > numbers;
   unsigned long long num = 0;
   auto it = numbers.cbegin();
-  // std::cout << "start input numbers\n";
   while (in >> num) {
-    // std::cout << "+num\n";
     numbers.insert(it, num);
     ++it;
   }
--- gavrilova.polina/S1/main.cpp
+++ gavrilova.polina/S1/main.cpp
@@ -10,34 +10,23 @@ int main()
   using ULL = unsigned long long;
 
   FwdList< std::pair< std::string, FwdList< ULL > > > listOfPairs{};
-  // std::cout << "1\n";
   size_t numOfPairs = 0;
   std::string name;
   size_t maxLen = 0;
   auto cur_it = listOfPairs.begin();
   while (std::cin >> name && !std::cin.eof()) {
-    // std::cout << "input_name\n";
     FwdList< ULL > numbers = inputNumbers(std::cin);
-    // std::cout << "finish input numbers\n";
     maxLen = (maxLen < numbers.size()) ? numbers.size() : maxLen;
-<<<<<<< HEAD
-    std::pair< std::string, FwdList< ULL > > new_pair {name, numbers};
-    listOfPairs.insert(cur_it, new_pair);
-    // std::cout << "insert LOP\n";
-=======
     std::pair< std::string, FwdList< ULL > > new_pair{name, numbers};
     listOfPairs.insert(cur_it, new_pair);
->>>>>>> gavrilova.polina/S2
     ++cur_it;
     ++numOfPairs;
   }
-  // std::cout << "finnish input\n";
 
   if (numOfPairs) {
     outNames(std::cout, listOfPairs);
     std::cout << "\n";
   }
-  // std::cout << "11\n";
 
   if (maxLen == 0) {
     std::cout << "0" << "\n";
@@ -52,16 +41,7 @@ int main()
     std::cerr << e.what() << "\n";
     return 1;
   }
-<<<<<<< HEAD
-  if (maxLen == 0) {
-    std::cout << "0" << "\n";
-  } else {
-    outFwdListULL(std::cout, sums);
-  }
- std::cout << "1\n";
-=======
 
   outFwdListULL(std::cout, sums);
   std::cout << "\n";
->>>>>>> gavrilova.polina/S2
 }
