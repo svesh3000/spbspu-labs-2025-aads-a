1f992a714ce8a018ea09b71da882301db050171e
brev1ch <flirktv@gmail.com>
2025-05-25 23:09:54 +0300

    name fix 2

--- brevnov.nikita/S4/tree.hpp
+++ brevnov.nikita/S4/tree.hpp
@@ -189,8 +189,8 @@
       Node* T2 = x->right;
       x->right = y;
       y->left = T2;
-      y->height = std::max(height(y->left), height(y->right)) + 1;
-      x->height = std::max(height(x->left), height(x->right)) + 1;
+      y->nodeHeight = std::max(height(y->left), height(y->right)) + 1;
+      x->nodeHeight = std::max(height(x->left), height(x->right)) + 1;
       return x;
     }
 
@@ -201,8 +201,8 @@
       Node* T2 = y->left;
       y->left = x;
       x->right = T2;
-      x->height = std::max(height(x->left), height(x->right)) + 1;
-      y->height = std::max(height(y->left), height(y->right)) + 1;
+      x->nodeHeight = std::max(height(x->left), height(x->right)) + 1;
+      y->nodeHeight = std::max(height(y->left), height(y->right)) + 1;
       return y;
     }
 
