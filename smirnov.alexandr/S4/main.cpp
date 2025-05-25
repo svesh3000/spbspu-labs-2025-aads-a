c1a746c7eeb645c51df7c2877556c7a68e59eb1b
A1exCRE <alsmmm2065@gmail.com>
2025-05-26 00:36:30 +0300

    Final main.cpp

--- smirnov.alexandr/S4/main.cpp
+++ smirnov.alexandr/S4/main.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <fstream>
+#include <limits>
 #include "inputTrees.hpp"
+#include "commandProcessor.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
-  smirnov::TreeOfTrees trees;
-  std::ifstream file("test.txt");
-  smirnov::inputTrees(trees, file);
-  for (const auto & dict : trees)
-   {
-    std::cout << "Dictionary: " << dict.first << std::endl;
+  using namespace smirnov;
+  if (argc != 2)
+  {
+    std::cerr << "Invalid arguments\n";
+    return 1;
+  }
+  std::ifstream file(argv[1]);
+  if (!file.is_open())
+  {
+    std::cerr << "File open failed\n";
+    return 1;
+  }
+  TreeOfTrees trees;
+  inputTrees(trees, file);
+  std::string command;
+  while (std::cin >> command)
+  {
+    processCommand(trees, command, std::cin, std::cout);
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
   }
 }
