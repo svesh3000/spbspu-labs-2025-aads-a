a6388148bd130ada5b990630196b96af00a90b60
brev1ch <flirktv@gmail.com>
2025-05-25 19:38:45 +0300

    avltree

--- brevnov.nikita/S4/commandlist.hpp
+++ brevnov.nikita/S4/commandlist.hpp
@@ -6,8 +6,8 @@
 
 namespace brevnov
 {
-  using data = AVLTrees< size_t, std::string >;
-  using dataset = AVLTrees< std::string, data >;
+  using data = AVLTree< size_t, std::string >;
+  using dataset = AVLTree< std::string, data >;
   void print(std::ostream&, std::istream&, const dataset&);
   void complement(std::istream&, dataset&);
   void intersect(std::istream&, dataset&);
