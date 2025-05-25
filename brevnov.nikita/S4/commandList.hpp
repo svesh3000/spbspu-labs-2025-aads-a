80a08c67b59e5ff7a982791f463a1745f2a36077
brev1ch <flirktv@gmail.com>
2025-05-25 19:48:30 +0300

    h try

--- brevnov.nikita/S4/commandList.hpp
+++ brevnov.nikita/S4/commandList.hpp
@@ -1,13 +1,13 @@
-#ifndef COMMANDS_HPP
-#define COMMANDS_HPP
+#ifndef COMMANDLIST_HPP
+#define COMMANDLIST_HPP
 #include <iostream>
 #include <string>
 #include "tree.hpp"
 
 namespace brevnov
 {
-  using data = RBTree< size_t, std::string >;
-  using dataset = RBTree< std::string, data >;
+  using data = AVLTree< size_t, std::string >;
+  using dataset = AVLTree< std::string, data >;
   void print(std::ostream&, std::istream&, const dataset&);
   void complement(std::istream&, dataset&);
   void intersect(std::istream&, dataset&);
