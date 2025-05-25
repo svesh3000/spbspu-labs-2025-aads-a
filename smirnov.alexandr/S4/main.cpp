37aaa569b4ed3d3125f57f4e85b5c4054d4ffbd5
A1exCRE <alsmmm2065@gmail.com>
2025-05-26 00:02:22 +0300

    Start main.cpp

--- smirnov.alexandr/S4/main.cpp
+++ smirnov.alexandr/S4/main.cpp
@@ -1,2 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include "inputTrees.hpp"
+
 int main()
-{}
+{
+  smirnov::TreeOfTrees trees;
+  std::ifstream file("test.txt");
+  smirnov::inputTrees(trees, file);
+  for (const auto & dict : trees)
+   {
+    std::cout << "Dictionary: " << dict.first << std::endl;
+  }
+}
