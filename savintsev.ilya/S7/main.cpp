ff80c61b1a20b2eedee3c7bfa71d0d62938fb304
faxryzen <cheburek2006@gmail.com>
2025-05-25 15:24:17 +0300

    Добавлены файлы для тестов

---
+++ savintsev.ilya/S7/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <boost/hash2/xxhash.hpp>
+
+int main()
+{
+  //std::unordered_map< std::pair< std::string, std::string >, size_t > graph;
+}
---
+++ savintsev.ilya/S7/test-hash-map.cpp
@@ -0,0 +1,11 @@
+#include <boost/test/unit_test.hpp>
+#include <boost/hash2/xxhash.hpp>
+#include <string>
+#include "hash-map.hpp"
+
+using namespace savintsev;
+
+BOOST_AUTO_TEST_CASE(test_operator_sq)
+{
+  //HashMap< int, std::string, std::hash, hash2::xxhash_64 > book;
+}
