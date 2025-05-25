790c99f460e1561fd7cd133fc3fe876e6666b224
brev1ch <flirktv@gmail.com>
2025-05-25 22:58:12 +0300

    node fix

--- brevnov.nikita/S4/treeNode.hpp
+++ brevnov.nikita/S4/treeNode.hpp
@@ -7,23 +7,6 @@ namespace brevnov
   template< class Key, class Value >
   struct TreeNode
   {
-    TreeNode() = default;
-
-    explicit TreeNode(const std::pair< Key, Value > & k):
-      left(nullptr),
-      right(nullptr),
-      parent(nullptr),
-      nodeHeight(1),
-      data(k)
-    {}
-
-    explicit TreeNode(std::pair< Key, Value > && k):
-      left(nullptr),
-      right(nullptr),
-      parent(nullptr),
-      nodeHeight(1),
-      data(std::move(k))
-    {}
     TreeNode* left;
     TreeNode* right;
     TreeNode* parent;
