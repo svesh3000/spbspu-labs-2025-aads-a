469396c3193e7c6dd12acbf947370d646f5427ef
deonik3 <kis.sergey2006@gmail.com>
2025-05-27 01:16:54 +0300

    create functions out- and in- bound

--- kiselev.sergey/S7/commands.cpp
+++ kiselev.sergey/S7/commands.cpp
@@ -2,6 +2,45 @@
 #include <map>
 #include <string>
 
+namespace
+{
+  void bound(std::ostream& out, std::istream& in, const kiselev::Graphs& graphs, const std::string& b)
+  {
+    std::string graph;
+    std::string vertex;
+    in >> graph >> vertex;
+    auto graphIt = graphs.find(graph);
+    if (graphIt == graphs.end())
+    {
+      out << "<INVALID COMMAND>\n";
+      return;
+    }
+    std::map< std::string, std::string > vertexes = graphIt->second.getVertexes();
+    if (vertexes.find(vertex) == vertexes.end())
+    {
+      out << "<INVALID COMMAND>\n";
+      return;
+    }
+    std::map< std::string, std::map< unsigned int, unsigned int > > bound;
+    if (b == "out")
+    {
+      bound = graphIt->second.getOutBound(vertex);
+    }
+    else
+    {
+      bound = graphIt->second.getInBound(vertex);
+    }
+    for (auto it = bound.begin(); it != bound.end(); ++it)
+    {
+      out << it->first;
+      for (auto weightIt = it->second.begin(); weightIt != it->second.end(); ++weightIt)
+      {
+        out << " " << weightIt->first;
+      }
+      out << "\n";
+    }
+  }
+}
 void kiselev::graphs(std::ostream& out, const Graphs& graphs)
 {
   for (auto it = graphs.begin(); it != graphs.end(); ++it)
@@ -25,4 +64,14 @@ void kiselev::vertexes(std::ostream& out, std::istream& in, const Graphs& graphs
   {
     out << iter->first << "\n";
   }
+}
+
+void kiselev::outBound(std::ostream& out, std::istream& in, const Graphs& graphs)
+{
+  bound(out, in, graphs, "out");
+}
+
+void kiselev::inBound(std::ostream& out, std::istream& in, const Graphs& graphs)
+{
+  bound(out, in, graphs, "in");
 }
