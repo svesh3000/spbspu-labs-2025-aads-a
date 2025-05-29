5e00a287c84c93990c3426c69a8d996e24d986ac
brev1ch <flirktv@gmail.com>
2025-05-30 00:15:25 +0300

    test fix

--- brevnov.nikita/S5/test-traverse.cpp
+++ brevnov.nikita/S5/test-traverse.cpp
@@ -10,20 +10,20 @@ using namespace brevnov;
 
 BOOST_AUTO_TEST_CASE(traverse)
 {
-  AVLTree< int, std::string > tree({ { 10, "a" }, { 7, "b" }, { 15, "c" }, { 5, "d" }, { 8, "e" } });
+  AVLTree< int, std::string > tree({ { 13, "a" }, { 10, "b" }, { 18, "c" }, { 8, "d" }, { 11, "e" } });
   std::ostringstream iss;
   KeySum funLnr;
   funLnr = tree.traverse_lnr(funLnr);
   iss << funLnr.sum << funLnr.str;
-  BOOST_TEST(iss.str() == "45 d b e a c");
+  BOOST_TEST(iss.str() == "60 d b e a c");
   KeySum funRnl;
   funRnl = tree.traverse_rnl(funRnl);
   iss.str("");
   iss << funRnl.sum << funRnl.str;
-  BOOST_TEST(iss.str() == "45 c a e b d");
+  BOOST_TEST(iss.str() == "60 c a e b d");
   iss.str("");
   KeySum funBr;
   funBr = tree.traverse_breadth(funBr);
   iss << funBr.sum << funBr.str;
-  BOOST_TEST(iss.str() == "45 a b c d e");
+  BOOST_TEST(iss.str() == "60 a b c d e");
 }
