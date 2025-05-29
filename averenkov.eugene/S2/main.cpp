9be239237e236e4a67d5387213bf6ab1f89a967b
Savishna <bravissimo28@mail.ru>
2025-05-29 21:42:30 +0300

    return all

--- averenkov.eugene/S2/main.cpp
+++ averenkov.eugene/S2/main.cpp
@@ -1,438 +1,214 @@
 #include <iostream>
-#include <climits>
 #include <fstream>
 #include <sstream>
 #include <string>
-#include <cctype>
-#include <stdexcept>
-#include "stack.hpp"
-#include "queue.hpp"
+#include "BiTree.hpp"
 
+using Dictionary = Tree<int, std::string>;
+using DictionaryStorage = Tree<std::string, Dictionary>;
+using str = const std::string&;
 
-using namespace averenkov;
-
-bool isOverflow(long long a, long long b)
+void loadDictionaries(str filename, DictionaryStorage& storage)
 {
-  if (a == 0 || b == 0)
+  std::ifstream file(filename);
+  if (!file)
   {
-    return false;
+    std::cerr << "file error\n";
+    return;
   }
-  if (a > 0)
+
+  std::string line;
+  while (std::getline(file, line))
   {
-    if (b > 0)
-    {
-      return a > LLONG_MAX / b;
-    }
-    else
-    {
-      return b < LLONG_MIN / a;
-    }
-  }
-  else
-  {
-    if (b > 0)
-    {
-      return a < LLONG_MIN / b;
-    }
-    else
-    {
-      return a < LLONG_MAX / b;
-    }
-  }
-}
-
-bool isOperator(char c)
-{
-  return c == '+' || c == '-' || c == '*' || c == '/' || c == '%';
-}
-
-bool precedenceFirst(char first, char second)
-{
-  if ((first == '+' || first == '-') && (second == '*' || second == '/' || second == '%'))
-  {
-    return false;
-  }
-  return true;
-}
-
-
-/*std::string infixToPostfix(const std::string& infix)
-{
-  Stack< char > stack;
-  std::string postfix;
-  std::string number;
-  bool isNegative = false;
-
-  for (size_t i = 0; i < infix.size(); ++i)
-  {
-    char ch = infix[i];
-
-    if (std::isspace(ch))
+    if (line.empty())
     {
       continue;
     }
-    if (std::isdigit(ch))
-    {
-      number += ch;
-    }
-    else if (ch == '-' && (i == 0 || infix[i - 1] == '('))
-    {
-      isNegative = true;
-    }
-    else
-    {
-      if (!number.empty())
-      {
-        if (isNegative)
-        {
-          postfix += '-';
-          isNegative = false;
-        }
-        postfix += number + " ";
-        number.clear();
-      }
 
-      if (ch == '(')
-      {
-        stack.push(ch);
-      }
-      else if (ch == ')')
-      {
-        while (!stack.empty() && stack.top() != '(')
-        {
-          postfix += stack.drop();
-          postfix += ' ';
-        }
-        if (stack.empty())
-        {
-          throw std::runtime_error("parentheses error");
-        }
-        stack.drop();
-      }
-      else if (isOperator(ch))
-      {
-        while (!stack.empty() && precedenceFirst(stack.top(), ch))
-        {
-          postfix += stack.drop();
-          postfix += ' ';
-        }
-        stack.push(ch);
-      }
-      else
-      {
-        throw std::runtime_error("error character in expression");
-      }
+    std::istringstream iss(line);
+    std::string dictName;
+    iss >> dictName;
+
+    Dictionary dict;
+    int key;
+    std::string value;
+    while (iss >> key >> value)
+    {
+      dict.push(key, value);
     }
+
+    storage.push(dictName, dict);
   }
-
-  if (!number.empty())
-  {
-    if (isNegative)
-    {
-      postfix += '-';
-    }
-    postfix += number + " ";
-  }
-
-  while (!stack.empty())
-  {
-    if (stack.top() == '(')
-    {
-      throw std::runtime_error("parentheses error");
-    }
-    postfix += stack.drop();
-    postfix += ' ';
-  }
-
-  if (!postfix.empty() && postfix.back() == ' ')
-  {
-    postfix.pop_back();
-  }
-
-  return postfix;
-}*/
-std::string infixToPostfix(const std::string& infix)
-{
-  Stack<char> stack;
-  std::string postfix;
-  std::string number;
-  bool isNegative = false;
-  bool expectOperand = true; // Ожидаем операнд (число или открывающую скобку)
-
-  for (size_t i = 0; i < infix.size(); ++i)
-  {
-    char ch = infix[i];
-
-    if (std::isspace(ch)) continue;
-
-    if (std::isdigit(ch))
-    {
-      number += ch;
-      expectOperand = false;
-    }
-    else if (ch == '-' && expectOperand)
-    {
-      isNegative = true;
-    }
-    else
-    {
-      if (!number.empty())
-      {
-        if (isNegative)
-        {
-          postfix += '-';
-          isNegative = false;
-        }
-        postfix += number + " ";
-        number.clear();
-      }
-
-      if (ch == '(')
-      {
-        stack.push(ch);
-        expectOperand = true;
-      }
-      else if (ch == ')')
-      {
-        while (!stack.empty() && stack.top() != '(')
-        {
-          postfix += stack.drop();
-          postfix += ' ';
-        }
-        if (stack.empty())
-        {
-          throw std::runtime_error("parentheses error");
-        }
-        stack.drop();
-        expectOperand = false;
-      }
-      else if (isOperator(ch))
-      {
-        while (!stack.empty() && stack.top() != '(' && precedenceFirst(stack.top(), ch))
-        {
-          postfix += stack.drop();
-          postfix += ' ';
-        }
-        stack.push(ch);
-        expectOperand = true;
-      }
-      else
-      {
-        throw std::runtime_error("error character in expression");
-      }
-    }
-  }
-
-  if (!number.empty())
-  {
-    if (isNegative)
-    {
-      postfix += '-';
-    }
-    postfix += number + " ";
-  }
-
-  while (!stack.empty())
-  {
-    if (stack.top() == '(')
-    {
-      throw std::runtime_error("parentheses error");
-    }
-    postfix += stack.drop();
-    postfix += ' ';
-  }
-
-  if (!postfix.empty() && postfix.back() == ' ')
-  {
-    postfix.pop_back();
-  }
-
-  return postfix;
 }
 
-long long evaluatePostfix(const std::string& postfix)
+void printDictionary(const Dictionary& dict, str name)
 {
-  Stack< long long > stack;
-  std::istringstream iss(postfix);
-  std::string token;
-
-  while (iss >> token)
+  if (dict.empty())
   {
-    if (std::isdigit(token[0]))
-    {
-      stack.push(std::stoll(token));
-    }
-    else if (isOperator(token[0]))
-    {
-      try
-      {
-        long long b = stack.drop();
-        long long a = stack.drop();
-        long long result;
-
-        switch (token[0])
-        {
-          case '+':
-            if (a > 0 && b > 0 && a > LLONG_MAX - b)
-            {
-              throw std::runtime_error("overflow error");
-            }
-            result = a + b;
-            break;
-          case '-':
-            if (b > 0 && a < LLONG_MIN + b)
-            {
-              throw std::runtime_error("overflow error");
-            }
-            result = a - b;
-            break;
-          case '*':
-            if (isOverflow(a, b))
-            {
-              throw std::runtime_error("overflow error");
-            }
-            result = a * b;
-            break;
-          case '/':
-            if (b == 0)
-            {
-              throw std::runtime_error("division zero");
-            }
-            result = a / b;
-            break;
-          case '%':
-            if (b == 0)
-            {
-              throw std::runtime_error("modul zero");
-            }
-            result = (a % b + b) % b;
-            break;
-          default:
-            throw std::runtime_error("invalid operator");
-        }
-
-        stack.push(result);
-      }
-      catch (const std::underflow_error& e)
-      {
-        throw std::runtime_error("expression error");
-      }
-    }
-    else
-    {
-      throw std::runtime_error("invalid token");
-    }
+    std::cout << "<EMPTY>\n";
+    return;
   }
 
-  return stack.drop();
-}
-
-
-/*void processExpressions(std::istream& input)
-{
-  Stack< long long > results;
-  std::string line;
-  std::string token;
-
-  while (input >> token)
+  std::cout << name;
+  for (auto it = dict.begin(); it != dict.end(); ++it)
   {
-    if (token == "(" || token == ")" || isOperator(token[0]) || std::isdigit(token[0]))
-    {
-      line += token + " ";
-    }
-    else
-    {
-      throw std::runtime_error("invalid token");
-    }
-
-    if (input.peek() == '\n' || input.peek() == EOF)
-    {
-      if (!line.empty())
-      {
-        std::string postfix = infixToPostfix(line);
-        long long result = evaluatePostfix(postfix);
-        results.push(result);
-        line.clear();
-      }
-    }
-  }
-
-  std::cout << results.drop();
-
-  while (!results.empty())
-  {
-    std::cout << " " << results.drop();
+    std::cout << " " << it->first << " " << it->second;
   }
   std::cout << "\n";
-}*/
-
-void processExpressions(std::istream& input)
-{
-  Stack<long long> results;
-  std::string line;
-
-  while (std::getline(input, line))
-  {
-    // Пропускаем пустые строки
-    if (line.empty()) continue;
-    try {
-      std::string postfix = infixToPostfix(line);
-      long long result = evaluatePostfix(postfix);
-      results.push(result);
-    }
-    catch (const std::exception& e) {
-      throw;
-    }
-  }
-  // Вывод результатов в обратном порядке
-  if (!results.empty()) {
-    Stack<long long> temp;
-    while (!results.empty()) {
-      temp.push(results.drop());
-    }
-    std::cout << temp.drop();
-    while (!temp.empty()) {
-      std::cout << " " << temp.drop();
-    }
-    std::cout << "\n";
-  }
 }
+
+void complement(DictionaryStorage& storage, str newName, str name1, str name2)
+{
+  auto dict1 = storage.find(name1);
+  auto dict2 = storage.find(name2);
+
+  if (dict1 == storage.end() || dict2 == storage.end())
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
+
+  Dictionary result;
+  for (auto it = dict1->second.begin(); it != dict1->second.end(); ++it)
+  {
+    if (dict2->second.find(it->first) == dict2->second.end())
+    {
+      result.push(it->first, it->second);
+    }
+  }
+
+  storage.push(newName, result);
+}
+
+void intersect(DictionaryStorage& storage, str newName, str name1, str name2)
+{
+  auto dict1 = storage.find(name1);
+  auto dict2 = storage.find(name2);
+
+  if (dict1 == storage.end() || dict2 == storage.end())
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
+
+  Dictionary result;
+  for (auto it = dict1->second.begin(); it != dict1->second.end(); ++it)
+  {
+    if (dict2->second.find(it->first) != dict2->second.end())
+    {
+      result.push(it->first, it->second);
+    }
+  }
+
+  storage.push(newName, result);
+}
+
+void unionDicts(DictionaryStorage& storage, str newName, str name1, str name2)
+{
+  auto dict1 = storage.find(name1);
+  auto dict2 = storage.find(name2);
+
+  if (dict1 == storage.end() || dict2 == storage.end())
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
+
+  Dictionary result;
+  for (auto it = dict1->second.begin(); it != dict1->second.end(); ++it)
+  {
+    result.push(it->first, it->second);
+  }
+  for (auto it = dict2->second.begin(); it != dict2->second.end(); ++it)
+  {
+    if (result.find(it->first) == result.end())
+    {
+      result.push(it->first, it->second);
+    }
+  }
+
+  storage.push(newName, result);
+}
+
 int main(int argc, char* argv[])
 {
-  try
+  if (argc != 2)
   {
-    if (argc > 2)
-    {
-      std::cerr << "Error\n";
-      return 1;
-    }
+    std::cerr << "Usage: " << argv[0] << " filename\n";
+    return 1;
+  }
 
-    if (argc == 2)
+  DictionaryStorage dictionaries;
+  loadDictionaries(argv[1], dictionaries);
+
+  std::string line;
+  while (std::getline(std::cin, line))
+  {
+    std::istringstream iss(line);
+    std::string command;
+    iss >> command;
+
+    if (command == "print")
     {
-      std::ifstream file(argv[1]);
-      if (!file)
+      std::string dictName;
+      if (iss >> dictName)
       {
-        std::cerr << "Error\n";
-        return 1;
+        auto dict = dictionaries.find(dictName);
+        if (dict != dictionaries.end())
+        {
+          printDictionary(dict->second, dictName);
+        }
+        else
+        {
+          std::cout << "<INVALID COMMAND>\n";
+        }
       }
-      processExpressions(file);
+      else
+      {
+        std::cout << "<INVALID COMMAND>\n";
+      }
+    }
+    else if (command == "complement")
+    {
+      std::string newName, name1, name2;
+      if (iss >> newName >> name1 >> name2)
+      {
+        complement(dictionaries, newName, name1, name2);
+      }
+      else
+      {
+        std::cout << "<INVALID COMMAND>\n";
+      }
+    }
+    else if (command == "intersect")
+    {
+      std::string newName, name1, name2;
+      if (iss >> newName >> name1 >> name2)
+      {
+        intersect(dictionaries, newName, name1, name2);
+      }
+      else
+      {
+        std::cout << "<INVALID COMMAND>\n";
+      }
+    }
+    else if (command == "union")
+    {
+      std::string newName, name1, name2;
+      if (iss >> newName >> name1 >> name2)
+      {
+        unionDicts(dictionaries, newName, name1, name2);
+      }
+      else
+      {
+        std::cout << "<INVALID COMMAND>\n";
+      }
     }
     else
     {
-      processExpressions(std::cin);
+      std::cout << "<INVALID COMMAND>\n";
     }
   }
 
-  catch (const std::underflow_error& e)
-  {
-    std::cout << "\n";
-    return 0;
-  }
-
-  catch (const std::exception& e)
-  {
-    std::cerr << e.what() << "\n";
-    return 1;
-  }
   return 0;
 }
