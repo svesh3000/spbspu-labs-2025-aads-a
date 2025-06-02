be03763db1534f3d6e162881fc523a3a4f817bee
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-06-03 00:53:39 +0300

    added key_sum with tests

---
+++ zholobov.oleg/S5/key_sum.hpp
@@ -0,0 +1,39 @@
+#ifndef KEY_SUM_HPP
+#define KEY_SUM_HPP
+
+#include <limits>
+#include <stdexcept>
+#include <string>
+
+namespace {
+  using KeyType = long;
+
+  bool isAdditionOverflow(long lhs, long rhs)
+  {
+    constexpr long max_value = std::numeric_limits< long >::max();
+    constexpr long min_value = std::numeric_limits< long >::min();
+    return (((rhs > 0) && (lhs > (max_value - rhs))) || ((rhs < 0) && (lhs < (min_value - rhs))));
+  }
+
+}
+
+namespace zholobov {
+
+  struct KeySum {
+    void operator()(const std::pair< const KeyType, std::string >& key_value);
+    KeyType sum_ = 0;
+    std::string values_;
+  };
+
+  void KeySum::operator()(const std::pair< const KeyType, std::string >& key_value)
+  {
+    if (isAdditionOverflow(sum_, key_value.first)) {
+      throw std::overflow_error("Numeric overflow");
+    }
+    sum_ += key_value.first;
+    values_ += (" " + key_value.second);
+  }
+
+}
+
+#endif
