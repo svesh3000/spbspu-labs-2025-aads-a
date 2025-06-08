8d278aa549e6c7b7f0b12ebdc8842dbff81c9901
A1exCRE <alsmmm2065@gmail.com>
2025-06-08 23:20:27 +0300

    fix unit

--- smirnov.alexandr/S1/test-list.cpp
+++ smirnov.alexandr/S1/test-list.cpp
@@ -4,8 +4,8 @@
 BOOST_AUTO_TEST_CASE(DefaultConstructorTest)
 {
   smirnov::List< int > list;
-  BOOST_CHECK(list.empty());
-  BOOST_CHECK(list.size() == 0);
+  BOOST_TEST(list.empty());
+  BOOST_TEST(list.size() == 0);
 }
 
 BOOST_AUTO_TEST_CASE(PushFrontTest)
@@ -15,9 +15,9 @@ BOOST_AUTO_TEST_CASE(PushFrontTest)
   list.push_front(2);
   list.push_front(3);
 
-  BOOST_CHECK(list.size() == 3);
-  BOOST_CHECK(list.front() == 3);
-  BOOST_CHECK(list.back() == 1);
+  BOOST_TEST(list.size() == 3);
+  BOOST_TEST(list.front() == 3);
+  BOOST_TEST(list.back() == 1);
 }
 
 BOOST_AUTO_TEST_CASE(PopFrontTest)
@@ -28,7 +28,7 @@ BOOST_AUTO_TEST_CASE(PopFrontTest)
 
   list.pop_front();
   BOOST_TEST(list.front() == 1);
-  BOOST_TEST(list.size() == 2);
+  BOOST_TEST(list.size() == 1);
 
   list.pop_front();
   BOOST_TEST(list.empty());
@@ -37,15 +37,15 @@ BOOST_AUTO_TEST_CASE(PopFrontTest)
 
 BOOST_AUTO_TEST_CASE(CopyConstructorTest)
 {
-  smirnov::List<int> list;
-  list.push_front(3);
-  list.push_front(2);
-  list.push_front(1);
-  smirnov::List< int > list2(list);
+  smirnov::List<int> list1;
+  list1.push_front(3);
+  list1.push_front(2);
+  list1.push_front(1);
+  smirnov::List< int > list2(list1);
 
-  BOOST_CHECK(list2.size() == 3);
-  BOOST_CHECK(list2.front() == 1);
-  BOOST_CHECK(list2.back() == 3);
+  BOOST_TEST(list2.size() == 3);
+  BOOST_TEST(list2.front() == 3);
+  BOOST_TEST(list2.back() == 1);
 }
 
 BOOST_AUTO_TEST_CASE(MoveConstructorTest)
@@ -57,10 +57,10 @@ BOOST_AUTO_TEST_CASE(MoveConstructorTest)
 
   smirnov::List< int > list2(std::move(list1));
 
-  BOOST_CHECK(list2.size() == 3);
-  BOOST_CHECK(list2.front() == 1);
-  BOOST_CHECK(list2.back() == 3);
-  BOOST_CHECK(list1.empty());
+  BOOST_TEST(list2.size() == 3);
+  BOOST_TEST(list2.front() == 3);
+  BOOST_TEST(list2.back() == 1);
+  BOOST_TEST(list1.empty());
 }
 
 BOOST_AUTO_TEST_CASE(CopyAssignmentTest)
@@ -73,9 +73,9 @@ BOOST_AUTO_TEST_CASE(CopyAssignmentTest)
   smirnov::List< int > list2;
   list2 = list1;
 
-  BOOST_CHECK(list2.size() == 3);
-  BOOST_CHECK(list2.front() == 1);
-  BOOST_CHECK(list2.back() == 3);
+  BOOST_TEST(list2.size() == 3);
+  BOOST_TEST(list2.front() == 3);
+  BOOST_TEST(list2.back() == 1);
 }
 
 BOOST_AUTO_TEST_CASE(MoveAssignmentTest)
@@ -88,10 +88,10 @@ BOOST_AUTO_TEST_CASE(MoveAssignmentTest)
   smirnov::List< int > list2;
   list2 = std::move(list1);
 
-  BOOST_CHECK(list2.size() == 3);
-  BOOST_CHECK(list2.front() == 1);
-  BOOST_CHECK(list2.back() == 3);
-  BOOST_CHECK(list1.empty());
+  BOOST_TEST(list2.size() == 3);
+  BOOST_TEST(list2.front() == 3);
+  BOOST_TEST(list2.back() == 1);
+  BOOST_TEST(list1.empty());
 }
 
 BOOST_AUTO_TEST_CASE(SwapTest)
@@ -105,18 +105,19 @@ BOOST_AUTO_TEST_CASE(SwapTest)
 
   list1.swap(list2);
 
-  BOOST_CHECK(list1.size() == 1);
-  BOOST_CHECK(list1.front() == 3);
-  BOOST_CHECK(list2.size() == 2);
-  BOOST_CHECK(list2.front() == 1);
+  BOOST_TEST(list1.size() == 1);
+  BOOST_TEST(list1.front() == 3);
+  BOOST_TEST(list2.size() == 2);
+  BOOST_TEST(list2.front() == 2);
+  BOOST_TEST(list2.back() == 1);
 }
 
 BOOST_AUTO_TEST_CASE(IteratorTest)
 {
   smirnov::List< int > list;
-  list.push_front(1);
-  list.push_front(2);
   list.push_front(3);
+  list.push_front(2);
+  list.push_front(1);
 
   auto it = list.begin();
   BOOST_CHECK(*it == 1);
@@ -131,9 +132,9 @@ BOOST_AUTO_TEST_CASE(IteratorTest)
 BOOST_AUTO_TEST_CASE(ConstIteratorTest)
 {
   smirnov::List< int > list;
-  list.push_front(1);
-  list.push_front(2);
   list.push_front(3);
+  list.push_front(2);
+  list.push_front(1);
 
   auto it = list.cbegin();
   BOOST_CHECK(*it == 1);
