44181d717bcdbf3ea81eda8cf7d08f02a796dfdf
Maksim_Shramko <maxramko@gmail.com>
2025-06-09 17:13:34 +0300

    Adding all tests (stack &  main)

---
+++ shramko.maksim/S2/test-stack.cpp
@@ -0,0 +1,59 @@
+#include <boost/test/unit_test.hpp>
+#include <sstream>
+#include "stack.hpp"
+#include "print.hpp"
+
+BOOST_AUTO_TEST_CASE(stack_basic_constructor_test)
+{
+  shramko::StackString stack;
+  BOOST_TEST(stack.empty());
+  BOOST_TEST(stack.size() == 0);
+}
+
+BOOST_AUTO_TEST_CASE(stack_pop_test)
+{
+  shramko::StackString stack;
+  stack.push("1");
+  stack.push("2");
+  stack.push("3");
+  stack.push("4");
+  stack.push("5");
+  BOOST_TEST(stack.size() == 5);
+  stack.pop();
+  BOOST_TEST(stack.size() == 4);
+  std::ostringstream out;
+  shramko::outputStack(out, stack);
+  BOOST_TEST(out.str() == "4 3 2 1");
+}
+
+BOOST_AUTO_TEST_CASE(stack_lvalue_push_test)
+{
+  shramko::StackString stack;
+  std::string temp = "3";
+  stack.push(temp);
+  std::ostringstream out;
+  shramko::outputStack(out, stack);
+  BOOST_TEST(out.str() == "3");
+  BOOST_TEST(stack.size() == 1);
+}
+
+BOOST_AUTO_TEST_CASE(stack_top_size_test)
+{
+  shramko::StackString stack;
+  stack.push("2");
+  stack.push("3");
+  BOOST_TEST(stack.top() == "3");
+  BOOST_TEST(stack.size() == 2);
+}
+
+BOOST_AUTO_TEST_CASE(stack_move_push_test)
+{
+  shramko::StackString stack;
+  stack.push("1");
+  BOOST_TEST(!stack.empty());
+  BOOST_TEST(stack.size() == 1);
+  BOOST_TEST(stack.top() == "1");
+  stack.push("2");
+  BOOST_TEST(stack.top() == "2");
+  BOOST_TEST(stack.size() == 2);
+}
