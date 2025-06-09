cd4bfab4d42dc50e6bd5a77618af0168fbe2a8e3
deonik3 <kis.sergey2006@gmail.com>
2025-06-09 09:30:29 +0300

    create addelement + deleteelement + listdict

--- kiselev.sergey/F0/commands.cpp
+++ kiselev.sergey/F0/commands.cpp
@@ -2,6 +2,7 @@
 #include <stdexcept>
 #include <string>
 #include <iostream>
+#include "list.hpp"
 
 void kiselev::doNewDict(std::istream& in, Dicts& dicts)
 {
@@ -25,4 +26,64 @@ void kiselev::doDeleteDict(std::istream& in, Dicts& dicts)
     return;
   }
   dicts.erase(dict);
+}
+
+void kiselev::doAddElement(std::istream& in, Dicts& dicts)
+{
+  std::string nameDict;
+  std::string eng;
+  std::string rus;
+  in >> nameDict >> eng >> rus;
+  auto dictIt = dicts.find(nameDict);
+  if (dictIt == dicts.end())
+  {
+    std::cout << "<DICTIONARY NOT FOUND>\n";
+    return;
+  }
+  Dict dict = dictIt->second;
+  auto engIt = dict.find(eng);
+  if (engIt == dict.end())
+  {
+    List< std::string > translations;
+    translations.pushBack(rus);
+    dict[eng] = translations;
+  }
+  else
+  {
+    engIt->second.pushBack(rus);
+  }
+}
+
+void kiselev::doDeleteElement(std::istream& in, Dicts& dicts)
+{
+  std::string nameDict;
+  std::string eng;
+  in >> nameDict >> eng;
+  auto dictIt = dicts.find(nameDict);
+  if (dictIt == dicts.end())
+  {
+    std::cout << "<DICTIONARY NOT FOUND>\n";
+    return;
+  }
+  Dict dict = dictIt->second;
+  auto engIt = dict.find(eng);
+  if (engIt == dict.end())
+  {
+    std::cout << "<ENGWORD NOT FOUND>\n";
+    return;
+  }
+  dict.erase(eng);
+}
+
+void kiselev::doListDict(std::ostream& out, const Dicts& dicts)
+{
+  if (dicts.empty())
+  {
+    out << "<EMPTY>\n";
+    return;
+  }
+  for (auto it = dicts.cbegin(); it != dicts.cend(); ++it)
+  {
+    out << it->first << "\n";
+  }
 }
--- kiselev.sergey/F0/commands.hpp
+++ kiselev.sergey/F0/commands.hpp
@@ -10,5 +10,8 @@ namespace kiselev
   using Dicts = RBTree< std::string, Dict >;
   void doNewDict(std::istream&, Dicts&);
   void doDeleteDict(std::istream&, Dicts&);
+  void doAddElement(std::istream&, Dicts&);
+  void doDeleteElement(std::istream&, Dicts&);
+  void doListDict(std::ostream&, const Dicts&);
 }
 #endif
