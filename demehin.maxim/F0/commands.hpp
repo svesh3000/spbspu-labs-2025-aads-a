bb11af8cf1cc1c784075be779ce0af291c35416a
TvoyBatyA12343 <maxdem06@gmail.com>
2025-06-09 12:19:51 +0300

    little fix

--- demehin.maxim/F0/commands.hpp
+++ demehin.maxim/F0/commands.hpp
@@ -6,9 +6,9 @@
 
 namespace demehin
 {
-  using list_t = demehin::List< std::string >;
-  using tree_t = demehin::Tree< std::string, list_t >;
-  using dict_t = demehin::Tree< std::string, tree_t >;
+  using list_t = List< std::string >;
+  using tree_t = Tree< std::string, list_t >;
+  using dict_t = Tree< std::string, tree_t >;
 
   void printHelp(std::ostream&);
   void createDict(std::istream&, dict_t&);
--- demehin.maxim/F0/main.cpp
+++ demehin.maxim/F0/main.cpp
@@ -55,7 +55,7 @@ int main(int argc, char* argv[])
 
   if (argc != 2)
   {
-    std::cerr << "ERROR: invalid number of parameters";
+    std::cerr << "ERROR: invalid number of parameters\n";
     return 1;
   }
 
