ae5b7032161336419dd05104af79a169129bac13
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-06-09 23:49:43 +0300

    implement commands

--- maslov.aleksey/S7/graph.hpp
+++ maslov.aleksey/S7/graph.hpp
@@ -21,16 +21,148 @@ namespace maslov
   struct Graph
   {
     void addEdge(const std::string & v1, const std::string & v2, int weight);
+    void addVertex(const std::string & v);
+    void deleteEdge(const std::string & v1, const std::string & v2, int weight);
+    std::vector< std::string > getVertexes() const;
+    bool hasVertex(const std::string & vertex) const;
+    bool hasEdge(const std::string & v1, const std::string & v2, int weight) const;
+    std::vector< std::pair< std::string, int > > getOutbound(const std::string & vertex) const;
+    std::vector< std::pair< std::string, int > > getInbound(const std::string & vertex) const;
+    void merge(const Graph & graph);
+    void extract(const Graph & graph, const std::set< std::string > & extractVertexes);
    private:
     std::set< std::string > vertexes;
     std::unordered_map<std::pair< std::string, std::string >, std::vector< int >, PairHash > edges;
   };
 
+  void Graph::extract(const Graph & graph, const std::set< std::string > & extractVertexes)
+  {
+    for (auto it = extractVertexes.begin(); it != extractVertexes.end(); it++)
+    {
+      addVertex(*it);
+    }
+    for (auto it = graph.edges.begin(); it != graph.edges.end(); it++)
+    {
+      const auto vertexes = it->first;
+      if (hasVertex(vertexes.first) && hasVertex(vertexes.second))
+      {
+        for (auto itWeight = it->second.begin(); itWeight != it->second.end(); ++itWeight)
+        {
+          addEdge(vertexes.first, vertexes.second, *itWeight);
+        }
+      }
+    }
+  }
+
+  void Graph::merge(const Graph & graph)
+  {
+    for (auto it = graph.edges.begin(); it != graph.edges.end(); it++)
+    {
+      for (auto itWeight = it->second.begin(); itWeight != it->second.end(); ++itWeight)
+      {
+        addEdge(it->first.first, it->first.second, *itWeight);
+      }
+    }
+    for (auto it = graph.vertexes.begin(); it != graph.vertexes.end(); it++)
+    {
+      addVertex(*it);
+    }
+  }
+
+  void Graph::addVertex(const std::string & v)
+  {
+    vertexes.insert(v);
+  }
+
+  bool Graph::hasVertex(const std::string & vertex) const
+  {
+    return vertexes.find(vertex) != vertexes.end();
+  }
+
+  bool Graph::hasEdge(const std::string & v1, const std::string & v2, int weight) const
+  {
+    auto it = edges.find({v1, v2});
+    if (it == edges.end())
+    {
+      return false;
+    }
+    for (int existWeight: it->second)
+    {
+      if (existWeight == weight)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
   void Graph::addEdge(const std::string & v1, const std::string & v2, int weight)
   {
+    if (hasEdge(v1, v2, weight))
+    {
+      return;
+    }
     edges[{v1, v2}].push_back(weight);
     vertexes.insert(v1);
     vertexes.insert(v2);
+  }
+
+  void Graph::deleteEdge(const std::string & v1, const std::string & v2, int weight)
+  {
+    auto it = edges.find({v1, v2});
+    if (it == edges.end())
+    {
+      throw std::logic_error("ERROR: there is no such edge");
+    }
+    auto weights = it->second;
+    for (auto itWeight = weights.begin(); itWeight != weights.end(); itWeight++)
+    {
+      if (*itWeight == weight)
+      {
+        weights.erase(itWeight);
+        break;
+      }
+    }
+    throw std::logic_error("ERROR: there is no such edge");;
+  }
+
+  std::vector< std::string > Graph::getVertexes() const
+  {
+    return {vertexes.begin(), vertexes.end()};
+  }
+
+  std::vector< std::pair< std::string, int > > Graph::getOutbound(const std::string & vertex) const
+  {
+    std::vector< std::pair< std::string, int > > result;
+    for (auto it = edges.begin(); it != edges.end(); it++)
+    {
+      if (it->first.first == vertex)
+      {
+        for (int weight: it->second)
+        {
+          result.push_back({it->first.second, weight});
+        }
+      }
+    }
+    std::sort(result.begin(), result.end());
+    return result;
+  }
+
+  std::vector< std::pair< std::string, int > > Graph::getInbound(const std::string & vertex) const
+  {
+    std::vector< std::pair< std::string, int > > result;
+    for (auto it = edges.begin(); it != edges.end(); it++)
+    {
+      if (it->first.second == vertex)
+      {
+        for (int weight: it->second)
+        {
+          result.push_back({it->first.first, weight});
+        }
+      }
+    }
+    std::sort(result.begin(), result.end());
+    return result;
   }
 }
 
--- maslov.aleksey/S7/main.cpp
+++ maslov.aleksey/S7/main.cpp
@@ -3,36 +3,150 @@
 #include <fstream>
 #include <functional>
 #include <limits>
+#include <algorithm>
 #include "graph.hpp"
+
+namespace
+{
+  void printBounds(std::ostream & out, std::vector< std::pair< std::string, int > > bounds)
+  {
+    for (auto it = bounds.begin(); it != bounds.end(); it++)
+    {
+      out << it->first << ' ' << it->second << '\n';
+    }
+  }
+}
 
 namespace maslov
 {
   void getGraphs(std::ostream & out, const std::unordered_map< std::string, Graph > & graphs)
-  {}
+  {
+    std::vector< std::string > graphNames;
+    for (auto it = graphs.begin(); it != graphs.end(); it++)
+    {
+      graphNames.push_back(it->first);
+    }
+    std::sort(graphNames.begin(), graphNames.end());
+    for (auto it = graphNames.begin(); it != graphNames.end(); it++)
+    {
+      out << *it << '\n';
+    }
+  }
 
   void getVertexes(std::istream & in, std::ostream & out, const std::unordered_map< std::string, Graph > & graphs)
-  {}
+  {
+    std::string graphName;
+    in >> graphName;
+    Graph graph = graphs.at(graphName);
+    std::vector< std::string > vertexes = graph.getVertexes();
+    for (auto it = vertexes.begin(); it != vertexes.end(); it++)
+    {
+      out << *it << '\n';
+    }
+  }
 
   void getOutbound(std::istream & in, std::ostream & out, const std::unordered_map< std::string, Graph > & graphs)
-  {}
+  {
+    std::string graphName, vertexName;
+    in >> graphName >> vertexName;
+    Graph graph = graphs.at(graphName);
+    if (!graph.hasVertex(vertexName))
+    {
+      throw std::logic_error("ERROR: there is no such vertex");
+    }
+    auto outbounds = graph.getOutbound(vertexName);
+    printBounds(out, outbounds);
+  }
   
   void getInbound(std::istream & in, std::ostream & out, const std::unordered_map< std::string, Graph > & graphs)
-  {}
+  {
+    std::string graphName, vertexName;
+    in >> graphName >> vertexName;
+    Graph graph = graphs.at(graphName);
+    if (!graph.hasVertex(vertexName))
+    {
+      throw std::logic_error("ERROR: there is no such vertex");
+    }
+    auto inbounds = graph.getInbound(vertexName);
+    printBounds(out, inbounds);
+  }
 
   void createEdge(std::istream & in, std::unordered_map< std::string, Graph > & graphs)
-  {}
+  {
+    std::string graphName, v1, v2;
+    int weight;
+    in >> graphName >> v1 >> v2 >> weight;
+    Graph graph = graphs.at(graphName);
+    graph.addEdge(v1, v2, weight);
+  }
 
   void deleteEdge(std::istream & in, std::unordered_map< std::string, Graph > & graphs)
-  {}
+  {
+    std::string graphName, v1, v2;
+    int weight;
+    in >> graphName >> v1 >> v2 >> weight;
+    Graph graph = graphs.at(graphName);
+    graph.deleteEdge(v1, v2, weight);
+  }
 
   void createGraph(std::istream & in, std::unordered_map< std::string, Graph > & graphs)
-  {}
+  {
+    std::string graphName;
+    in >> graphName;
+    if (graphs.find(graphName) != graphs.end())
+    {
+      throw std::logic_error("ERROR: such a graph already exists");
+    }
+    Graph newGraph;
+    graphs[graphName] = newGraph;
+    size_t vertexesCount = 0;
+    in >> vertexesCount;
+    std::string vertex;
+    for (size_t i = 0; i < vertexesCount; i++)
+    {
+      in >> vertex;
+      newGraph.addVertex(vertex);
+    }
+  }
 
   void mergeGraph(std::istream & in, std::unordered_map< std::string, Graph > & graphs)
-  {}
+  {
+    std::string newGraphName, graphName1, graphName2;
+    in >> newGraphName >> graphName1 >> graphName2;
+    if (graphs.find(newGraphName) != graphs.end())
+    {
+      throw std::logic_error("ERROR: such a graph already exists");
+    }
+    Graph graph1 = graphs.at(graphName1);
+    Graph graph2 = graphs.at(graphName2);
+    Graph mergedGraph = graph1;
+    mergedGraph.merge(graph2);
+    graphs[newGraphName] = mergedGraph;  
+  }
 
   void extractGraph(std::istream & in, std::unordered_map< std::string, Graph > & graphs)
-  {}
+  {
+    std::string newGraphName, graphName;
+    in >> newGraphName >> graphName;
+    if (graphs.find(newGraphName) != graphs.end())
+    {
+      throw std::logic_error("ERROR: such a graph already exists");
+    }
+    Graph graph = graphs.at(graphName);
+    size_t countVertexes = 0;
+    in >> countVertexes;
+    std::set< std::string > vertexes;
+    std::string vertex;
+    for (size_t i = 0; i < countVertexes; ++i)
+    {
+      in >> vertex;
+      graph.hasVertex(vertex);
+      vertexes.insert(vertex);
+    }
+    Graph extractedGraph;
+    extractedGraph.extract(graph, vertexes);
+    graphs[newGraphName] = extractedGraph;
+  }
 
   void readGraphs(std::istream & in, std::unordered_map< std::string, Graph > & graphs)
   {
@@ -79,25 +193,18 @@ int main(int argc, char * argv[])
   cmds["merge"] = std::bind(maslov::mergeGraph, std::ref(std::cin), std::ref(graphs));
   cmds["extract"] = std::bind(maslov::extractGraph, std::ref(std::cin), std::ref(graphs));
 
-  try
+  readGraphs(fileInput, graphs);
+  std::string command;
+  while (!(std::cin >> command).eof())
   {
-    readGraphs(fileInput, graphs);
-    std::string command;
-    while (!(std::cin >> command).eof())
+    try
     {
-      try
-      {
-        cmds.at(command)();
-      }
-      catch (const std::exception &)
-      {
-        std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-        std::cout << "<INVALID COMMAND>\n";
-      }
+      cmds.at(command)();
     }
-  }
-  catch (const std::exception & e)
-  {
-    std::cerr << e.what() << '\n';
+    catch (const std::exception &)
+    {
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      std::cout << "<INVALID COMMAND>\n";
+    }
   }
 }
