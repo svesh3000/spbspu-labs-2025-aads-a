aa2b85934fb3fa96b5f29e3397d7b32bf8ac3e96
svesh3000 <russkij2006@mail.ru>
2025-06-10 18:46:57 +0300

    Main function

--- sveshnikov.michail/S4/main.cpp
+++ sveshnikov.michail/S4/main.cpp
@@ -1,2 +1,16 @@
-int main()
-{}
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+
+  if (argc ==1)
+  {
+
+  }
+  else
+  {
+    std::cerr << "ERROR: the file is not specified!";
+    return 1;
+  }
+  return 0;
+}
