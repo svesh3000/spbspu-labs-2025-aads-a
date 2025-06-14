31db0a7e9afabbc62fa865d532305103470bab30
svesh3000 <russkij2006@mail.ru>
2025-06-14 16:51:55 +0300

    Iterator and const iterator

--- sveshnikov.michail/S4/tree-iter.hpp
+++ sveshnikov.michail/S4/tree-iter.hpp
@@ -1,48 +1,50 @@
-#ifndef TREE_CITER_HPP
-#define TREE_CITER_HPP
+#ifndef TREE_ITER_HPP
+#define TREE_ITER_HPP
 #include <iterator>
 #include "tree-node.hpp"
 
 namespace sveshnikov
 {
   template< class Key, class T >
-  class ConstIter: public std::iterator< bidirectional_iterator_tag, Key, T >
+  class Iter: public std::iterator< bidirectional_iterator_tag, Key, T >
   {
   public:
     using value_t = std::pair< Key, T >;
-    ConstIter();
-    ConstIter(const tree_node_t< Key, T > *node);
-    ConstIter(const ConstIter &rhs) = default;
-    ~ConstIter() = default;
+    Iter();
+    Iter(const tree_node_t< Key, T > *node);
+    Iter(const Iter &rhs) = default;
+    ~Iter() = default;
 
-    ConstIter &operator=(const ConstIter &rhs) = default;
-    ConstIter &operator++() noexcept;
-    ConstIter operator++(int) noexcept;
-    ConstIter &operator--() noexcept;
-    ConstIter operator--(int) noexcept;
+    Iter &operator=(const Iter &rhs) = default;
+    Iter &operator++() noexcept;
+    Iter operator++(int) noexcept;
+    Iter &operator--() noexcept;
+    Iter operator--(int) noexcept;
 
     const value_t &operator*() const noexcept;
+    value_t &operator*() noexcept;
     const value_t *operator->() const noexcept;
+    value_t *operator->() noexcept;
 
-    bool operator!=(const ConstIter &rhs) const noexcept;
-    bool operator==(const ConstIter &rhs) const noexcept;
+    bool operator!=(const Iter &rhs) const noexcept;
+    bool operator==(const Iter &rhs) const noexcept;
 
   private:
     const tree_node_t< Key, T > *node_;
   };
 
   template< class Key, class T >
-  ConstIter< Key, T >::ConstIter():
+  Iter< Key, T >::Iter():
     node_(nullptr)
   {}
 
   template< class Key, class T >
-  ConstIter< Key, T >::ConstIter(const tree_node_t< Key, T > *node):
+  Iter< Key, T >::Iter(const tree_node_t< Key, T > *node):
     node_(node)
   {}
 
   template< class Key, class T >
-  ConstIter< Key, T > &ConstIter< Key, T >::operator++() noexcept
+  Iter< Key, T > &Iter< Key, T >::operator++() noexcept
   {
     assert(node_ != nullptr);
     if (node_->right_->height_ != 0)
@@ -66,16 +68,16 @@ namespace sveshnikov
   }
 
   template< class Key, class T >
-  ConstIter< Key, T > ConstIter< Key, T >::operator++(int) noexcept
+  Iter< Key, T > Iter< Key, T >::operator++(int) noexcept
   {
     assert(node_ != nullptr);
-    ConstIter result(*this);
+    Iter result(*this);
     ++(*this);
     return result;
   }
 
   template< class Key, class T >
-  ConstIter< Key, T > &ConstIter< Key, T >::operator--() noexcept
+  Iter< Key, T > &Iter< Key, T >::operator--() noexcept
   {
     assert(node_ != nullptr);
     if (node_->left_->height_ != 0)
@@ -98,36 +100,48 @@ namespace sveshnikov
   }
 
   template< class Key, class T >
-  ConstIter< Key, T > ConstIter< Key, T >::operator--(int) noexcept
+  Iter< Key, T > Iter< Key, T >::operator--(int) noexcept
   {
     assert(node_ != nullptr);
-    ConstIter result(*this);
+    Iter result(*this);
     --(*this);
     return result;
   }
 
   template< class Key, class T >
-  const ConstIter< Key, T >::value_t &ConstIter< Key, T >::operator*() const noexcept
+  const Iter< Key, T >::value_t &Iter< Key, T >::operator*() const noexcept
   {
     assert(node_ != nullptr);
     return node_->data_;
   }
 
   template< class Key, class T >
-  const ConstIter< Key, T >::value_t *ConstIter< Key, T >::operator->() const noexcept
+  Iter< Key, T >::value_t &Iter< Key, T >::operator*() noexcept
+  {
+    return const_cast< value_t & >(static_cast< const Iter< Key, T > & >(*this).operator*());
+  }
+
+  template< class Key, class T >
+  const Iter< Key, T >::value_t *Iter< Key, T >::operator->() const noexcept
   {
     assert(node_ != nullptr);
     return std::addressof(node_->data_);
   }
 
   template< class Key, class T >
-  bool ConstIter< Key, T >::operator!=(const ConstIter &rhs) const noexcept
+  Iter< Key, T >::value_t *Iter< Key, T >::operator->() noexcept
+  {
+    return const_cast< value_t * >(static_cast< const Iter< Key, T > * >(this)->operator->())
+  }
+
+  template< class Key, class T >
+  bool Iter< Key, T >::operator!=(const Iter &rhs) const noexcept
   {
     return node_ != rhs.node_;
   }
 
   template< class Key, class T >
-  bool ConstIter< Key, T >::operator==(const ConstIter &rhs) const noexcept
+  bool Iter< Key, T >::operator==(const Iter &rhs) const noexcept
   {
     return node_ == rhs.node_;
   }
