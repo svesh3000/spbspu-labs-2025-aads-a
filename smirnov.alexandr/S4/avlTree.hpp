ec5f2849c65aeeda5081027cf02ae68127f3aaab
A1exCRE <alsmmm2065@gmail.com>
2025-06-14 16:42:32 +0300

    realization AVLtree

--- smirnov.alexandr/S4/avlTree.hpp
+++ smirnov.alexandr/S4/avlTree.hpp
@@ -2,6 +2,7 @@
 #define AVL_TREE_HPP
 #include <cstddef>
 #include <stdexcept>
+#include <memory>
 #include "node.hpp"
 #include "constIterator.hpp"
 
@@ -11,7 +12,6 @@ namespace smirnov
   class AvlTree
   {
   public:
-    using NodeType = Node< Key, Value >;
     using const_iterator = ConstIterator< Key, Value, Compare >;
     AvlTree();
     AvlTree(const AvlTree & other);
@@ -31,19 +31,325 @@ namespace smirnov
     const_iterator cend() const noexcept;
     const_iterator find(const Key & key) const noexcept;
   private:
-    NodeType * root_;
+    Node< Key, Value > * root_;
     size_t size_;
     Compare comp_;
-    void clearNode(NodeType * node);
-    int height(NodeType * node) const;
-    void updateHeight(NodeType * node);
-    int getBalanceFactor(NodeType * node) const;
-    NodeType * rotateLeft(NodeType * node);
-    NodeType * rotateRight(NodeType * node);
-    NodeType * balance(NodeType * node);
-    NodeType * addOrGet(NodeType * node, const Key & key, const Value & value, size_t & size);
-    NodeType * findNode(NodeType * node, const Key & key) const;
-    NodeType * minNode(Node<Key, Value>* node) const;
+    void clearNode(Node< Key, Value > * node);
+    int height(Node< Key, Value > * node) const;
+    void updateHeight(Node< Key, Value > * node);
+    int getBalanceFactor(Node< Key, Value > * node) const;
+    Node< Key, Value > * rotateLeft(Node< Key, Value > * node);
+    Node< Key, Value > * rotateRight(Node< Key, Value > * node);
+    Node< Key, Value > * balance(Node< Key, Value > * node);
+    Node< Key, Value > * addOrGet(Node< Key, Value > * node, const Key & key, const Value & value, size_t & size);
+    Node< Key, Value > * findNode(Node< Key, Value > * node, const Key & key) const;
+    Node< Key, Value > * minNode(Node< Key, Value > * node) const;
   };
+
+  template < typename Key, typename Value, typename Compare >
+  AvlTree< Key, Value, Compare >::AvlTree():
+    root_(nullptr),
+    size_(0),
+    comp_(Compare())
+  {}
+
+  template < typename Key, typename Value, typename Compare >
+  AvlTree< Key, Value, Compare >::AvlTree(const AvlTree & other):
+    root_(nullptr),
+    size_(0),
+    comp_(other.comp_)
+  {
+    for (auto it = other.cbegin(); it != other.cend(); ++it)
+    {
+      operator[](it->first) = it->second;
+    }
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  AvlTree< Key, Value, Compare >::AvlTree(AvlTree && other) noexcept:
+    root_(other.root_),
+    size_(other.size_),
+    comp_(std::move(other.comp_))
+  {
+    other.root_ = nullptr;
+    other.size_ = 0;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  AvlTree< Key, Value, Compare >::~AvlTree()
+  {
+    clearNode(root_);
+    root_ = nullptr;
+    size_ = 0;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  AvlTree< Key, Value, Compare > & AvlTree< Key, Value, Compare >::operator=(const AvlTree & other)
+  {
+    if (this != std::addressof(other))
+    {
+      clear();
+      comp_ = other.comp_;
+      for (auto it = other.cbegin(); it != other.cend(); ++it)
+      {
+        operator[](it->first) = it->second;
+      }
+    }
+    return *this;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  AvlTree< Key, Value, Compare > & AvlTree< Key, Value, Compare >::operator=(AvlTree && other) noexcept
+  {
+    if (this != std::addressof(other))
+    {
+      clearNode(root_);
+      root_ = other.root_;
+      size_ = other.size_;
+      comp_ = std::move(other.comp_);
+      other.root_ = nullptr;
+      other.size_ = 0;
+    }
+    return *this;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  bool AvlTree< Key, Value, Compare >::empty() const noexcept
+  {
+    return size_ == 0;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  size_t AvlTree< Key, Value, Compare >::size() const noexcept
+  {
+    return size_;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  void AvlTree< Key, Value, Compare >::clear() noexcept
+  {
+    clearNode(root_);
+    root_ = nullptr;
+    size_ = 0;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  void AvlTree< Key, Value, Compare >::swap(AvlTree & other) noexcept
+  {
+    std::swap(root_, other.root_);
+    std::swap(size_, other.size_);
+    std::swap(comp_, other.comp_);
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  Value & AvlTree< Key, Value, Compare >::operator[](const Key & key)
+  {
+    root_ = addOrGet(root_, key, Value(), size_);
+    NodeType * node = findNode(root_, key);
+    return std::addressof(node->data)->second;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  const Value & AvlTree< Key, Value, Compare >::operator[](const Key & key) const
+  {
+    NodeType * node = findNode(root_, key);
+    if (!node)
+    {
+      throw std::out_of_range("Key not found");
+    }
+    return std::addressof(node->data)->second;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  Value & AvlTree< Key, Value, Compare >::at(const Key & key)
+  {
+    NodeType * node = findNode(root_, key);
+    if (!node)
+    {
+      throw std::out_of_range("Key not found");
+    }
+    return std::addressof(node->data)->second;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  const Value & AvlTree< Key, Value, Compare >::at(const Key & key) const
+  {
+    NodeType * node = findNode(root_, key);
+    if (!node)
+    {
+      throw std::out_of_range("Key not found");
+    }
+    return std::addressof(node->data)->second;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  typename AvlTree < Key, Value, Compare >::const_iterator AvlTree< Key, Value, Compare >::cbegin() const noexcept
+  {
+    return const_iterator(minNode(root_));
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  typename AvlTree< Key, Value, Compare >::const_iterator AvlTree< Key, Value, Compare >::cend() const noexcept
+  {
+    return const_iterator(nullptr);
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  typename AvlTree< Key, Value, Compare >::const_iterator AvlTree< Key, Value, Compare >::find(const Key & key) const noexcept
+  {
+    NodeType * node = findNode(root_, key);
+    if (node)
+    {
+      return const_iterator(node);
+    }
+    return cend();
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  void AvlTree< Key, Value, Compare >::clearNode(Node< Key, Value > * node)
+  {
+    if (!node)
+    {
+      return;
+    }
+    clearNode(node->left);
+    clearNode(node->right);
+    delete node;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  int AvlTree< Key, Value, Compare >::height(Node< Key, Value > * node) const
+  {
+    if (node == nullptr)
+    {
+      return 0;
+    }
+    else
+    {
+      return node->height;
+    }
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  void AvlTree< Key, Value, Compare >::updateHeight(Node< Key, Value > * node)
+  {
+    int leftH = height(node->left);
+    int rightH = height(node->right);
+    if (leftH > rightH)
+    {
+      node->height = leftH + 1;
+    }
+    else
+    {
+      node->height = rightH + 1;
+    }
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  int AvlTree< Key, Value, Compare >::getBalanceFactor(Node< Key, Value > * node) const
+  {
+    return height(node->left) - height(node->right);
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  Node< Key, Value > * AvlTree< Key, Value, Compare >::rotateLeft(Node< Key, Value > * node)
+  {
+    NodeType* newRoot = node->right;
+    node->right = newRoot->left;
+    newRoot->left = node;
+    updateHeight(node);
+    updateHeight(newRoot);
+    return newRoot;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  Node< Key, Value > * AvlTree< Key, Value, Compare >::rotateRight(Node< Key, Value > * node)
+  {
+    NodeType* newRoot = node->left;
+    node->left = newRoot->right;
+    newRoot->right = node;
+    updateHeight(node);
+    updateHeight(newRoot);
+    return newRoot;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  Node< Key, Value > * AvlTree< Key, Value, Compare >::balance(Node< Key, Value > * node)
+  {
+    updateHeight(node);
+    int balanceFactor = getBalanceFactor(node);
+    if (balanceFactor > 1)
+    {
+      if (getBalanceFactor(node->left) < 0)
+      {
+        node->left = rotateLeft(node->left);
+      }
+      return rotateRight(node);
+    }
+    if (balanceFactor < -1)
+    {
+      if (getBalanceFactor(node->right) > 0)
+      {
+        node->right = rotateRight(node->right);
+      }
+      return rotateLeft(node);
+    }
+    return node;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  Node< Key, Value > * AvlTree< Key, Value, Compare >::addOrGet(Node< Key, Value > * node, const Key & key, const Value & value, size_t & size)
+  {
+    if (!node)
+    {
+      ++size;
+      return new NodeType(key, value);
+    }
+    if (comp_(key, node->data.first))
+    {
+      node->left = addOrGet(node->left, key, value, size);
+    }
+    else if (comp_(node->data.first, key))
+    {
+      node->right = addOrGet(node->right, key, value, size);
+    }
+    else
+    {
+      node->data.second = value;
+    }
+    return balance(node);
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  Node< Key, Value > * AvlTree< Key, Value, Compare >::findNode(Node< Key, Value > * node, const Key & key) const
+  {
+    if (!node)
+    {
+      return nullptr;
+    }
+    if (comp_(key, node->data.first))
+    {
+      return findNode(node->left, key);
+    }
+    else if (comp_(node->data.first, key))
+    {
+      return findNode(node->right, key);
+    }
+    return node;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  Node< Key, Value > * AvlTree< Key, Value, Compare >::minNode(Node< Key, Value > * node) const
+  {
+    if (!node)
+    {
+      return nullptr;
+    }
+    while (node->left)
+    {
+      node = node->left;
+    }
+    return node;
+  }
 }
 #endif
--- smirnov.alexandr/S4/constIterator.hpp
+++ smirnov.alexandr/S4/constIterator.hpp
@@ -1,7 +1,7 @@
 #ifndef CONST_ITERATOR_HPP
 #define CONST_ITERATOR_HPP
-#include "node.hpp"
 #include <functional>
+#include "node.hpp"
 
 namespace smirnov
 {
