d9d263e6e56095a4737ef7def142414afa5348be
A1exCRE <alsmmm2065@gmail.com>
2025-06-14 17:15:22 +0300

    realization citerator

--- smirnov.alexandr/S4/constIterator.hpp
+++ smirnov.alexandr/S4/constIterator.hpp
@@ -22,5 +22,47 @@ namespace smirnov
     explicit ConstIterator(Node< Key, Value > * node);
     Node< Key, Value > * current_;
   };
+
+  template < typename Key, typename Value, typename Compare >
+  ConstIterator< Key, Value, Compare >::ConstIterator():
+    current_(nullptr)
+  {}
+
+  template < typename Key, typename Value, typename Compare >
+  ConstIterator<Key, Value, Compare>::ConstIterator(Node< Key, Value > * node):
+    current_(node)
+  {}
+
+  template < typename Key, typename Value, typename Compare >
+  const std::pair< const Key, Value > & ConstIterator< Key, Value, Compare >::operator*() const
+  {
+    return current_->data;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  const std::pair< const Key, Value > * ConstIterator< Key, Value, Compare >::operator->() const
+  {
+    return std::adressof(current_->data);
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  ConstIterator< Key, Value, Compare > & ConstIterator< Key, Value, Compare >::operator++()
+  {
+    current_ = nullptr;
+    return *this;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  bool ConstIterator< Key, Value, Compare >::operator==(const ConstIterator & other) const
+  {
+    return current_ == other.current_;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  bool ConstIterator< Key, Value, Compare >::operator!=(const ConstIterator & other) const
+  {
+    return !(*this == other);
+  }
 }
 #endif
+
