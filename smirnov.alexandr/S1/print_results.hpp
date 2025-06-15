2fb1015f81365ca2f4e1eaa2cb0587d5c63faa37
A1exCRE <alsmmm2065@gmail.com>
2025-06-15 14:17:44 +0300

    reconnecting

--- smirnov.alexandr/S1/print_results.hpp
+++ smirnov.alexandr/S1/print_results.hpp
@@ -2,7 +2,7 @@
 #define PRINT_RESULTS_HPP
 #include <iostream>
 #include <string>
-#include "list.hpp"
+#include <common/list/list.hpp>
 
 namespace smirnov
 {
--- smirnov.alexandr/S1/read_sequences.hpp
+++ smirnov.alexandr/S1/read_sequences.hpp
@@ -2,7 +2,7 @@
 #define READ_SEQUENCES_HPP
 #include <iostream>
 #include <string>
-#include "list.hpp"
+#include <common/list/list.hpp>
 
 namespace smirnov
 {
