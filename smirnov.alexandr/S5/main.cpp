9bcb5120b59d3a4fb03a3720ccfa893ce58fad22
A1exCRE <alsmmm2065@gmail.com>
2025-06-16 14:21:57 +0300

    create main.cpp

--- smirnov.alexandr/S5/main.cpp
+++ smirnov.alexandr/S5/main.cpp
@@ -1,2 +1,63 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <tree/avlTee.hpp>
+#include "key_sum.hpp"
+
+int main(int argc, char* argv[])
+{
+  using namespace smirnov;
+  AvlTree< int, std::string > dict;
+  if (argc == 3)
+  {
+    int key = 0;
+    std::string value = "";
+    std::ifstream file(argv[2]);
+    while (file >> key >> value)
+    {
+      dict[key] = value;
+    }
+    if (!file.eof())
+    {
+      std::cerr << "Bad input file\n";
+      return 1;
+    }
+  }
+  else
+  {
+    std::cerr << "No such file\n";
+    return 1;
+  }
+  try
+  {
+    KeySum func;
+    std::string mode(argv[1]);
+    if (mode == "ascending")
+    {
+      func = dict.traverse_lnr(func);
+    }
+    else if (mode == "descending")
+    {
+      func = dict.traverse_rnl(func);
+    }
+    else if (mode == "breadth")
+    {
+      func = dict.traverse_breadth(func);
+    }
+    else
+    {
+      std::cerr << "Invalid traversal mode\n";
+      return 1;
+    }
+    std::cout << func.result << func.elems << "\n";
+  }
+  catch (const std::overflow_error & e)
+  {
+    std::cerr << "Error: " << e.what() << "\n";
+    return 1;
+  }
+  catch (const std::exception & e)
+  {
+    std::cerr << "Error: " << e.what() << "\n";
+    return 1;
+  }
+}
