5eaaf0ddd466653953d3bea3bb8cd1186b658967
Might be a Fox <e.kizhin@yandex.ru>
2025-06-19 15:48:55 +0300

    Remove Args Parser from S5

--- kizhin.evgeniy/S5/main.cpp
+++ kizhin.evgeniy/S5/main.cpp
@@ -1,17 +1,23 @@
+#include <cstring>
 #include <fstream>
 #include <iostream>
 #include <string>
-#include "args-parser.hpp"
 #include "map-utils.hpp"
 
 int main(int argc, char** argv)
 {
+  if (argc != 3 || argv[2][0] == '\0') {
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " <ascending|descending|breadth> <filename>";
+    return 1;
+  }
+  const char* filename = argv[2];
   using namespace kizhin;
   try {
-    Args args = parseArgs(argc, argv, std::cerr);
-    std::fstream in(args.filename);
+    std::fstream in(filename);
     if (!in.is_open()) {
-      throw std::logic_error("Failed to open file: " + args.filename);
+      std::cerr << "Failed to open file: " << filename << '\n';
+      return 1;
     }
     MapT map;
     if (!(in >> map)) {
@@ -19,12 +25,14 @@ int main(int argc, char** argv)
       return 0;
     }
     ValueCollector result;
-    if (args.traverse == args.ascending) {
+    if (std::strcmp(argv[1], "ascending") == 0) {
       result = map.traverseLmr(result);
-    } else if (args.traverse == args.descending) {
+    } else if (std::strcmp(argv[1], "descending") == 0) {
       result = map.traverseRml(result);
-    } else {
+    } else if (std::strcmp(argv[1], "breadth") == 0) {
       result = map.traverseBreadth(result);
+    } else {
+      throw std::logic_error("Invalid traverse type given");
     }
     std::cout << result.keys << ' ' << result.values << '\n';
   } catch (const std::exception& e) {
