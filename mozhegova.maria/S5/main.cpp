ae21b7322a10c45368ee895f41fac2efe14e26f3
mipxe <mariamozegova8726@gmail.com>
2025-06-20 22:23:27 +0300

    create functional object

--- mozhegova.maria/S5/main.cpp
+++ mozhegova.maria/S5/main.cpp
@@ -1,2 +1,20 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <tree.hpp>
+#include "key_summ.hpp"
+
+int main(int argc, char * argv[])
+{
+  if (argc != 3)
+  {
+    std::cerr << "wrong arguments\n";
+    return 1;
+  }
+  std::ifstream file(argv[2]);
+  if (!file.is_open())
+  {
+    std::cerr << "file is not open\n";
+    return 1;
+  }
+
+}
