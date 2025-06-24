fe3fb72f13843ffba9257b46268b813031ea38fd
Ksenia-rgb <ksenialymova@mail.ru>
2025-06-24 15:52:35 +0300

    add and fix commands, add help, add command process

--- alymova.ksenia/F0/commands.cpp
+++ alymova.ksenia/F0/commands.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <string>
 #include <exception>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
 #include "dict-utils.hpp"
 
 namespace
@@ -17,6 +20,29 @@ namespace
       }
     }
     return it;
+  }
+  void unionLists(alymova::List< std::string >& list, const alymova::List< std::string >& unioned)
+  {
+    alymova::List< std::string > copy(unioned);
+    list.sort();
+    copy.sort();
+    list.merge(copy);
+    list.unique();
+  }
+  void intersectLists(alymova::List< std::string >& list, const alymova::List< std::string >& intersected)
+  {
+    for (auto it = list.begin(); it != list.end();)
+    {
+      auto it1 = findTranslate(intersected, *it);
+      if (it1 == intersected.cend())
+      {
+        it = list.erase(it);
+      }
+      else
+      {
+        it++;
+      }
+    }
   }
 }
 
@@ -46,18 +72,13 @@ void alymova::size(std::istream& in, std::ostream& out, const DictSet& set)
   {
     throw std::logic_error("<INVALID ERROR>");
   }
-  auto it = set.find(name);
-  if (it == set.end())
-  {
-    out << "<NOT FOUND>";
-    return;
-  }
-  if (it->second.size() == 0)
+  const Dictionary& dict = set.at(name);
+  if (dict.size() == 0)
   {
     out << "<EMPTY>";
     return;
   }
-  out << it->second.size();
+  out << dict.size();
 }
 
 void alymova::removeDict(std::istream& in, std::ostream& out, DictSet& set)
@@ -68,12 +89,7 @@ void alymova::removeDict(std::istream& in, std::ostream& out, DictSet& set)
   {
     throw std::logic_error("<INVALID COMMAND>");
   }
-  auto it = set.find(name);
-  if (it == set.end())
-  {
-    out << "<NOT FOUND>";
-    return;
-  }
+  set.at(name);
   set.erase(name);
   out << "<SUCCESSFULLY REMOVED>";
 }
@@ -86,13 +102,7 @@ void alymova::addWord(std::istream& in, std::ostream& out, DictSet& set)
   {
     throw std::logic_error("<INVALID COMMAND>");
   }
-  auto it_dict = set.find(name);
-  if (it_dict == set.end())
-  {
-    out << "<NOT FOUND>";
-    return;
-  }
-  Dictionary& dict = it_dict->second;
+  Dictionary& dict = set.at(name);
   auto it_word = dict.find(word);
   if (it_word == dict.end())
   {
@@ -101,7 +111,7 @@ void alymova::addWord(std::istream& in, std::ostream& out, DictSet& set)
     out << "<ADDED WORD AND TRANSLATE>";
     return;
   }
-  TranslateSet& translates = it_word->second;
+  List< std::string >& translates = it_word->second;
   if (findTranslate(translates, translate) == translates.cend())
   {
     translates.push_back(translate);
@@ -119,20 +129,9 @@ void alymova::fixWord(std::istream& in, std::ostream& out, DictSet& set)
   {
     throw std::logic_error("<INVALID COMMAND>");
   }
-  auto it_dict = set.find(name);
-  if (it_dict == set.end())
-  {
-    out << "<NOT FOUND>";
-    return;
-  }
-  Dictionary& dict = it_dict->second;
-  auto it_word = dict.find(word);
-  if (it_word == dict.end())
-  {
-    out << "<NOT FOUND>";
-    return;
-  }
-  it_word->first = new_word;
+  Dictionary& dict = set.at(name);
+  dict.at(word);
+  dict.find(word)->first = new_word;
   out << "<SUCCESSFULLY FIXED>";
 }
 
@@ -144,23 +143,11 @@ void alymova::findWord(std::istream& in, std::ostream& out, const DictSet& set)
   {
     throw std::logic_error("<INVALID COMMAND>");
   }
-  auto it_dict = set.find(name);
-  if (it_dict == set.end())
-  {
-    out << "<NOT FOUND>";
-    return;
-  }
-  const Dictionary& dict = it_dict->second;
-  auto it_word = dict.find(word);
-  if (it_word == dict.end())
-  {
-    out << "<NOT FOUND>";
-    return;
-  }
-  out << it_word->second;
+  const Dictionary& dict = set.at(name);
+  out << dict.at(word);
 }
 
-void alymova::containSubword(std::istream& in, std::ostream& out, DictSet& set)
+void alymova::containSubword(std::istream& in, std::ostream& out, const DictSet& set)
 {
   std::string name, subword;
   in >> name >> subword;
@@ -168,13 +155,7 @@ void alymova::containSubword(std::istream& in, std::ostream& out, DictSet& set)
   {
     throw std::logic_error("<INVALID COMMAND>");
   }
-  auto it_dict = set.find(name);
-  if (it_dict == set.end())
-  {
-    out << "<NOT FOUND>";
-    return;
-  }
-  Dictionary& dict = it_dict->second;
+  const Dictionary& dict = set.at(name);
   List< std::string > suitable;
   for (auto it = dict.begin(); it != dict.end(); it++)
   {
@@ -198,59 +179,48 @@ void alymova::removeWord(std::istream& in, std::ostream& out, DictSet& set)
   {
     throw std::logic_error("<INVALID COMMAND>");
   }
-  auto it_dict = set.find(name);
-  if (it_dict == set.end())
-  {
-    out << "<NOT FOUND>";
-    return;
-  }
-  Dictionary& dict = it_dict->second;
-  size_t cnt = dict.erase(word);
-  if (cnt == 0)
-  {
-    out << "<NOT FOUND>";
-    return;
-  }
+  Dictionary& dict = set.at(name);
+  dict.at(word);
+  dict.erase(word);
   out << "<SUCCESSFULLY REMOVED>";
 }
 
 void alymova::addTranslate(std::istream& in, std::ostream& out, DictSet& set)
 {
-  addWord(in, out, set);
+  std::string name, word, translate;
+  in >> name >> word >> translate;
+  if (!in)
+  {
+    throw std::logic_error("<INVALID COMMAND>");
+  }
+  Dictionary& dict = set.at(name);
+  List< std::string >& translates = dict.at(word);
+  if (findTranslate(translates, translate) != translates.cend())
+  {
+    throw std::logic_error("Such translate already added");
+  }
+  translates.push_back(translate);
+  out << "<SUCCESSFULL ADDED>";
 }
 
 void alymova::findEnglishEquivalent(std::istream& in, std::ostream& out, const DictSet& set)
 {
   std::string name;
-  in >> name;
+  List< std::string > translates;
+  in >> name >> translates;
   if (!in)
   {
     throw std::logic_error("<INVALID COMMAND>");
   }
-  auto it_dict = set.find(name);
-  if (it_dict == set.end())
-  {
-    out << "<NOT FOUND>";
-    return;
-  }
-
-  List< std::string > translates, equivalents;
-  in >> translates;
-  if (!in)
-  {
-    throw std::logic_error("<INVALID COMMAND>");
-  }
-  const Dictionary& dict = it_dict->second;
+  const Dictionary& dict = set.at(name);
+  List< std::string > equivalents;
   for (auto it = dict.begin(); it != dict.end(); it++)
   {
-    const TranslateSet& old_tr = it->second;
-    for (auto it_list = translates.begin(); it_list != translates.end(); it_list++)
+    List< std::string > current_translates = it->second;
+    intersectLists(current_translates, translates);
+    if (!current_translates.empty())
     {
-      if (findTranslate(old_tr, *it_list) != old_tr.cend())
-      {
-        equivalents.push_back(it->first);
-        break;
-      }
+      equivalents.push_back(it->first);
     }
   }
   if (equivalents.empty())
@@ -269,26 +239,14 @@ void alymova::removeTranslate(std::istream& in, std::ostream& out, DictSet& set)
   {
     throw std::logic_error("<INVALID COMMAND>");
   }
-  auto it_dict = set.find(name);
-  if (it_dict == set.end())
+  Dictionary& dict = set.at(name);
+  List< std::string >& translates = dict.at(word);
+  auto it_translate = findTranslate(translates, translate);
+  if (it_translate == translates.cend())
   {
-    out << "<NOT FOUND>";
-    return;
+    throw std::logic_error("<INVALID COMMAND>");
   }
-  Dictionary& dict = it_dict->second;
-  auto it_word = dict.find(word);
-  if (it_word == dict.end())
-  {
-    out << "<NOT FOUND>";
-    return;
-  }
-  auto it_translate = findTranslate(it_word->second, translate);
-  if (it_translate == it_word->second.cend())
-  {
-    out << "<NOT FOUND>";
-    return;
-  }
-  it_word->second.erase(it_translate);
+  translates.erase(it_translate);
   out << "<SUCCESSFULLY REMOVED>";
 }
 
@@ -300,35 +258,29 @@ void alymova::printContent(std::istream& in, std::ostream& out, const DictSet& s
   {
     throw std::logic_error("<INVALID COMMAND>");
   }
-  auto it_dict = set.find(name);
-  if (it_dict == set.end())
-  {
-    out << "<NOT FOUND>";
-    return;
-  }
-  const Dictionary& dict = it_dict->second;
+  const Dictionary& dict = set.at(name);
   if (dict.empty())
   {
-    out << '\n';
     return;
   }
-  char c = dict.begin()->first[0];
-  out << c;
-  for (auto it = dict.begin(); it != dict.end(); it++)
+  auto it = dict.begin();
+  char first_letter = it->first[0];
+  out << first_letter << ' ' << (it++)->first;
+  for (; it != dict.end(); it++)
   {
-    if (it->first[0] == c)
+    if (it->first[0] == first_letter)
     {
       out << ' ' << it->first;
     }
     else
     {
-      c = it->first[0];
-      out << '\n' << c << ' ' << it->first;
+      first_letter = it->first[0];
+      out << '\n' << first_letter << ' ' << it->first;
     }
   }
 }
 
-void alymova::translate(std::istream& in, std::ostream& out, DictSet& set)
+void alymova::translate(std::istream& in, std::ostream& out, const DictSet& set)
 {
   List< std::string > names;
   std::string word;
@@ -341,13 +293,7 @@ void alymova::translate(std::istream& in, std::ostream& out, DictSet& set)
   List< Dictionary > dicts;
   for (auto it = names.begin(); it != names.end(); it++)
   {
-    auto it_dict = set.find(*it);
-    if (it_dict == set.end())
-    {
-      out << "<NOT FOUND>";
-      return;
-    }
-    dicts.push_back(it_dict->second);
+    dicts.push_back(set.at(*it));
   }
   List< std::string > translates;
   for (auto it = dicts.begin(); it != dicts.end(); it++)
@@ -363,6 +309,112 @@ void alymova::translate(std::istream& in, std::ostream& out, DictSet& set)
     out << "<NOT FOUND>";
     return;
   }
+  translates.sort();
   translates.unique();
   out << translates;
+}
+
+void alymova::unionDicts(std::istream& in, std::ostream& out, DictSet& set)
+{
+  std::string newname, name1, name2;
+  in >> newname >> name1 >> name2;
+  if (!in)
+  {
+    throw std::logic_error("<INVALID COMMAND>");
+  }
+  Dictionary dict1 = set.at(name1);
+  Dictionary dict2 = set.at(name2);
+  for (auto it = dict2.begin(); it != dict2.end(); it++)
+  {
+    auto it1 = dict1.find(it->first);
+    if (it1 == dict1.end())
+    {
+      dict1.insert({it->first, it->second});
+    }
+    else
+    {
+      unionLists(it1->second, it->second);
+    }
+  }
+  set[newname] = dict1;
+  out << "<SUCCESSFULLY UNIONED>";
+}
+
+void alymova::intersectDicts(std::istream& in, std::ostream& out, DictSet& set)
+{
+  std::string newname, name1, name2;
+  in >> newname >> name1 >> name2;
+  if (!in)
+  {
+    throw std::logic_error("<INVALID COMMAND>");
+  }
+  Dictionary dict1 = set.at(name1);
+  Dictionary dict2 = set.at(name2);
+  for (auto it = dict2.begin(); it != dict2.end();)
+  {
+    auto it1 = dict1.find(it->first);
+    if (it1 == dict1.end())
+    {
+      it = dict2.erase(it);
+    }
+    else
+    {
+      intersectLists(it->second, it1->second);
+      it++;
+    }
+  }
+  set[newname] = dict2;
+  out << "<SUCCESSFULLY INTERSECTED>";
+}
+
+void alymova::printDayWord(std::istream& in, std::ostream& out, const DictSet& set)
+{
+  std::string name;
+  in >> name;
+  if (!in)
+  {
+    throw std::logic_error("<INVALID COMMAND>");
+  }
+  const Dictionary& dict = set.at(name);
+  std::srand(std::time(nullptr));
+  int random_num = std::rand() % dict.size();
+  auto it_word = dict.begin();
+  for (size_t i = 0; i < random_num; i++, it_word++);
+  out << "Have a good day with word: " << it_word->first;
+}
+
+void alymova::printHelp(std::ostream& out)
+{
+  out << "You can use some commands\n";
+  out << std::setw(60) << std::left << "create <dict> " << "create new empty dictionary\n";
+  out << std::setw(60) << std::left << "size <name> " << "print dictionary size\n";
+  out << std::setw(60) << std::left << "remove_dictionary <name> " << "remove existing dictionary\n";
+  out << std::setw(60) << std::left << "add_word <dict> <word> <translate> ";
+  out << "add word with translate in dictionary\n";
+  out << std::setw(60) << std::left << "fix_word <dict> <word> <new_word> ";
+  out << "replace word in dictionary by new_word\n";
+  out << std::setw(60) << std::left << "find_word <dict> <word> ";
+  out << "print word translates from dictionary\n";
+  out << std::setw(60) << std::left << "contain <dict> <subword> ";
+  out << "print words that contain subword in dictionary\n";
+  out << std::setw(60) << std::left << "remove_word <dict> <word> ";
+  out << "remove existing word from existing dictionary\n";
+  out << std::setw(60) << std::left << "add_translate <dict> <word> <translate> ";
+  out << "add new translate to existing word in dictionary\n";
+  out << std::setw(60) << std::left << "find_english_equivalent <dict> <tr_count> <tr_1> ... <tr_n> ";
+  out << "find english equivalents of translates in dictionary\n";
+  out << std::setw(60) << std::left << "remove_translate <dict> <word> <translate> ";
+  out << "remove existing translate of existing word in existing dictionary\n";
+  out << std::setw(60) << std::left << "print_content <dict> ";
+  out << "print dictionary content by letters\n";
+  out << std::setw(60) << std::left << "translate <dict_count> <dict_1> ... <dict_n> <word> ";
+  out << "print word translates from dictionaries\n";
+  out << std::setw(60) << std::left << "union <dict1> <dict2> ";
+  out << "create new dictionary as union of dictionary1, dictionary2\n";
+  out << std::setw(60) << std::left << "intersect <dict1> <dict2> ";
+  out << "create new dictionary as intersection of dictionary1, dictionary2\n";
+  out << std::setw(60) << std::left << "day_word <dict> ";
+  out << "print word of day:)\n";
+  out << std::setw(60) << std::left << "save <file> " << "save all dictionaries in file\n";
+  out << "Result of programm work will be saved in file <dictionaries.txt> as default\n";
 }
--- alymova.ksenia/F0/main.cpp
+++ alymova.ksenia/F0/main.cpp
@@ -2,11 +2,17 @@
 #include <fstream>
 #include <clocale>
 #include <cstring>
+#include <limits>
 #include "commands.hpp"
 
 int main(int argc, char** argv)
 {
   using namespace alymova;
+  using namespace std::placeholders;
+  using TranslateSet = List< std::string >;
+  using Dictionary = TwoThreeTree< std::string, TranslateSet, std::less< std::string > >;
+  using DictSet = TwoThreeTree< std::string, Dictionary, std::less< std::string > >;
+  using CommandSet = TwoThreeTree< std::string, std::function< void() >, std::less< std::string > >;
   std::setlocale(LC_CTYPE, "rus");
 
   if (argc > 2)
@@ -20,6 +26,7 @@ int main(int argc, char** argv)
   {
     if (std::strcmp(argv[1], "--help") == 0)
     {
+      printHelp(std::cout);
       return 0;
     }
     file.open(argv[1]);
@@ -33,30 +40,51 @@ int main(int argc, char** argv)
 
   try
   {
-    TranslateSet tr1{"yabloko"};
-    TranslateSet tr2{"banan", "bananovyj"};
-    Dictionary dict{{"apple", tr1}, {"banana", tr2}};
-    Dictionary dict1(dict);
-    DictSet set{{"first", dict}, {"second", dict}};
-    translate(*input, std::cout, set);
+    //DictSet dataset = readDictionaryFile(*input);
+    DictSet dataset;
+    CommandSet commands;
+    commands["create"] = std::bind(create, std::ref(std::cin), std::ref(std::cout), std::ref(dataset));
+    commands["size"] = std::bind(size, std::ref(std::cin), std::ref(std::cout), std::cref(dataset));
+    commands["remove_dictionary"] = std::bind(removeDict, std::ref(std::cin), std::ref(std::cout), std::ref(dataset));
+    commands["add_word"] = std::bind(addWord, std::ref(std::cin), std::ref(std::cout), std::ref(dataset));
+    commands["fix_word"] = std::bind(fixWord, std::ref(std::cin), std::ref(std::cout), std::ref(dataset));
+    commands["find_word"] = std::bind(findWord, std::ref(std::cin), std::ref(std::cout), std::cref(dataset));
+    commands["contain"] = std::bind(containSubword, std::ref(std::cin), std::ref(std::cout), std::cref(dataset));
+    commands["remove_word"] = std::bind(removeWord, std::ref(std::cin), std::ref(std::cout), std::ref(dataset));
+    commands["add_translate"] = std::bind(addTranslate, std::ref(std::cin), std::ref(std::cout), std::ref(dataset));
+
+    auto short_name1 = std::bind(findEnglishEquivalent, std::ref(std::cin), std::ref(std::cout), std::cref(dataset));
+    commands["find_english_equivalent"] = short_name1;
+
+    auto short_name2 = std::bind(removeTranslate, std::ref(std::cin), std::ref(std::cout), std::ref(dataset));
+    commands["remove_translate"] = short_name1;
+
+    commands["print_content"] = std::bind(printContent, std::ref(std::cin), std::ref(std::cout), std::cref(dataset));
+    commands["translate"] = std::bind(translate, std::ref(std::cin), std::ref(std::cout), std::cref(dataset));
+    commands["union"] = std::bind(unionDicts, std::ref(std::cin), std::ref(std::cout), std::ref(dataset));
+    commands["intersect"] = std::bind(intersectDicts, std::ref(std::cin), std::ref(std::cout), std::ref(dataset));
+    commands["day_word"] = std::bind(printDayWord, std::ref(std::cin), std::ref(std::cout), std::ref(dataset));
+    std::string command;
+    while (!(std::cin >> command).eof())
+    {
+      try
+      {
+        commands.at(command)();
+        std::cout << '\n';
+      }
+      catch (const std::exception& e)
+      {
+        if (std::cin.fail())
+        {
+          std::cin.clear(std::cin.rdstate() ^ std::ios::failbit);
+        }
+        std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+        std::cout << "<INVALID COMMAND>\n";
+      }
+    }
   }
   catch (const std::exception& e)
   {
     std::cerr << e.what() << '\n';
   }
-  /*printContent(std::cin, std::cout, set);
-  std::cout << '\n';
-  for (auto it0 = set.begin(); it0 != set.end(); it0++)
-  {
-    std::cout << it0->first << '\n';
-    for (auto it = it0->second.begin(); it != it0->second.end(); it++)
-    {
-      std::cout << it->first;
-      for (auto it1 = it->second.begin(); it1 != it->second.end(); it1++)
-      {
-        std::cout << ' ' << *it1;
-      }
-      std::cout << '\n';
-    }
-  }*/
 }
