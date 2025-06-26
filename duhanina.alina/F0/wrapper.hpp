38ac3785dcb70a7d200a2e34a8752a2a24b9c324
alina <duhaninaalina3@gmail.com>
2025-06-27 00:25:29 +0300

    start

---
+++ duhanina.alina/F0/wrapper.hpp
@@ -0,0 +1,26 @@
+#ifndef WRAPPER_HPP
+#define WRAPPER_HPP
+
+#include <iostream>
+#include <functional>
+#include <tree.hpp>
+
+namespace duhanina
+{
+  void build_codes_wrapper(std::istream&, std::ostream&);
+  void show_codes_wrapper(std::istream&, std::ostream&);
+  void save_codes_wrapper(std::istream&, std::ostream&);
+  void load_codes_wrapper(std::istream&, std::ostream&);
+  void clear_codes_wrapper(std::istream&, std::ostream&);
+  void encode_file_with_codes_wrapper(std::istream&, std::ostream&);
+  void decode_file_with_codes_wrapper(std::istream&, std::ostream&);
+  void encode_file_wrapper(std::istream&, std::ostream&);
+  void decode_file_wrapper(std::istream&, std::ostream&);
+  void compare_wrapper(std::istream&, std::ostream&);
+  void suggest_encodings_wrapper(std::istream& in, std::ostream& out);
+  void check_encoding_wrapper(std::istream& in, std::ostream& out);
+
+  Tree< std::string, std::function< void() >, std::less< std::string > > initialize_commands(std::istream& in, std::ostream& out);
+}
+
+#endif
