5a5d4e8f8c1f6b3bb9d6fe60fa8da77f7d84a01d
Nekich06 <p.nikita.73526@gmail.com>
2025-06-27 12:54:33 +0300

    Implemented input of data from file

--- petrov.nikita/S5/main.cpp
+++ petrov.nikita/S5/main.cpp
@@ -1,2 +1,50 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <avl_tree.hpp>
+
+namespace
+{
+  int checkArguments(const int argc);
+
+  int checkArguments(const int argc)
+  {
+    if (argc == 3)
+    {
+      return 1;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+}
+
+int main(const int argc, const char * const * argv)
+{
+  using namespace petrov;
+  if (!checkArguments(argc))
+  {
+    std::cerr << "ERROR: Invalid number of command line arguments";
+    std::cerr << '\n';
+    return 1;
+  }
+  AVLTree< int, std::string > tree;
+  std::ifstream in(argv[2]);
+  while (!in.eof())
+  {
+    if (in.eof())
+    {
+      break;
+    }
+    int key = 0;
+    std::string val;
+    if (in >> key >> val)
+    {
+      tree.insert({ key, val });
+    }
+  }
+  for (auto it = tree.cbegin(); it != tree.cend(); ++it)
+  {
+    std::cout << it->second << "\n";
+  }
+}
