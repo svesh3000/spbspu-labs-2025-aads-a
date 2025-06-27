bc26e8bfd68a53d8a1a6e62960989cd41706429a
abramov_vladislav_ <vladabramov06@yandex.ru>
2025-06-27 22:01:58 +0300

    add dictionary_collection.hpp

---
+++ abramov.vladislav/F0/dictionary_collection.hpp
@@ -0,0 +1,19 @@
+#ifndef DICTIONARY_COLLECTION_HPP
+#define DICTIONARY_COLLECTION_HPP
+#include "dictionary.hpp"
+
+namespace abramov
+{
+  struct DictionaryCollection
+  {
+    DictionaryCollection();
+    Dictionary &findDict(const std::string &name);
+    const Dictionary &cfindDict(const std::string &name) const;
+    void addDict(const std::string &name);
+    void addCompleteDict(const std::string &name, const Dictionary &dict);
+    void deleteDict(const std::string &name);
+  private:
+    HashTable< std::string, Dictionary > collection_;
+  };
+}
+#endif
