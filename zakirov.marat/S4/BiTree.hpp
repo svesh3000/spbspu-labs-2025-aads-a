18be574c7178bff9719d1464d917aee1c9c6a9c8
ZakirovMS <mz.08.08.06@gmail.com>
2025-06-27 21:29:24 +0300

    Erase and clear tree methods

--- zakirov.marat/S4/BiTree.hpp
+++ zakirov.marat/S4/BiTree.hpp
@@ -40,9 +40,9 @@ namespace zakirov
 
     std::pair< BiIter< K, T, C >, bool > insert(const std::pair< K, T > & value);
     std::pair< BiIter< K, T, C >, bool > insert(BiIter< K, T, C > pos, const std::pair< K, T > & value);
-    BiIter< K, T, C > erase(BiIter< K, T, C > pos);
-    BiIter< K, T, C > erase(CBiIter< K, T, C > pos);
-    T erase(const K & key);
+    BiIter< K, T, C > erase(BiIter< K, T, C > pos) noexcept;
+    BiIter< K, T, C > erase(CBiIter< K, T, C > pos) noexcept;
+    size_t erase(const K & key) noexcept;
     void clear() noexcept;
 
     BiIter find(const K & key);
@@ -53,6 +53,8 @@ namespace zakirov
   private:
     std::pair< BiIter< K, T, C >, bool > find_place_check(const K & k);
     std::pair< BiIter< K, T, C >, bool > find_place(const K & k,  BiNode< K, T > * pos);
+    BiNode< K, T > * rotate_left(BiNode< K, T > * root);
+    BiNode< K, T > * rotate_right(BiNode< K, T > * root);
     BiNode< K, T > * head_;
     size_t size_;
     C cmp_;
@@ -297,27 +299,248 @@ namespace zakirov
   }
 
   template < class K, class T, class C >
-  BiIter< K, T, C > BiTree< K, T, C >::erase(BiIter< K, T, C > pos)
+  BiIter< K, T, C > BiTree< K, T, C >::erase(BiIter< K, T, C > pos) noexcept
   {
+    BiNode< K, T > * to_deleted = pos.node_;
+    BiNode< K, T > * next = pos.node_;
+    BiNode< K, T > * child;
+    detail::next(next);
+    if (!to_deleted)
+    {
+      return pos;
+    }
 
+    if (!to_deleted->left_ && !to_deleted->right_)
+    {
+      if (parent->left == to_deleted)
+      {
+        parent->left = nullptr;
+      }
+      else
+      {
+        parent->right = nullptr;
+      }
+
+      delete to_deleted;
+      --size_;
+      BiIter< K, T, C > res(next);
+      return res;
+    }
+    else if (!to_deleted->left_)
+    {
+      child = to_deleted->right_;
+      if (to_deleted->parent_) 
+      {
+        if (to_deleted->parent_->left_ == to_deleted) 
+        {
+          to_deleted->parent_->left_ = child;
+        } 
+        else 
+        {
+          to_deleted->parent_->right_ = child;
+        }
+      }
+
+      child->parent_ = to_deleted->parent_;
+      to_deleted->parent_->right_ = child;
+    }
+    else if (!to_deleted->right_)
+    {
+      child = to_deleted->left_;
+      if (to_deleted->parent_) 
+      {
+        if (to_deleted->parent_->left_ == to_deleted) 
+        {
+          to_deleted->parent_->left_ = child;
+        } 
+        else 
+        {
+          to_deleted->parent_->right_ = child;
+        }
+      }
+
+      child->parent_ = to_deleted->parent_;
+      to_deleted->parent_->left_ = child;
+    }
+    else
+    {
+      child = to_deleted->right_; 
+      while (child->left_)
+      {
+        child = child->left_;
+      }
+
+      if (child != to_deleted->right_)
+      {
+        child->parent_->left_ = child->right_;
+        if (child->right_)
+        {
+          child->right_->parent_ = child->parent_;
+        }
+
+        child->right_ = to_deleted->right_;
+        to_deleted->right_->parent_ = child;
+      }
+
+      child->left_ = to_deleted->left_;
+      to_deleted->left_->parent_ = child;
+      child->parent_ = to_deleted->parent_;
+      if (to_deleted->parent_) 
+      {
+        if (to_deleted->parent_->left_ == to_deleted) 
+        {
+          to_deleted->parent_->left_ = child;
+        } 
+        else 
+        {
+          to_deleted->parent_->right_ = child;
+        }
+      }
+    }
+
+    if (to_deleted == head_)
+    {
+      head_ = child;
+    }
+
+    delete to_deleted;
+    --size_;
+    BiIter< K, T, C > res(next);
+    return res;
   }
 
   template < class K, class T, class C >
-  BiIter< K, T, C > BiTree< K, T, C >::erase(CBiIter< K, T, C > pos)
+  BiIter< K, T, C > BiTree< K, T, C >::erase(CBiIter< K, T, C > pos) noexcept
   {
+    BiNode< K, T > * to_deleted = pos.node_;
+    BiNode< K, T > * next = pos.node_;
+    BiNode< K, T > * child;
+    detail::next(next);
+    if (!to_deleted)
+    {
+      return pos;
+    }
 
+    if (!to_deleted->left_ && !to_deleted->right_)
+    {
+      if (parent->left == to_deleted)
+      {
+        parent->left = nullptr;
+      }
+      else
+      {
+        parent->right = nullptr;
+      }
+
+      delete to_deleted;
+      --size_;
+      BiIter< K, T, C > res(next);
+      return res;
+    }
+    else if (!to_deleted->left_)
+    {
+      child = to_deleted->right_;
+      if (to_deleted->parent_) 
+      {
+        if (to_deleted->parent_->left_ == to_deleted) 
+        {
+          to_deleted->parent_->left_ = child;
+        } 
+        else 
+        {
+          to_deleted->parent_->right_ = child;
+        }
+      }
+
+      child->parent_ = to_deleted->parent_;
+      to_deleted->parent_->right_ = child;
+    }
+    else if (!to_deleted->right_)
+    {
+      child = to_deleted->left_;
+      if (to_deleted->parent_) 
+      {
+        if (to_deleted->parent_->left_ == to_deleted) 
+        {
+          to_deleted->parent_->left_ = child;
+        } 
+        else 
+        {
+          to_deleted->parent_->right_ = child;
+        }
+      }
+
+      child->parent_ = to_deleted->parent_;
+      to_deleted->parent_->left_ = child;
+    }
+    else
+    {
+      child = to_deleted->right_; 
+      while (child->left_)
+      {
+        child = child->left_;
+      }
+
+      if (child != to_deleted->right_)
+      {
+        child->parent_->left_ = child->right_;
+        if (child->right_)
+        {
+          child->right_->parent_ = child->parent_;
+        }
+
+        child->right_ = to_deleted->right_;
+        to_deleted->right_->parent_ = child;
+      }
+
+      child->left_ = to_deleted->left_;
+      to_deleted->left_->parent_ = child;
+      child->parent_ = to_deleted->parent_;
+      if (to_deleted->parent_) 
+      {
+        if (to_deleted->parent_->left_ == to_deleted) 
+        {
+          to_deleted->parent_->left_ = child;
+        } 
+        else 
+        {
+          to_deleted->parent_->right_ = child;
+        }
+      }
+    }
+
+    if (to_deleted == head_)
+    {
+      head_ = child;
+    }
+
+    delete to_deleted;
+    --size_;
+    BiIter< K, T, C > res(next);
+    return res;
   }
 
   template < class K, class T, class C >
-  T BiTree< K, T, C >::erase(const K & key)
+  size_t BiTree< K, T, C >::erase(const K & key) noexcept
   {
+    BiIter< K, T > it = find(k);
+    if (it == end())
+    {
+      return 0;
+    }
 
+    erase(it)
+    return 1;
   }
 
   template < class K, class T, class C >
   void BiTree< K, T, C >::clear() noexcept
   {
-
+    BiIter< K, T > it = first();
+    while (it != end())
+    {
+      it = erase(it);
+    }
   }
 
   template < class K, class T, class C >
@@ -358,6 +581,74 @@ namespace zakirov
 
     BiIter<K, T, C> res(pos);
     return make_pair(res, false)
+  }
+
+  template< class K, class T, class C >
+  BiNode< K, T > * BiTree< K, T, C >::rotate_right(BiNode< K, T > * root)
+  {
+    if (!root)
+    {
+      throw std::invalid_argument("Invalid rotation");
+    }
+
+    if (!root->left_)
+    {
+      throw std::invalid_argument("Invalid rotation");
+    }
+
+    BiNode< K, T > * new_root = root->left_;
+    new_root->parent_ = root->parent_;
+    if (root->parent_)
+    {
+      if (root->parent_->left_ == root)
+      {
+        root->parent_->left_ = new_root;
+      }
+      else
+      {
+        root->parent_->right_ = new_root;
+      }
+    }
+ 
+    root->parent_ = new_root;
+    root->left_ = new_root->right_;
+    new_root->right_ = root;
+    new_root->right_->parent_ = new_root;
+    return new_root;
+  }
+
+  template< class K, class T >
+  BiNode< K, T >* rotate_left(BiNode< K, T > * root)
+  {
+    if (!root)
+    {
+      throw std::invalid_argument("Invalid rotation");
+    }
+
+    if (!root->right)
+    {
+      throw std::invalid_argument("Invalid rotation");
+    }
+
+    BiTree< T >* new_root = root->right_;
+    new_root->parent_ = root->parent_;
+    if (root->parent_)
+    {
+      if (root->parent_->left_ == root)
+      {
+        root->parent_->left_ = new_root;
+      }
+      else
+      {
+        root->parent_->right_ = new_root;
+      }
+    }
+
+    root->parent_ = new_root;
+    root->right_ = new_root->left_;
+    new_root->left_ = root;
+    new_root->left_->parent_ = new_root;
+    return new_root;
   }
 }
 
