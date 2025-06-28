1dfb04500e2fe81eff9726eb81228824dc10c7a7
brev1ch <flirktv@gmail.com>
2025-06-28 15:33:02 +0300

    check fix

--- brevnov.nikita/S5/main.cpp
+++ brevnov.nikita/S5/main.cpp
@@ -3,19 +3,43 @@
 #include <functional>
 #include <iostream>
 #include <string>
+#include <limits>
 #include <utility>
 #include "tree.hpp"
-#include "keySum.hpp"
 
 using namespace brevnov;
 using Tree = AVLTree< int, std::string >;
 
-void input(std::istream& input, Tree& tree)
+namespace
 {
-  std::pair< int, std::string > pair;
-  while (input >> pair.first >> pair.second)
+  struct KeySum
   {
-    tree.insert(pair);
+    void operator()(const std::pair< const int, std::string >& pair)
+    {
+      const int min = std::numeric_limits< int >::min();
+      const int max = std::numeric_limits< int >::max();
+      if ((sum > max - pair.first) && (sum > 0 && pair.first > 0))
+      {
+        throw std::logic_error("Overflow");
+      }
+      if ((sum < min + pair.first) && (sum < 0 && pair.first < 0))
+      {
+        throw std::logic_error("Underflow");
+      }
+      sum += pair.first;
+      str += " " + pair.second;
+    }
+    int sum = 0;
+    std::string str;
+  };
+
+  void input(std::istream& input, Tree& tree)
+  {
+    std::pair< int, std::string > pair;
+    while (input >> pair.first >> pair.second)
+    {
+      tree.insert(pair);
+    }
   }
 }
 
