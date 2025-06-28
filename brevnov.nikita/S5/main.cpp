26044ca8cb8a06712b3b241df1a4f67a1b016d2d
brev1ch <flirktv@gmail.com>
2025-06-28 15:42:54 +0300

    KeySum

--- brevnov.nikita/S5/main.cpp
+++ brevnov.nikita/S5/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <limits>
 #include <utility>
+#include "keySum.hpp"
 #include "tree.hpp"
 
 using namespace brevnov;
@@ -12,27 +13,6 @@ using Tree = AVLTree< int, std::string >;
 
 namespace
 {
-  struct KeySum
-  {
-    void operator()(const std::pair< const int, std::string >& pair)
-    {
-      const int min = std::numeric_limits< int >::min();
-      const int max = std::numeric_limits< int >::max();
-      if ((sum > max - pair.first) && (sum > 0 && pair.first > 0))
-      {
-        throw std::logic_error("Overflow");
-      }
-      if ((sum < min + pair.first) && (sum < 0 && pair.first < 0))
-      {
-        throw std::logic_error("Underflow");
-      }
-      sum += pair.first;
-      str += " " + pair.second;
-    }
-    int sum = 0;
-    std::string str;
-  };
-
   void input(std::istream& input, Tree& tree)
   {
     std::pair< int, std::string > pair;
