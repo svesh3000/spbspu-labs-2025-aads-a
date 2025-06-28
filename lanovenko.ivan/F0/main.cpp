59e4ff947650a06acabbc36801597bfcc07a134a
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-06-29 01:41:03 +0300

    tree added

--- lanovenko.ivan/F0/main.cpp
+++ lanovenko.ivan/F0/main.cpp
@@ -1,5 +1,5 @@
-#include <limits>
 #include <tree.hpp>
+#include <limits>
 #include "command_processing.hpp"
 #include "input_output_processing.hpp"
 
@@ -28,18 +28,20 @@ int main(int arg, char** argv)
   commands["create_target"] = std::bind(create_target, std::ref(std::cin), std::ref(trg));
   commands["delete_target"] = std::bind(delete_target, std::ref(std::cin), std::ref(trg), std::ref(trgs));
   commands["engage_max_targets"] = std::bind(engage_max_targets, std::ref(std::cin), std::ref(std::cout),
-    std::ref(trg), std::ref(trgs), std::ref(ps));
+  std::ref(trg), std::ref(trgs), std::ref(ps));
   commands["engage_balanced"] = std::bind(engage_balanced, std::ref(std::cin), std::ref(std::cout),
-    std::ref(trg), std::ref(trgs), std::ref(ps));
+   std::ref(trg), std::ref(trgs), std::ref(ps));
   commands["engage_top_threats"] = std::bind(engage_top_threats, std::ref(std::cin), std::ref(std::cout),
-    std::ref(trg), std::ref(trgs), std::ref(ps));
+   std::ref(trg), std::ref(trgs), std::ref(ps));
   commands["engage_manual"] = std::bind(engage_manual, std::ref(std::cin), std::ref(std::cout),
-    std::ref(trg), std::ref(trgs), std::ref(ps));
+   std::ref(trg), std::ref(trgs), std::ref(ps));
   commands["solve_threat"] = std::bind(solve_threat, std::ref(std::cin), std::ref(std::cout),
-    std::ref(trg), std::ref(trgs), std::ref(ps));
+   std::ref(trg), std::ref(trgs), std::ref(ps));
+  commands["target_list"] = std::bind(target_list, std::ref(std::cin), std::ref(std::cout), std::cref(trgs));
   commands["system_status"] = std::bind(system_status, std::ref(std::cin), std::ref(std::cout), std::cref(ps));
   commands["create_target_set"] = std::bind(create_target_set, std::ref(std::cin), std::ref(trgs));
   commands["add_target_to_set"] = std::bind(add_target_to_set, std::ref(std::cin), std::ref(trgs), std::ref(trg));
+  commands["all_targets"] = std::bind(all_targets, std::ref(std::cout), std::cref(trg));
   std::string command;
   while (!(std::cin >> command).eof())
   {
