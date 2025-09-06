a82275c4ed28f3b5b1cb3ff5ff69a5257112850c
A1exCRE <alsmmm2065@gmail.com>
2025-09-06 16:37:37 +0300

    fix main

--- smirnov.alexandr/S5/main.cpp
+++ smirnov.alexandr/S5/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <functional>
 #include <tree/avlTee.hpp>
 #include "key_sum.hpp"
 
+
+namespace
+{
+  using Tree = smirnov::AvlTree< int, std::string >;
+  using Result = smirnov::KeySum;
+  void traverseLnr(const Tree & tree, Result & res)
+  {
+    res = tree.traverse_lnr(res);
+  }
+  void traverseRnl(const Tree & tree, Result & res)
+  {
+    res = tree.traverse_rnl(res);
+  }
+  void traverseBreadth(const Tree & tree, Result & res)
+  {
+    res = tree.traverse_breadth(res);
+  }
+}
 int main(int argc, char* argv[])
 {
   using namespace smirnov;
@@ -32,33 +51,16 @@ int main(int argc, char* argv[])
     std::cout << "<EMPTY>\n";
     return 0;
   }
+  AvlTree< std::string, std::function< void(KeySum &) > > commands;
+  using namespace std::placeholders;
+  commands["ascending"] = std::bind(traverseLnr, std::cref(dict), _1);
+  commands["descending"] = std::bind(traverseRnl, std::cref(dict), _1);
+  commands["breadth"] = std::bind(traverseBreadth, std::cref(dict), _1);
+  std::string command = argv[1];
+  KeySum func;
   try
   {
-    KeySum func;
-    std::string mode(argv[1]);
-    if (mode == "ascending")
-    {
-      func = dict.traverse_lnr(func);
-    }
-    else if (mode == "descending")
-    {
-      func = dict.traverse_rnl(func);
-    }
-    else if (mode == "breadth")
-    {
-      func = dict.traverse_breadth(func);
-    }
-    else
-    {
-      std::cerr << "Invalid traversal mode\n";
-      return 1;
-    }
-    std::cout << func.result;
-    if (!func.elems.empty())
-    {
-      std::cout << " " << func.elems;
-    }
-    std::cout << "\n";
+    commands.at(command)(func);
   }
   catch (const std::overflow_error & e)
   {
@@ -70,4 +72,10 @@ int main(int argc, char* argv[])
     std::cerr << "Error: " << e.what() << "\n";
     return 1;
   }
+  std::cout << func.result;
+  if (!func.elems.empty())
+  {
+    std::cout << " " << func.elems;
+  }
+  std::cout << "\n";
 }
