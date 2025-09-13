f355de8008741f54026f9230ac16b15a624c8a08
svesh3000 <russkij2006@mail.ru>
2025-09-13 14:58:09 +0300

    Commands graph, vertexes, outbound, inbound

--- sveshnikov.michail/S7/commands.cpp
+++ sveshnikov.michail/S7/commands.cpp
@@ -1,16 +1,64 @@
 #include "commands.hpp"
+#include <iostream>
+
+namespace
+{
+  void ioBound(const sveshnikov::AvlTree< std::string, sveshnikov::Array< unsigned int > > &bounds,
+      std::ostream &out);
+
+  void ioBound(const sveshnikov::AvlTree< std::string, sveshnikov::Array< unsigned int > > &bounds,
+      std::ostream &out)
+  {
+    for (auto i = bounds.begin(); i != bounds.end(); i++)
+    {
+      out << i->first;
+      for (size_t j = 0; j < i->second.getSize(); j++)
+      {
+        out << ' ' << i->second[j];
+      }
+      out << '\n';
+    }
+  }
+}
 
 void sveshnikov::graph(const GraphsMap_t &graph_map, std::ostream &out)
-{}
+{
+  if (graph_map.empty())
+  {
+    return;
+  }
+  for (auto it = graph_map.begin(); it != graph_map.end(); it++)
+  {
+    out << it->first << '\n';
+  }
+}
 
 void sveshnikov::vertexes(const GraphsMap_t &graph_map, std::istream &in, std::ostream &out)
-{}
+{
+  std::string graph_name;
+  in >> graph_name;
+  Array< std::string > verts = graph_map.at(graph_name).get_vertexes();
+  for (auto i = 0; i < verts.getSize(); i++)
+  {
+    out << verts[i] << '\n';
+  }
+}
 
 void sveshnikov::outbound(const GraphsMap_t &graph_map, std::istream &in, std::ostream &out)
-{}
+{
+  std::string graph_name, vertex;
+  in >> graph_name >> vertex;
+  auto outbounds = graph_map.at(graph_name).get_outbounds(vertex);
+  ioBound(outbounds, out);
+}
 
 void sveshnikov::inbound(const GraphsMap_t &graph_map, std::istream &in, std::ostream &out)
-{}
+{
+  std::string graph_name, vertex;
+  in >> graph_name >> vertex;
+  auto inbounds = graph_map.at(graph_name).get_inbounds(vertex);
+  ioBound(inbounds, out);
+}
 
 void sveshnikov::bind(GraphsMap_t &graph_map, std::istream &in)
 {}
