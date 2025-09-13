82e84f8d10cc1df2e021e854675bc762d9456ebd
svesh3000 <russkij2006@mail.ru>
2025-09-13 15:40:05 +0300

    Small fix for acceptance

--- sveshnikov.michail/S7/main.cpp
+++ sveshnikov.michail/S7/main.cpp
@@ -46,7 +46,6 @@ int main(int argc, char **argv)
     catch (const std::out_of_range &e)
     {
       std::cout << "<INVALID COMMAND>" << '\n';
-      std::cerr << e.what() << '\n';
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
     catch (std::exception &e)
